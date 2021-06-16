bf8fdfbcfd55e462d2e04fc467abc55c6d6353b2
ednark <ednark@gmail.com>
2021-06-16 15:45:15 -0400

    fix up paths

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/ash 
 set -euo pipefail
 
 SECRETS=$(echo $VCAP_SERVICES | jq -r '.["user-provided"][] | select(.name == "secrets") | .credentials')
@@ -28,51 +28,60 @@ export S3_REGION
 #   # cp "$APP_ROOT/web/template-.htaccess" "$APP_ROOT/web/.htaccess"
 # fi
 
-install_drupal() {
-    ROOT_USER_NAME=$(echo $SECRETS | jq -r '.ROOT_USER_NAME')
-    ROOT_USER_PASS=$(echo $SECRETS | jq -r '.ROOT_USER_PASS')
+# install_drupal() {
+#     ROOT_USER_NAME=$(echo $SECRETS | jq -r '.ROOT_USER_NAME')
+#     ROOT_USER_PASS=$(echo $SECRETS | jq -r '.ROOT_USER_PASS')
 
-    : "${ROOT_USER_NAME:?Need and root user name for Drupal}"
-    : "${ROOT_USER_PASS:?Need and root user pass for Drupal}"
+#     : "${ROOT_USER_NAME:?Need and root user name for Drupal}"
+#     : "${ROOT_USER_PASS:?Need and root user pass for Drupal}"
 
-    drupal site:install \
-        --root=$APP_ROOT/web \
-        --no-interaction \
-        --account-name="$ROOT_USER_NAME" \
-        --account-pass="$ROOT_USER_PASS" \
-        --langcode="en"
-    # Delete some data created in the "standard" install profile
-    # See https://www.drupal.org/project/drupal/issues/2583113
-    drupal --root=$APP_ROOT/web entity:delete shortcut_set default --no-interaction
-    drupal --root=$APP_ROOT/web config:delete active field.field.node.article.body --no-interaction
-    # Set site uuid to match our config
-    UUID=$(grep uuid $APP_ROOT/web/sites/default/config/system.site.yml | cut -d' ' -f2)
-    drupal --root=$APP_ROOT/web config:override system.site uuid $UUID
-}
+#     drupal site:install \
+#         --root=$APP_ROOT/web \
+#         --no-interaction \
+#         --account-name="$ROOT_USER_NAME" \
+#         --account-pass="$ROOT_USER_PASS" \
+#         --langcode="en"
+#     # Delete some data created in the "standard" install profile
+#     # See https://www.drupal.org/project/drupal/issues/2583113
+#     drupal --root=$APP_ROOT entity:delete shortcut_set default --no-interaction
+#     drupal --root=$APP_ROOT config:delete active field.field.node.article.body --no-interaction
+#     # Set site uuid to match our config
+#     UUID=$(grep uuid $APP_ROOT/web/sites/default/config/system.site.yml | cut -d' ' -f2)
+#     drupal --root=$APP_ROOT config:override system.site uuid $UUID
+# }
 
 if [ "${CF_INSTANCE_INDEX:-''}" == "0" ] && [ "${APP_NAME}" == "web" ]; then
-  if [ "$APP_ID" = "docker" ] ; then
-    # make sure database is created
-    echo "create database $DB_NAME;" | mysql --host="$DB_HOST" --port="$DB_PORT" --user="$DB_USER" --password="$DB_PW" || true
-  fi
+#   if [ "$APP_ID" = "docker" ] ; then
+#     # make sure database is created
+#     echo "create database $DB_NAME;" | mysql --host="$DB_HOST" --port="$DB_PORT" --user="$DB_USER" --password="$DB_PW" || true
+#   fi
 
-  drupal --root=$APP_ROOT/web list | grep database > /dev/null || install_drupal
-  # Mild data migration: fully delete database entries related to these
-  # modules. These plugins (and the dependencies) can be removed once they've
-  # been uninstalled in all environments
+#    drupal list | grep database > /dev/null || echo "install_drupal"
+#   # Mild data migration: fully delete database entries related to these
+#   # modules. These plugins (and the dependencies) can be removed once they've
+#   # been uninstalled in all environments
 
-  # Sync configs from code
-  drupal --root=$APP_ROOT/web config:import
+#   # Sync configs from code
+#    drupal config:import
 
-  # Secrets
-  ADMIN_EMAIL=$(echo $SECRETS | jq -r '.ADMIN_EMAIL')
-  CRON_KEY=$(echo $SECRETS | jq -r '.CRON_KEY')
-  drupal --root=$APP_ROOT/web config:override system.site mail $ADMIN_EMAIL > /dev/null
-  drupal --root=$APP_ROOT/web config:override update.settings notification.emails.0 $ADMIN_EMAIL > /dev/null
+    drush state:set system.maintenance_mode 1 -y
+    drush cr
+    drush updatedb --no-cache-clear -y
+    drush cim -y || drush cim -y
+    drush cim -y
+    drush php-eval "node_access_rebuild();" -y
+    drush state:set system.maintenance_mode 0 -y
+    drush cr
 
-  # Import initial content
-  # drush --root=$APP_ROOT/web default-content-deploy:import --no-interaction
+#   # Secrets
+    # ADMIN_EMAIL=$(echo $SECRETS | jq -r '.ADMIN_EMAIL')
+    # CRON_KEY=$(echo $SECRETS | jq -r '.CRON_KEY')
+    # drupal config:override system.site mail $ADMIN_EMAIL > /dev/null
+    # drupal config:override update.settings notification.emails.0 $ADMIN_EMAIL > /dev/null
 
-  # Clear the cache
-  drupal --root=$APP_ROOT/web cache:rebuild --no-interaction
+# #   # Import initial content
+# #   # drush --root=$APP_ROOT/web default-content-deploy:import --no-interaction
+
+# #   # Clear the cache
+#     drupal cache:rebuild --no-interaction
 fi
