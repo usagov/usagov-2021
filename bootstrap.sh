7c448aebe39a6f928b0cfb3645a7235eb06f31a9
ednark <ednark@gmail.com>
2021-06-23 10:10:41 -0400

    updates

--- bootstrap.sh
+++ bootstrap.sh
@@ -64,14 +64,14 @@ if [ "${CF_INSTANCE_INDEX:-''}" == "0" ] && [ "${APP_NAME}" == "web" ]; then
 #   # Sync configs from code
 #    drupal config:import
 
-    drush state:set system.maintenance_mode 1 -y
-    drush cr
-    drush updatedb --no-cache-clear -y
-    drush cim -y || drush cim -y
-    drush cim -y
-    drush php-eval "node_access_rebuild();" -y
-    drush state:set system.maintenance_mode 0 -y
-    drush cr
+    # drush state:set system.maintenance_mode 1 -y
+    # drush cr
+    # drush updatedb --no-cache-clear -y
+    # drush cim -y || drush cim -y
+    # drush cim -y
+    # drush php-eval "node_access_rebuild();" -y
+    # drush state:set system.maintenance_mode 0 -y
+    # drush cr
 
 #   # Secrets
     # ADMIN_EMAIL=$(echo $SECRETS | jq -r '.ADMIN_EMAIL')
