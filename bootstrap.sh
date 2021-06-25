57ccb7d14c2b011943099775a24813d2a572343d
ednark <ednark@gmail.com>
2021-06-25 15:58:53 -0400

    bin tools always mount local dir over var/www

--- bootstrap.sh
+++ bootstrap.sh
@@ -23,8 +23,6 @@ echo  "Fixing File Permissions ... "
 chown nginx:nginx /var/www
 # find /var/www -group 0 -user 0 -print0 | xargs -P 0 -0 --no-run-if-empty chown --no-dereference nginx:nginx
 find /var/www -not -user 100 -not -group 101 -print0 | xargs -P 0 -0 --no-run-if-empty chown --no-dereference nginx:nginx
-sleep 5
-find /var/www -not -user 100 -not -group 101 -print0 | xargs -P 0 -0 --no-run-if-empty chown --no-dereference nginx:nginx
 
 # if [ -n "$S3_BUCKET" ] && [ -n "$S3_REGION" ]; then
 #   # Add Proxy rewrite rules to the top of the htaccess file
