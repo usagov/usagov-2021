b86ebd1a6782e9406599e020de63fd16d1d97683
ednark <ednark@gmail.com>
2021-07-02 09:58:07 -0400

    remove tome sync disabling

--- web/sites/default/settings.local.php
+++ web/sites/default/settings.local.php
@@ -1,8 +1,10 @@
 <?php
 
-$databases['default']['default'] = [
-    'driver' => 'sqlite',
-    'database' => '/var/www/'.getenv('LOCALDB'),
-];
+// $databases['default']['default'] = [
+//     'driver' => 'sqlite',
+//     'database' => '/var/www/'.getenv('LOCALDB'),
+// ];
 
 $config['admin_toolbar_tools.settings']['hoverintent_functionality'] = TRUE;
+
+$settings['container_yamls'][] = DRUPAL_ROOT . '/sites/development.services.yml';
