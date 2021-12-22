c2f169af4f3d6f321d2e5ac5a8069e20c4a730f4
ednark <ednark@gmail.com>
2021-12-22 16:56:52 -0500

    main: get all scripts during cms build

--- scripts/tome-to-s3-sync.sh
+++ scripts/tome-to-s3-sync.sh
@@ -6,19 +6,12 @@ export AWS_ACCESS_KEY_ID=$(echo "${VCAP_SERVICES}" | jq -r '.["s3"][]? | select(
 export AWS_SECRET_ACCESS_KEY=$(echo "${VCAP_SERVICES}" | jq -r '.["s3"][]? | select(.name == "storage") | .credentials.secret_access_key')
 export AWS_ENDPOINT=$(echo "${VCAP_SERVICES}" | jq -r '.["s3"][]? | select(.name == "storage") | .credentials.hostname')
 
-#move from /var/www/html into /tmp/rand
-#lowercase /tmp/rand
-#delete /tmp/rand
-
-#lowercase=$(echo $file | tr '[A-Z]' '[a-z]'])
-#/bin/mv $file $lowercase
-
-# find | xargs mv og_filename $(echo $og_filename | tr '[:upper:]' '[:lower:]')
-
-#find . -exec mv filename='{}' $(echo $filename | tr '[:upper:]' '[:lower:]') ';'
-
-
-
 #aws s3 sync /var/www/html s3://$BUCKET_NAME/web --acl public-read --endpoint-url https://$AWS_ENDPOINT --no-verify-ssl --delete
 
-aws s3 ls s3://$BUCKET_NAME
+mkdir -p /tmp/export;
+cp -R /var/www/html/* /tmp/export/
+cd /tmp/export
+
+for f in `find .`; do mv -v "$f" $(echo $f | tr '[A-Z]' '[a-z]'); done
+
+aws s3 sync /tmp/export s3://$BUCKET_NAME/web/ --acl public-read --delete
