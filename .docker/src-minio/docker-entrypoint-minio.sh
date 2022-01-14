cbeee11750ce91e0d98badda588d38233373b021
ednark <ednark@gmail.com>
2022-01-14 11:12:42 -0500

    main: create minio bucket dir by defualt if not exists

--- .docker/src-minio/docker-entrypoint-minio.sh
+++ .docker/src-minio/docker-entrypoint-minio.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+mkdir -p /data/local/web
 mc config host rm local
 mc config host add local http://127.0.0.1:9000 ${MINIO_ROOT_USER:-minioadmin} ${MINIO_ROOT_PASSWORD:-miniopass}
 mc mb -p local/web
 mc policy set public local/web
 mc policy set public local/web/*
-chmod -R 777 /data/web/
+chmod -R 777 /data/local/web
 
 /usr/bin/docker-entrypoint.sh "$@"
