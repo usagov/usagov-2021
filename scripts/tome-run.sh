ff0cf15fdbc488b46579d014c5fc7fe9e6dee06d
ednark <ednark@gmail.com>
2022-01-31 11:02:09 -0500

    usagov-83-tome-log: setup 30 min cron

--- scripts/tome-run.sh
+++ scripts/tome-run.sh
@@ -2,15 +2,20 @@
 
 SCRIPT_PATH=$(dirname "$0")
 
-URI=${1:-http://beta.usa.gov}
+URI=${1:-https://beta.usa.gov}
 
 TOMELOGDIR=/tmp/tome-log
-TOMELOG=$TOMELOGDIR/tome_$(date +"%Y_%m_%d_%H_%M_%S").log
+TOMELOG=$TOMELOGDIR/tome_run_$(date +"%Y/%m/%d/%H_%M").log
 
 mkdir -p $TOMELOGDIR
 touch $TOMELOG
 
-$SCRIPT_PATH/tome-build.sh $URI 2>&1 | tee $TOMELOG
-$SCRIPT_PATH/tome-sync.sh $TOMELOG
+# check nodes and blocks for any content changes in the last 30 minutes
+export CONTENT_UPDATED=$(drush sql:query "SELECT SUM(c) FROM ( (SELECT count(*) as c from node_field_data where changed > (UNIX_TIMESTAMP(now())-(1800)))
+ UNION ( SELECT count(*) as c from block_content_field_data where changed > (UNIX_TIMESTAMP(now())-(1800))) ) as x")
+if [ "$CONTENT_UPDATED" != "0" ]; then
+  $SCRIPT_PATH/tome-build.sh $URI 2>&1 | tee $TOMELOG
+  $SCRIPT_PATH/tome-sync.sh $TOMELOG
+fi;
 
 echo "Full Log : $TOMELOG";
--- scripts/tome-sync.sh
+++ scripts/tome-sync.sh
@@ -8,17 +8,22 @@ AWS_ACCESS_KEY_ID=$(echo "${VCAP_SERVICES}" | jq -r '.["s3"][]? | select(.name =
 AWS_SECRET_ACCESS_KEY=$(echo "${VCAP_SERVICES}" | jq -r '.["s3"][]? | select(.name == "storage") | .credentials.secret_access_key')
 AWS_ENDPOINT=$(echo "${VCAP_SERVICES}" | jq -r '.["s3"][]? | select(.name == "storage") | .credentials.hostname')
 
-mkdir -p /tmp/tome;
-cp -R /var/www/html/* /tmp/tome/
-cd /tmp/tome
+# Use a unique dir for each run - just in case more than one are running
+UNIQ_DIR=$(date +"%Y_%m_%d_%H_%M_%S")
 
-for f in `find /tmp/tome/*`; do
+mkdir -p /tmp/tome/$UNIQ_DIR;
+cp -R /var/www/html/* /tmp/tome/$UNIQ_DIR
+cd /tmp/tome/$UNIQ_DIR
+
+# lower case  everything
+for f in `find /tmp/tome/$UNIQ_DIR/*`; do
   ff=$(echo $f | tr '[A-Z]' '[a-z]');
   [ "$f" != "$ff" ] && mv -v "$f" "$ff";
 done
 
+# TODO: fix --endpoint-url and --no-verify-ssl. They are required for minio functionality on local docker but cause trouble against real s3
 #aws s3 sync /var/www/html s3://$BUCKET_NAME/web --acl public-read --endpoint-url https://$AWS_ENDPOINT --no-verify-ssl --delete
-echo "aws s3 sync /tmp/tome s3://$BUCKET_NAME/web/ --acl public-read --delete"
+echo "aws s3 sync /tmp/tome/$UNIQ_DIR s3://$BUCKET_NAME/web/ --acl public-read --delete"
 
 if [ -z "$TOMELOG" ]; then
   echo "aws s3 cp $TOMELOG s3://$BUCKET_NAME/tome/$TOMELOG"
