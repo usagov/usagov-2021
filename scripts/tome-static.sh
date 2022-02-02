0116937b545bde83b322a372f09386d39686df11
ednark <ednark@gmail.com>
2022-02-02 17:05:08 -0500

    usagov-83-tome-log: cron tome needs uri per env

--- scripts/tome-static.sh
+++ scripts/tome-static.sh
@@ -1,6 +1,6 @@
 #!/bin/ash
 
-URI=${1:-http://beta.usa.gov}
+URI=${1:-https://beta.usa.gov}
 
 echo "Starting Static Site Generation : "$(date)
 #time drush -vvv tome:static --uri=$URI --process-count=1 --path-count=1
