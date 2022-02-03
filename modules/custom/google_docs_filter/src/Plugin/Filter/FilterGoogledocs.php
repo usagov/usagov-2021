a741ee1f5508caf5ef26b35b9c54cb8c799f7e15
Kyle <raymond.jester@gsa.gov>
2022-02-03 10:43:29 -0600

    Updated google docs filter

--- modules/custom/google_docs_filter/src/Plugin/Filter/FilterGoogledocs.php
+++ modules/custom/google_docs_filter/src/Plugin/Filter/FilterGoogledocs.php
@@ -24,11 +24,10 @@ use Drupal\Core\Form\FormStateInterface;
 
 class FilterGoogledocs extends FilterBase
 {
-    public function process($text, $langcode)
-    {
-        $replace =  $this->t('') ;
-        $new_text = str_replace(array('rel="noopener"','target="_blank"'), $replace, $text);
-        $result = new FilterProcessResult($new_text);
-        return $result;
-    }
+public function process($text, $langcode){
+    $replace = $this->t('');
+    $new_text = str_replace(array('rel="noopener"', 'target="_blank"'), $replace, $text);
+    $result = new FilterProcessResult($new_text);
+    return $result;
+}
 }
