38a6cb4d5eb2ba2997487166068c7c51bc04befb
ednark <ednark@gmail.com>
2022-02-07 17:17:00 -0500

    usagov-83-tome-log: cron triggers

--- scripts/tome-run.sh
+++ scripts/tome-run.sh
@@ -4,22 +4,23 @@ SCRIPT_PATH=$(dirname "$0")
 
 URI=${1:-https://beta.usa.gov}
 
-TOMELOGDIR=/tmp/tome-log
-TOMELOG=$TOMELOGDIR/tome_run_$(date +"%Y/%m/%d/%H_%M").log
-
-mkdir -p $TOMELOGDIR
-touch $TOMELOG
+YMD=$(date +"%Y/%m/%d")
+HM=$(date +"%H_%M")
 
 # check nodes and blocks for any content changes in the last 30 minutes
 export CONTENT_UPDATED=$(drush sql:query "SELECT SUM(c) FROM ( (SELECT count(*) as c from node_field_data where changed > (UNIX_TIMESTAMP(now())-(1800)))
  UNION ( SELECT count(*) as c from block_content_field_data where changed > (UNIX_TIMESTAMP(now())-(1800))) ) as x")
 if [ "$CONTENT_UPDATED" != "0" ]; then
-  echo "Found a change to block or node content in the last 30 minutes: running static site build"
-  echo "Writing log to $TOMELOG";
-  $SCRIPT_PATH/tome-build.sh $URI 2>&1 | tee $TOMELOG
+
+  TOMELOGDIR=/tmp/tome-log/tome_run_$YMD
+  TOMELOG=$TOMELOGDIR/$HM.log
+
+  mkdir -p $TOMELOGDIR
+  touch $TOMELOG
+
+  echo "Found site changes: running static site build: $TOMELOG"
+  $SCRIPT_PATH/tome-static.sh $URI 2>&1 | tee $TOMELOG
   $SCRIPT_PATH/tome-sync.sh $TOMELOG
 else
   echo "No change to block or node content in the last 30 minutes: no need for static site build"
 fi
-
-echo "Full Log : $TOMELOG"
