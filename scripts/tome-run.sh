44405ae0b39029895c51281c3b2c8408d4a1a514
ednark <ednark@gmail.com>
2022-02-09 00:41:20 -0500

    main: update tome scripts to run from ron and log to tmp

--- scripts/tome-run.sh
+++ scripts/tome-run.sh
@@ -1,8 +1,9 @@
-#!/bin/ash
+#!/bin/sh
 
 SCRIPT_PATH=$(dirname "$0")
 
 URI=${1:-https://beta.usa.gov}
+FORCE=${2:-0}
 
 YMD=$(date +"%Y/%m/%d")
 HM=$(date +"%H_%M")
@@ -10,9 +11,9 @@ HM=$(date +"%H_%M")
 # check nodes and blocks for any content changes in the last 30 minutes
 export CONTENT_UPDATED=$(drush sql:query "SELECT SUM(c) FROM ( (SELECT count(*) as c from node_field_data where changed > (UNIX_TIMESTAMP(now())-(1800)))
  UNION ( SELECT count(*) as c from block_content_field_data where changed > (UNIX_TIMESTAMP(now())-(1800))) ) as x")
-if [ "$CONTENT_UPDATED" != "0" ]; then
+if [ "$CONTENT_UPDATED" != "0" ] || [[ "$FORCE" =~ ^\-{0,2}f\(orce\)?$ ]]; then
 
-  TOMELOGDIR=/tmp/tome-log/tome_run_$YMD
+  TOMELOGDIR=/tmp/tome-log/$YMD
   TOMELOG=$TOMELOGDIR/$HM.log
 
   mkdir -p $TOMELOGDIR
