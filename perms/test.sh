25eabf01f2637d56a38b7585d166b2ada9b3a6f4
ednark <ednark@gmail.com>
2022-03-10 15:17:34 -0500

    docker-perms: perms

---
+++ perms/test.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+echo "TESTING permission when NOT forcing ids"
+
+# rm -f ./root
+# rm -f ./nginx
+
+# echo "building custom container"
+# docker build -q --force-rm \
+#     -t usagov-perms \
+#     -f ./Dockerfile-perms ./
+
+# echo "::: Running non-forced-id tests"
+
+# docker run -v $(pwd):/app --rm --user root  usagov-perms /in-container-test.sh
+# echo;
+# echo "Host permissions for 'root'"
+# ls -al ./root
+# ls -n ./root
+
+
+# docker run -v $(pwd):/app --rm --user nginx usagov-perms /in-container-test.sh
+# echo
+# echo "Host permissions for 'nginx'"
+# ls -al ./nginx
+# ls -n  ./nginx
+
+# echo; echo; echo; echo;
+
+# rm -f ./root.forced
+# rm -f ./nginx.forced
+
+# echo "TESTING permission when forcing ids"
+
+echo "building custom container"
+docker build -q --force-rm \
+    -t usagov-perms \
+    --build-arg FORCEIDS=1 \
+    --build-arg UID=$(id -u) \
+    -f ./Dockerfile-perms ./
+
+# echo "::: Running forced-id tests"
+
+# docker run -v $(pwd):/app --rm --user root  usagov-perms /in-container-test.sh
+# echo;
+# echo "Host permissions for 'root'"
+# ls -al ./root.forced
+# ls -n ./root.forced
+
+# docker run -v $(pwd):/app --rm --user nginx usagov-perms /in-container-test.sh
+# echo
+# echo "Host permissions for 'nginx'"
+# ls -al ./nginx.forced
+# ls -n  ./nginx.forced
