608b81b12bd0a543ce6e320ab2ed3bc40fbfd227
ednark <ednark@gmail.com>
2022-04-21 12:04:57 -0400

    m1-fix: minio aliases

--- .docker/src-minio/docker-entrypoint-minio.sh
+++ .docker/src-minio/docker-entrypoint-minio.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-mkdir -p /data/local/web
-mc --insecure config host rm docker
-mc --insecure config host add docker http://127.0.0.1:9000 ${MINIO_ROOT_USER:-minioadmin} ${MINIO_ROOT_PASSWORD:-miniopass}
+# mc --insecure config host rm docker
+# mc --insecure config host add docker https://127.0.0.1:9000 ${MINIO_ROOT_USER:-minioadmin} ${MINIO_ROOT_PASSWORD:-miniopass}
+# mc --insecure alias rm docker
+# mc --insecure alias set docker https://127.0.0.1:9000 ${MINIO_ROOT_USER:-minioadmin} ${MINIO_ROOT_PASSWORD:-miniopass}
 mc --insecure mb -p docker/local
 mc --insecure policy set public docker/local
 mc --insecure policy set public docker/local/
+mkdir -p /data/local/web
+chmod -R 777 /data/local/web
 mc --insecure policy set public docker/local/web
 mc --insecure policy set public docker/local/web/
-chmod -R 777 /data/local/web
 
 /usr/bin/docker-entrypoint.sh "$@"
