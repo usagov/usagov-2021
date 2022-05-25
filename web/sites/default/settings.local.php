3323cf6b457b943e995ecbb5d12443cecc820ab9
ednark <ednark@gmail.com>
2022-05-25 10:29:28 -0400

    minio-local: local minio working?

--- web/sites/default/settings.local.php
+++ web/sites/default/settings.local.php
@@ -19,6 +19,7 @@ $settings['container_yamls'][] = DRUPAL_ROOT . '/sites/development.services.yml'
 $cf_service_data = json_decode($_ENV['VCAP_SERVICES'] ?? '{}', true);
 
 $settings['file_public_path'] = 'sites/default/files';
+/*
 foreach ($cf_service_data as $service_provider => $service_list) {
   foreach ($service_list as $service) {
     if ($service['name'] === 'storage') {
@@ -30,6 +31,18 @@ foreach ($cf_service_data as $service_provider => $service_list) {
       $config['s3fs.settings']['private_folder'] = 'private';
       // -----------------------------------
 
+      $config['s3fs.settings']['root_folder'] = '';
+
+      $config['s3fs.settings']['use_cname'] = TRUE;
+      $config['s3fs.settings']['domain'] = $_SERVER['HTTP_HOST'] . '/s3/files';
+      $config['s3fs.settings']['domain_root'] = 'none';
+
+      $config['s3fs.settings']['use_customhost'] = TRUE;
+      $config['s3fs.settings']['hostname'] = $service['credentials']['hostname'];
+      $config['s3fs.settings']['use-path-style-endpoint'] = FALSE;
+      $config['s3fs.settings']['disable_version_sync'] = TRUE;
+      $config['s3fs.settings']['disable_cert_verify'] = TRUE;
+
       $config['s3fs.settings']['use_https'] = TRUE;
       $settings['s3fs.upload_as_private'] = FALSE;
       $settings['s3fs.use_s3_for_public'] = TRUE;
@@ -38,3 +51,4 @@ foreach ($cf_service_data as $service_provider => $service_list) {
     }
   }
 }
+*/
