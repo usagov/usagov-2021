3ae3c1b8baf8e9285958595e70d7aa4c993a55f3
Olawale Adekoya <olawaley2k@gmail.com>
2022-05-29 20:16:12 -0400

    M1_workaround: update files

--- bin/calls/node_amd64.sh
+++ bin/calls/node_amd64.sh
@@ -4,7 +4,7 @@ node='node'
 nodearm64='amd64/node'
 echo ${amd}
 if [ ${arm} == 'arm64' ]; then
-  sed -e 's|{node}|'${nodearm64}'|' .docker/Dockerfile-cms
+  sed -i '' -e 's|{node}|'${nodearm64}'|' .docker/Dockerfile-cms
 else
-  sed -e 's|{node}|'${node}'|' .docker/Dockerfile-cms
+  sed -i '' -e 's|{node}|'${node}'|' .docker/Dockerfile-cms
 fi
