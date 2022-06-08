c7dca0ddd1bf62459037260ae5569c8487bd1d3d
Olawale Adekoya <olawaley2k@gmail.com>
2022-06-08 16:17:40 -0400

    USAGOV_289-Local_Dev_Aggregate_resolution_to_288_ticket: update

--- web/sites/default/settings.local.php
+++ web/sites/default/settings.local.php
@@ -13,3 +13,39 @@ $settings['trusted_host_patterns'] = [
   '^127\.0\.0\.1$',
   '^cms-usagov\.docker\.local$',
 ];
+
+$cf_service_data = json_decode($_ENV['VCAP_SERVICES'] ?? '{}', TRUE);
+
+foreach ($cf_service_data as $service_list) {
+  foreach ($service_list as $service) {
+    if ($service['name'] === 'storage') {
+
+      $settings['s3fs.access_key'] = $service['credentials']['access_key_id'];
+      $settings['s3fs.secret_key'] = $service['credentials']['secret_access_key'];
+      $config['s3fs.settings']['bucket'] = $service['credentials']['bucket'];
+      $config['s3fs.settings']['region'] = $service['credentials']['region'];
+
+      $config['s3fs.settings']['root_folder'] = 'cms';
+      $config['s3fs.settings']['public_folder'] = 'public';
+      $config['s3fs.settings']['private_folder'] = 'private';
+
+      $S3_PROXY_PATH_CMS = getenv('S3_PROXY_PATH_CMS') ?: '/s3/files';
+
+      $config['s3fs.settings']['use_cname'] = TRUE;
+      $config['s3fs.settings']['domain'] = $_SERVER['HTTP_HOST'] .':'. $service['credentials']['endport']. $S3_PROXY_PATH_CMS;
+      $config['s3fs.settings']['domain_root'] = 'public';
+
+      $config['s3fs.settings']['use_customhost'] = TRUE;
+      $config['s3fs.settings']['hostname'] = $service['credentials']['fips_endpoint'];
+      $config['s3fs.settings']['use-path-style-endpoint'] = FALSE;
+
+      $config['s3fs.settings']['use_https'] = TRUE;
+      $settings['s3fs.upload_as_private'] = FALSE;
+      $settings['s3fs.use_s3_for_public'] = TRUE;
+      $settings['s3fs.use_s3_for_private'] = TRUE;
+
+
+
+    }
+  }
+}
