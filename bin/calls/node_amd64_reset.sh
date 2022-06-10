1426ada3d4fa08e057c8838a03f628e31f3b272e
Olawale Adekoya <olawaley2k@gmail.com>
2022-06-10 12:22:29 -0400

    M1_workaround: update for arm64

--- bin/calls/node_amd64_reset.sh
+++ bin/calls/node_amd64_reset.sh
@@ -4,5 +4,5 @@ node='node'
 nodearm64='arm64v8/node'
 echo ${arm}
 if [ ${arm} == 'arm64' ]; then
-  sed -i '' -e 's|FROM node:|FROM '${nodearm64}':|' .docker/Dockerfile-cms
+  sed -i '' -e 's|FROM arm64v8/node:|FROM '${node}':|' .docker/Dockerfile-cms
 fi  
