914f068c251d9b6537ab5b8ffc9d77e0bde2bdaa
Olawale Adekoya <olawaley2k@gmail.com>
2022-06-14 13:26:49 -0400

    M1_workaround: update

--- bin/calls/node_amd64_reset.sh
+++ bin/calls/node_amd64_reset.sh
@@ -2,7 +2,10 @@
 arm=`uname -m`
 node='node'
 nodearm64='arm64v8/node'
-echo ${arm}
+pfro='#{phantform_for_m1}'
+pend='#{phantform_for_m1-END}'
+echo '=========='${arm}'=========='
 if [ ${arm} == 'arm64' ]; then
   sed -i '' -e 's|FROM arm64v8/node:|FROM '${node}':|' .docker/Dockerfile-cms
+  sed -i '' -e "/^$pfro/,/^$pend/{/^$pfro/!{/^$pend/!d;};}" .docker/Dockerfile-cms
 fi  
