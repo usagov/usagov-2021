8ff98d2ebefb0b37c8d79c141b0c0abfcd126f8f
Olawale Adekoya <olawaley2k@gmail.com>
2022-06-21 12:05:54 -0400

    USAGOV_289-Local_Dev_Aggregate_resolution_to_288_ticket: remove not-needed directives

--- bin/src/exec.sh
+++ bin/src/exec.sh
@@ -4,7 +4,5 @@ docker exec -it -u nginx cms sh -c "mysql -h database -u root -pmysql drupal < u
 docker exec -it cms sh -c "drush en -y allowed_formats autologout image_style_warmer usa_admin_styles usa_twig_vars"
 docker exec -it -u nginx cms sh -c "drush cim -y"
 docker exec -it cms sh -c 'rm -rf s3/local/cms/public/css/* s3/local/cms/public/css/*'
-docker exec -it -u nginx cms sh -c 'drush -y config-set system.performance css.preprocess 0'
-docker exec -it -u nginx cms sh -c 'drush -y config-set system.performance js.preprocess 0'
 docker exec -it -u nginx cms sh -c 'drush -y s3fs-copy-local'
 docker exec -it -u nginx cms sh -c 'drush cr && drush cron'
