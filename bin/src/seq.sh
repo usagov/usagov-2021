ad7512a1b419a3ff24406cb02d623f060c5a8304
Olawale Adekoya <olawaley2k@gmail.com>
2022-06-21 12:50:56 -0400

    USAGOV_289-Local_Dev_Aggregate_resolution_to_288_ticket: update mutlple file to remove exec from build process

--- bin/src/seq.sh
+++ bin/src/seq.sh
@@ -3,9 +3,9 @@ echo '=============== bin/init ==============='
 bin/init
 echo '=============== docker compose up -d ==============='
 docker compose up -d
-echo '=============== bin/db-update ==============='
-bin/db-update
 echo '=============== bin/drupal-update ==============='
 bin/drupal-update
+echo '=============== bin/db-update ==============='
+bin/db-update
 echo '=============== docker compose up -d ==============='
 docker compose up -d
