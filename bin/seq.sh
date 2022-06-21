08c5f4e7615617ac044bcac295e88d6a4ff6cd31
Olawale Adekoya <olawaley2k@gmail.com>
2022-06-21 12:50:56 -0400

    USAGOV_289-Local_Dev_Aggregate_resolution_to_288_ticket: update mutlple file to remove exec from build process

--- bin/seq.sh
+++ bin/seq.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 bin/src/seq.sh
-bin/src/exec.sh
--- bin/src/exec.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo 'update database with backed up sql'
-docker exec -it -u nginx cms sh -c "mysql -h database -u root -pmysql drupal < usagov.sql"
-docker exec -it cms sh -c "drush en -y allowed_formats autologout image_style_warmer usa_admin_styles usa_twig_vars"
-docker exec -it -u nginx cms sh -c "drush cim -y"
-docker exec -it cms sh -c 'rm -rf s3/local/cms/public/css/* s3/local/cms/public/css/*'
-docker exec -it -u nginx cms sh -c 'drush -y s3fs-copy-local'
-docker exec -it -u nginx cms sh -c 'drush cr && drush cron'
