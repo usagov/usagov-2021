910ef247c46842080b1f9e14bd44dd952fc4db28
Olawale Adekoya <olawaley2k@gmail.com>
2022-06-23 14:02:49 -0400

    update for test

--- web/sites/default/settings.local.php
+++ web/sites/default/settings.local.php
@@ -1,40 +1,18 @@
 <?php
 
-if ( !empty(getenv('LOCALDB')) ) {
-    $databases['default']['default'] = [
-        'driver' => 'sqlite',
-        'database' => '/var/www/'.getenv('LOCALDB'),
-    ];
-}
-
 # $settings['tome_static_path_exclude'] = [];
-
-$settings['new_relic_rpm.api_key'] = getenv('NEW_RELIC_API_KEY');
-$config['new_relic_rpm.settings']['api_key'] = getenv('NEW_RELIC_API_KEY');
-
-$config['admin_toolbar_tools.settings']['hoverintent_functionality'] = TRUE;
+# $config['admin_toolbar_tools.settings']['hoverintent_functionality'] = TRUE;
 
 $settings['container_yamls'][] = DRUPAL_ROOT . '/sites/development.services.yml';
 
-$cf_service_data = json_decode($_ENV['VCAP_SERVICES'] ?? '{}', true);
+$config['s3fs.settings']['disable_version_sync'] = TRUE;
+$config['s3fs.settings']['disable_cert_verify'] = TRUE;
 
-$settings['file_public_path'] = 'sites/default/files';
-foreach ($cf_service_data as $service_provider => $service_list) {
-  foreach ($service_list as $service) {
-    if ($service['name'] === 'storage') {
-      $settings['s3fs.access_key'] = $service['credentials']['access_key_id'];
-      $settings['s3fs.secret_key'] = $service['credentials']['secret_access_key'];
-      $config['s3fs.settings']['bucket'] = $service['credentials']['bucket'];
-      $config['s3fs.settings']['region'] = $service['credentials']['region'];
-      $config['s3fs.settings']['public_folder'] = 'public';
-      $config['s3fs.settings']['private_folder'] = 'private';
-      // -----------------------------------
+$settings['trusted_host_patterns'] = [
+  '^localhost$',
+  '^127\.0\.0\.1$',
+  '^cms-usagov\.docker\.local$',
+];
 
-      $config['s3fs.settings']['use_https'] = TRUE;
-      $settings['s3fs.upload_as_private'] = FALSE;
-      $settings['s3fs.use_s3_for_public'] = TRUE;
-      $settings['s3fs.use_s3_for_private'] = TRUE;
-      $settings['php_storage']['twig']['directory'] = '../storage/php';
-    }
-  }
-}
+$config['system.performance']['css']['preprocess'] = FALSE; 
+$config['system.performance']['js']['preprocess'] = FALSE;
