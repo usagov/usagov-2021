bbdb445cec52cbf629a028dc6c9f53a3dbb6b0d8
Olawale Adekoya <olawaley2k@gmail.com>
2022-07-06 10:50:10 -0400

    conf_gradual_update: update betapush by adding profile and drush location

--- scripts/beta/betapush.sh
+++ scripts/beta/betapush.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+. /etc/profile
 www=/var/www &&\
   html=${www}/html &&\
   html_files=${html}/s3/files/ &&\
   theme=${www}/web/themes/custom/usagov &&\
+  drushcmd=/var/www/vendor/bin/drush $$\
   S3_BUCKET=`echo $VCAP_SERVICES | jq -r '.["s3"][]? | select(.name == "storage") | .credentials.bucket'` &&\
   URI=${1:-https://beta.usa.gov}
 
@@ -12,7 +14,7 @@ mkdir -p ${html_files}/js ${html_files}/css $html/themes/custom/usagov/fonts $ht
   drush cr --root=${www} && drush cron --root=${www} &&\
   drush -y s3fs-copy-local --root=${www} 
 
-tomestatic="mkdir -p /tmp/betahtml && drush cr --root=${www} && drush tome:static -y --uri=$URI --process-count=10 --path-count=10 --root=${www} && sleep 60 && cp -rf $theme/fonts $html/themes/custom/usagov/ && cp -rf $theme/images $html/themes/custom/usagov/ && cp -rf ${www}/html /tmp/betahtml"
+tomestatic="mkdir -p /tmp/betahtml && drush cr --root=${www} && ${drushcmd} tome:static -y --uri=$URI --process-count=10 --path-count=10 --root=${www} && sleep 60 && cp -rf $theme/fonts $html/themes/custom/usagov/ && cp -rf $theme/images $html/themes/custom/usagov/ && cp -rf ${www}/html /tmp/betahtml"
 
 if [ `echo "$VCAP_APPLICATION" | jq -r '.space_name'` != "local" ]; then
   ${www}/scripts/beta/betaupdate.sh -c ${tomestatic} -f ${html_files} -h ${html}
