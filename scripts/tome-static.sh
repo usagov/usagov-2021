77ecbcef7ab1c50e55ab1d33efb069cb18081b21
Olawale Adekoya <olawaley2k@gmail.com>
2022-07-07 10:51:19 -0400

    conf_gradual_update: comment out all tome(s)

--- scripts/tome-static.sh
+++ scripts/tome-static.sh
@@ -1,13 +1,13 @@
-#!/bin/sh
-set -x
+# #!/bin/sh
+# set -x
 
-URI=${1:-https://beta.usa.gov}
+# URI=${1:-https://beta.usa.gov}
 
-echo "Starting Static Site Generation : "$(date)
-mkdir -p /var/www/html
-# time drush -vvv tome:static --uri=$URI --process-count=1 --path-count=1
-# time drush tome:static -y --uri=$URI --process-count=5 --path-count=1
-time drush tome:static -y --uri=$URI --process-count=1 --path-count=1
-TOME_SUCCESS=$?
-echo "Finished Static Site Generation : "$(date)
-exit $TOME_SUCCESS
+# echo "Starting Static Site Generation : "$(date)
+# mkdir -p /var/www/html
+# # time drush -vvv tome:static --uri=$URI --process-count=1 --path-count=1
+# # time drush tome:static -y --uri=$URI --process-count=5 --path-count=1
+# time drush tome:static -y --uri=$URI --process-count=1 --path-count=1
+# TOME_SUCCESS=$?
+# echo "Finished Static Site Generation : "$(date)
+# exit $TOME_SUCCESS
