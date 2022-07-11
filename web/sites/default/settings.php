2f55c6484606cca5842f7d44b8523badc8f20bc6
ednark <ednark@gmail.com>
2022-07-11 17:58:10 -0400

    scanning: limit http_post for s3

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -796,6 +796,26 @@ if (getenv('NEW_RELIC_API_KEY')) {
  * Collect external service information from environment.
  * Cloud Foundry places all service credentials in VCAP_SERVICES
  */
+$cf_application_data = json_decode($_ENV['VCAP_APPLICATION'] ?? '{}', TRUE);
+$SERVER_HTTP_HOST = $_SERVER['HTTP_HOST'];
+if (!empty($cf_application_data['space_name']) &&
+    in_array($cf_application_data['space_name'],
+             ['dev', 'stage', 'prod'])) {
+  switch (strtolower($cf_application_data['space_name'])) {
+    case "dev":
+      $SERVER_HTTP_HOST = 'https://cms-dev.usa.gov';
+      break;
+
+    case "stage":
+      $SERVER_HTTP_HOST = 'https://cms-stage.usa.gov';
+      break;
+
+    case "prod":
+      $SERVER_HTTP_HOST = 'https://cms.usa.gov';
+      break;
+  }
+}
+
 
 $cf_service_data = json_decode($_ENV['VCAP_SERVICES'] ?? '{}', TRUE);
 
@@ -829,7 +849,7 @@ foreach ($cf_service_data as $service_list) {
       $S3_PROXY_PATH_CMS = getenv('S3_PROXY_PATH_CMS') ?: '/s3/files';
 
       $config['s3fs.settings']['use_cname'] = TRUE;
-      $config['s3fs.settings']['domain'] = $_SERVER['HTTP_HOST'] . $S3_PROXY_PATH_CMS;
+      $config['s3fs.settings']['domain'] = $SERVER_HTTP_HOST . $S3_PROXY_PATH_CMS;
       $config['s3fs.settings']['domain_root'] = 'public';
 
       $config['s3fs.settings']['use_customhost'] = TRUE;
@@ -852,7 +872,6 @@ $settings['php_storage']['twig']['directory'] = '../storage/php';
 // included here without fully understanding implications:
 $settings['cache']['bins']['data'] = 'cache.backend.php';
 
-$cf_application_data = json_decode($_ENV['VCAP_APPLICATION'] ?? '{}', TRUE);
 if (!empty($cf_application_data['space_name']) &&
     in_array($cf_application_data['space_name'],
              ['dev', 'stage', 'prod'])) {
