a5c6814bbf896d68b7bf2799b77dc8606ef1dad6
Olawale Adekoya <olawaley2k@gmail.com>
2022-07-20 08:59:11 -0400

    USAGOV_317-Needs_Review_permission_not_working: ready for dev

--- web/modules/custom/usa_content_moderation_notifications/src/UsaContentModerationNotificationsPermissionChecker.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Drupal\usa_content_moderation_notifications;
-
-/**
- * Check if a certain permission exist for current user.
- */
-class UsaContentModerationNotificationsPermissionChecker {
-}
