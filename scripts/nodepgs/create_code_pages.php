d5b4c73761759025d6dd3273297dcd8d817042b0
Olawale Adekoya <olawaley2k@gmail.com>
2022-07-27 16:31:23 -0400

    USAGOV_73-Configure_error_pages-such_as_404s: update

--- scripts/nodepgs/create_code_pages.php
+++ scripts/nodepgs/create_code_pages.php
@@ -8,7 +8,13 @@ drush scr scripts/nodepgs/src/nodecreate.php "The System Is Down" "scripts/nodep
 drush scr scripts/nodepgs/src/nodecreate.php "Page Not Found" "scripts/nodepgs/codepages/404.txt" "basic_page"
 drush scr scripts/nodepgs/src/nodecreate.php "No se encontró la página" "scripts/nodepgs/codepages/404es.txt" "basic_page" "es"
 
-# mkdir -p $WEBDOC/scode/
-# wget https://localhost/page-not-found --no-check-certificate -O $WEBDOC/scode/page-not-found.html
-# wget https://localhost/system-down --no-check-certificats6-svc -r /var/run/s6/services/php/
-# s6-svc -r /var/run/s6/services/nginx/
+drush config:set system.site page.404 '/page-not-found' -y
+
+mkdir -p $WEBDOC/scode/
+wget https://localhost/page-not-found --no-check-certificate -O $WEBDOC/scode/page-not-found.html
+wget https://localhost/es/no-se-encontro-la-pagina --no-check-certificate -O $WEBDOC/scode/no-se-encontro-la-pagina.html
+wget https://localhost/system-down --no-check-certificate -O $WEBDOC/scode/system-down.html
+
+# Restart services
+s6-svc -r /var/run/s6/services/php/
+s6-svc -r /var/run/s6/services/nginx/
--- scripts/nodepgs/src/nodecreate.php
+++ scripts/nodepgs/src/nodecreate.php
@@ -11,76 +11,60 @@ global $argv;
 $title = $argv[3];
 $body = file_get_contents('../' . $argv[4]);
 $ctype = $argv[5];
-$lang = $argv[6] ?? 'none';
-// dpm($lang);
-dpm('here');
-dpm($argv);
+$lang = $argv[6] ?? 'en';
 
-// use Drupal\node\Entity\Node;
-// use Drupal\Core\Entity\RevisionLogInterface;
+use Drupal\node\Entity\Node;
+use Drupal\Core\Entity\RevisionLogInterface;
 
-// // Expect title as $args[3] and body as $args[4] and content type as $args[5].
-// if (!isset($argv[3]) || trim($argv[3]) == '' || !isset($argv[4]) || trim($argv[4]) == '' || !isset($argv[5]) || trim($argv[5]) == '') {
-//   print "Expecting three arguments on the command line, title, body and content type, for the new node\n";
-//   exit;
-// }
-// $entity_type = 'node';
-// $query = \Drupal::entityTypeManager()->getStorage($entity_type)->getQuery();
-// $query->condition('title', $title);
-// $nids = $query->execute();
-// $count = 0;
+// Expect title as $args[3] and body as $args[4] and content type as $args[5].
+if (!isset($argv[3]) || trim($argv[3]) == '' || !isset($argv[4]) || trim($argv[4]) == '' || !isset($argv[5]) || trim($argv[5]) == '') {
+  print "Expecting three arguments on the command line, title, body and content type, for the new node\n";
+  exit;
+}
+$entity_type = 'node';
+$query = \Drupal::entityTypeManager()->getStorage($entity_type)->getQuery();
+$query->condition('title', $title);
+$nids = $query->execute();
+$count = 0;
 
-// // Check for published node.
-// foreach ($nids as $nid) {
-//   $node = Node::load($nid);
-//   if ($node->isPublished()) {
-//     $pnode = $nid;
-//     $count++;
-//   }
-// }
+// Check for published node.
+foreach ($nids as $nid) {
+  $node = Node::load($nid);
+  if ($node->isPublished()) {
+    $pnode = $nid;
+    $count++;
+  }
+}
 
 // echo $count . PHP_EOL;
-// if ($count == 0) {
-//   if (empty($nids) == TRUE) {
-//     echo 'Create node object with attached file.' . PHP_EOL;
-//     $node = Node::create(['type' => $ctype]);
-//     $node->set('title', $title);
-//     $node->body->value = $body;
-//     $node->body->format = 'html';
-//     $node->set('moderation_state', "published");
-
-//     $node->uid = 1;
-//     $node->save();
-//     $message = 'create new ' . $node->id() . ' node from create code script';
-//     \Drupal::logger('create_code_script')->notice($message);
-//   }
-//   // If needed to publish latest revision
-//   // to-do : Compare the Date of the latest revision to Published date. if it is higher then, publish the latest revision.
-//   // else {
-//   // foreach ($nids as $nid) {
-//   // $node = Node::load($nid);
-//   // if ($node->isLatestRevision()) {
-//   // echo 'Update latest revision to publish' . PHP_EOL;
-//   // $node->body->value = $body;
-//   // $node->body->format = 'html';
-//   // $node->set('moderation_state', "published");
-//   // $node->save();
-//   // $message = 'Update latestRevision ' . $node->id() . ' node from create code script';
-//   // \Drupal::logger('create_code_script')->notice($message);
-//   // }
-//   // }
-//   // }
-// }
-// else {
-//   echo 'Update published node of the count id.' . PHP_EOL;
-//   foreach ($nids as $nid) {
-//     dpm($nid);
-//     $node = Node::load($nid);
-//     dpm(get_class_methods($node));
-//     $node->uid = 1;
-//     $node->set('moderation_state', "published");
-//     $node->save();
-//     $message = 'Update published ' . $node->id() . ' node from create code script';
-//     \Drupal::logger('create_code_script')->notice($message);
-//   }
-// }
+if ($count == 0) {
+  if (empty($nids) == TRUE) {
+    echo 'Create node object with attached file.' . PHP_EOL;
+    $node = Node::create(['type' => $ctype]);
+    $node->set('title', $title);
+    $node->body->value = $body;
+    $node->body->format = 'html';
+    $node->set('moderation_state', "published");
+    $node->set('langcode', $lang);
+    $node->uid = 1;
+    $node->save();
+    $message = 'create new ' . $node->id() . ' node from create code script';
+    \Drupal::logger('create_code_script')->notice($message);
+  }
+}
+else {
+  echo 'Update published node of the count id.' . PHP_EOL;
+  foreach ($nids as $nid) {
+    $node = Node::load($nid);
+    $node->uid = 1;
+    $node->set('moderation_state', "published");
+    $node->body->value = $body;
+    $node->body->format = 'html';
+    $node->set('langcode', $lang);
+    $node->save();
+    $message = 'Update published ' . $node->id() . ' node from create code script';
+    \Drupal::logger('create_code_script')->notice($message);
+  }
+  $nid = $node->id() ?? 'no nid';
+  echo 'this "' . $nid . ' - ' . $lang . '" node was created or updated' . PHP_EOL;
+}
