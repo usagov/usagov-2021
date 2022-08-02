5fe32e0ceba74a916e88906d22885283c5f4b14e
Sebastian Cabrejos <107482219+thetypicalprogrammer@users.noreply.github.com>
2022-08-02 15:18:13 -0400

    USAGOV-29-Twig-Changes: add select state js

---
+++ web/themes/custom/usagov/scripts/selectstate.js
@@ -0,0 +1,10 @@
+function stateSelect() {
+  let e = document.getElementById("state-info");
+  let value = e.value;
+  let text = e.options[e.selectedIndex].text;
+  console.log("PRINT BEGIN");
+    //   console.log(value, text);
+    //   console.log(value);
+    //   console.log(text);
+  console.log("END OF PRINT");
+}
