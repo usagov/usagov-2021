1ff905de2a7ee864451bbff774e99cfd4d948a9a
Sebastian Cabrejos <107482219+thetypicalprogrammer@users.noreply.github.com>
2022-08-03 14:41:29 -0400

    USAGOV-29-Twig-Changes: add working javascript for state select dropdownA

--- web/themes/custom/usagov/scripts/selectstate.js
+++ web/themes/custom/usagov/scripts/selectstate.js
@@ -1,13 +1,7 @@
 function stateSelect() {
   let e = document.getElementById("state-info");
   let value = e.value;
-//   let text = e.options[e.selectedIndex].text;
-
-  console.log("PRINT BEGIN");
-//   console.log(value, text);
-  console.log(value);
-//   console.log(text);
-  //   window.location.assign(window.location.href + "/" + text.toLowerCase());
-
-  console.log("END OF PRINT");
+  window.location.assign(
+    window.location.href + "/" + value.replace(/\s/g, "-").toLowerCase()
+  );
 }
