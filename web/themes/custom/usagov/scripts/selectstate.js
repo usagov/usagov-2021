fabccca1f9bc6bf3b0cfc32e509208ba6b9e2846
Sebastian Cabrejos <107482219+thetypicalprogrammer@users.noreply.github.com>
2022-08-03 14:04:42 -0400

    USAGOV-29-Twig-Changes: update scss, attempt to fix html

--- web/themes/custom/usagov/scripts/selectstate.js
+++ web/themes/custom/usagov/scripts/selectstate.js
@@ -1,12 +1,13 @@
 function stateSelect() {
-  //   let e = document.getElementById("state-info");
-  //   let value = e.value;
-  //   let text = e.options[e.selectedIndex].text;
+  let e = document.getElementById("state-info");
+  let value = e.value;
+//   let text = e.options[e.selectedIndex].text;
+
   console.log("PRINT BEGIN");
-  //   console.log(value, text);
-  //   console.log(value);
-  //   console.log(text);
+//   console.log(value, text);
+  console.log(value);
+//   console.log(text);
   //   window.location.assign(window.location.href + "/" + text.toLowerCase());
+
   console.log("END OF PRINT");
-  alert("TESTING TESTING");
 }
