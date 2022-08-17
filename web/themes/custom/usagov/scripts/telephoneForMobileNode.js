6cfe56fbbd38226a75385200256bf0498d042ae6
IsabelLaurenceau <isabel.laurenceau@gsa.gov>
2022-08-17 12:50:23 -0400

    USAGOV-394-Telephone-number-for-mobile: Working for mobile with new regex

--- web/themes/custom/usagov/scripts/telephoneForMobileNode.js
+++ web/themes/custom/usagov/scripts/telephoneForMobileNode.js
@@ -1,9 +1,42 @@
 jQuery(document).ready(function ($) {
+  console.log("being called");
+
   if (window.innerWidth <= 950) {
     $(".field--type-telephone").replaceWith(function () {
+      function relaceNoCountry(num) {
+        return num.replace(/(\d{3})(\d{3})(\d{4})/, "$1-$2-$3");
+      }
+
+      function replaceWithCountry(num) {
+        return num.replace(/(\d{1})(\d{3})(\d{3})(\d{4})/, "+$1 $2-$3-$4");
+      }
+      // strip all non-numerical numbers to only link the actual phone number
+      const numberAndDesc = $(this).text();
+      console.log("numberAndDesc " + numberAndDesc);
+      const onlyPhoneNumber = numberAndDesc.replace(/\D/g, "");
+      console.log("onlyPhoneNumber " + onlyPhoneNumber);
+      const onlyDesc = numberAndDesc.replace(/\d/g, "");
+      console.log("onlyDesc" + onlyDesc);
+      // if length is 10 there's no country code
+      const formattedPhoneNumber =
+        onlyPhoneNumber.length == 10
+          ? relaceNoCountry(onlyPhoneNumber)
+          : replaceWithCountry(onlyPhoneNumber);
+      console.log("formattedPhoneNumber " + formattedPhoneNumber);
       return $(
-        "<a href='tel:" + $(this).html() + "'>" + $(this).html() + "</a>"
+        "<p><a href='tel:" +
+          onlyPhoneNumber +
+          "'>" +
+          formattedPhoneNumber +
+          "</a> " +
+          onlyDesc +
+          "</p>"
       );
+    });
+  }
+  if (window.innerWidth > 950) {
+    $(".field--type-telephone").replaceWith(function () {
+      return $("<p> big </p>");
     });
   }
 });
