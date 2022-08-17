e49a1d04339151cd5e027b19c9b487296f2e3746
IsabelLaurenceau <isabel.laurenceau@gsa.gov>
2022-08-17 11:27:17 -0400

    USAGOV-278-footer-headings: Dynamically checks for window width

--- web/themes/custom/usagov/scripts/newFooter.js
+++ web/themes/custom/usagov/scripts/newFooter.js
@@ -1,40 +1,44 @@
-(function ($) {
-  console.log("Hello world from js!");
-  console.log(window.innerWidth);
-  // setting isMobile with same chack that telephone numbers use to set to links
-  var isMobile = window.innerWidth <= 950 ? true : false;
-  console.log(isMobile);
-  var primaryLinks = $(".usa-gov-footer-heading");
-  // console.log(primaryLinks);
+window.addEventListener("resize", checkForMobile);
+window.addEventListener("load", checkForMobile);
 
-  var newElementType = isMobile ? "button" : "h3";
+function checkForMobile() {
+  (function ($) {
+    console.log("Hello world from js!");
 
-  $(".usa-gov-footer-heading").each(function (currentElement) {
-    console.log($(this));
-    console.log($(this).next());
+    console.log(window.innerWidth);
+    // setting isMobile with same chack that telephone numbers use to set to links
+    var isMobile = window.innerWidth <= 950 ? true : false;
+    console.log(isMobile);
 
-    var newElement = document.createElement(newElementType);
+    var newElementType = isMobile ? "button" : "h3";
 
-    // hardcoded because we know what it is
-    newElement.setAttribute("class", "usa-gov-footer-heading");
+    $(".usa-gov-footer-heading").each(function (currentElement) {
+      console.log($(this));
+      console.log($(this).next());
 
-    newElement.classList.toggle("usa-gov-footer-heading--button", isMobile);
+      var newElement = document.createElement(newElementType);
 
-    newElement.innerText = $(this).text();
+      // hardcoded because we know what it is
+      newElement.setAttribute("class", "usa-gov-footer-heading");
 
-    console.log(newElement);
-    console.log("created new element");
-    if (isMobile) {
-      var menuId = "usa-footer-menu-list-".concat(
-        Math.floor(Math.random() * 100000)
-      );
-      newElement.setAttribute("aria-controls", menuId);
-      newElement.setAttribute("aria-expanded", "false");
-      $(this).next().attr("id", menuId);
-      newElement.setAttribute("type", "button");
-    }
+      newElement.classList.toggle("usa-gov-footer-heading--button", isMobile);
 
-    $(this).after(newElement);
-    $(this).remove();
-  });
-})(jQuery);
+      newElement.innerText = $(this).text();
+
+      console.log(newElement);
+      console.log("created new element");
+      if (isMobile) {
+        var menuId = "usa-footer-menu-list-".concat(
+          Math.floor(Math.random() * 100000)
+        );
+        newElement.setAttribute("aria-controls", menuId);
+        newElement.setAttribute("aria-expanded", "false");
+        $(this).next().attr("id", menuId);
+        newElement.setAttribute("type", "button");
+      }
+
+      $(this).after(newElement);
+      $(this).remove();
+    });
+  })(jQuery);
+}
