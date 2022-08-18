a2ad84c151241ad6095fbda05620e72d95454ef9
IsabelLaurenceau <isabel.laurenceau@gsa.gov>
2022-08-18 09:45:32 -0400

    USAGOV-278-footer-headings: CSS (minus color) correct for desktop not for mobile

--- web/themes/custom/usagov/scripts/newFooter.js
+++ web/themes/custom/usagov/scripts/newFooter.js
@@ -7,21 +7,25 @@ function checkForMobile() {
 
     console.log(window.innerWidth);
     // setting isMobile with same chack that telephone numbers use to set to links
+    // beta uses 500
     var isMobile = window.innerWidth <= 950 ? true : false;
     console.log(isMobile);
 
     var newElementType = isMobile ? "button" : "h3";
 
-    $(".usa-gov-footer-heading").each(function (currentElement) {
+    $(".usa-gov-footer__primary-link").each(function (currentElement) {
       console.log($(this));
       console.log($(this).next());
 
       var newElement = document.createElement(newElementType);
 
       // hardcoded because we know what it is
-      newElement.setAttribute("class", "usa-gov-footer-heading");
+      newElement.setAttribute("class", "usa-gov-footer__primary-link");
 
-      newElement.classList.toggle("usa-gov-footer-heading--button", isMobile);
+      newElement.classList.toggle(
+        "usa-gov-footer__primary-link--button",
+        isMobile
+      );
 
       newElement.innerText = $(this).text();
 
