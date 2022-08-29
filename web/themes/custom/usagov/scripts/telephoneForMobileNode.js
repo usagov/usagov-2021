52e29686eb5b731b7c6697e30f411997327f37ea
IsabelLaurenceau <isabel.laurenceau@gsa.gov>
2022-08-29 10:59:01 -0400

    USAGOV-394-Telephone-number-for-mobile: moved into it's own function for re-use

--- web/themes/custom/usagov/scripts/telephoneForMobileNode.js
+++ web/themes/custom/usagov/scripts/telephoneForMobileNode.js
@@ -1,22 +1,24 @@
 console.log("in the og");
+
+function reformatNumForMobile(toReformat) {
+  //get phone number and description from innerText
+  const numAndDesc = toReformat.innerText;
+  console.log("numAndDec: " + numAndDesc);
+  const numberSplitter = numAndDesc.split(" ");
+  console.log("numberSplitter: " + numberSplitter);
+  const onlyNum = numberSplitter[0];
+  const cleanNumber = onlyNum.replace(/\D/g, "");
+  console.log("cleanNumber: " + cleanNumber);
+  const onlyDesc = numberSplitter.slice(1, numberSplitter.length).join(" ");
+  console.log("onlyDesc: " + onlyDesc);
+  return `<a href="tel: ${cleanNumber}"> ${onlyNum} </a> ${onlyDesc} `;
+}
+
 if (window.innerWidth <= 480) {
   const telephoneNumbers = document.querySelectorAll(".phoneNumberField");
   for (let i = 0; i < telephoneNumbers.length; i++) {
-    //get phone number and description from innerText
-    const numAndDesc = telephoneNumbers[i].innerText;
-    console.log("numAndDec: " + numAndDesc);
-    const numberSplitter = numAndDesc.split(" ");
-    console.log("numberSplitter: " + numberSplitter);
-    const onlyNum = numberSplitter[0];
-    const cleanNumber = onlyNum.replace(/\D/g, "");
-    console.log("cleanNumber: " + cleanNumber);
-    const onlyDesc = numberSplitter.slice(1, numberSplitter.length).join(" ");
-    console.log("onlyDesc: " + onlyDesc);
-
     // replace innerHTML with format
-    telephoneNumbers[
-      i
-    ].innerHTML = `<a href="tel: ${cleanNumber}"> ${onlyNum} </a> ${onlyDesc} `;
+    telephoneNumbers[i].innerHTML = reformatNumForMobile(telephoneNumbers[i]);
   }
 } else {
   const telephoneNumbers = document.querySelectorAll(".phoneNumberField");
