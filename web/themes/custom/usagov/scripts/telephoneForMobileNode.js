ec82dfafe868feb105c6c68f472d55971a511887
IsabelLaurenceau <isabel.laurenceau@gsa.gov>
2022-08-29 11:34:56 -0400

    USAGOV-394-Telephone-number-for-mobile: Working still want to clean/dry it up

--- web/themes/custom/usagov/scripts/telephoneForMobileNode.js
+++ web/themes/custom/usagov/scripts/telephoneForMobileNode.js
@@ -3,14 +3,10 @@ console.log("in the og");
 function reformatNumForMobile(toReformat) {
   //get phone number and description from innerText
   const numAndDesc = toReformat.innerText;
-  console.log("numAndDec: " + numAndDesc);
   const numberSplitter = numAndDesc.split(" ");
-  console.log("numberSplitter: " + numberSplitter);
   const onlyNum = numberSplitter[0];
   const cleanNumber = onlyNum.replace(/\D/g, "");
-  console.log("cleanNumber: " + cleanNumber);
   const onlyDesc = numberSplitter.slice(1, numberSplitter.length).join(" ");
-  console.log("onlyDesc: " + onlyDesc);
   return `<a href="tel: ${cleanNumber}"> ${onlyNum} </a> ${onlyDesc} `;
 }
 
@@ -18,16 +14,12 @@ if (window.innerWidth <= 480) {
   // for /agency-index
   const telephoneNumbers = document.querySelectorAll(".phoneNumberField");
   for (let i = 0; i < telephoneNumbers.length; i++) {
-    // replace innerHTML with format
     telephoneNumbers[i].innerHTML = reformatNumForMobile(telephoneNumbers[i]);
   }
 
   //for agency pages
   const telly = document.querySelector(".field--type-telephone");
-  // console.log("check for telly: " + telly.innerHTML);
   for (let i = 0; i < telly.children.length; i++) {
-    // replace innerHTML with format
-    // console.log("telly: " + telly.children[i].innerText);
     telly.children[i].innerHTML = reformatNumForMobile(telly.children[i]);
   }
 } else {
@@ -35,17 +27,11 @@ if (window.innerWidth <= 480) {
   for (let i = 0; i < telephoneNumbers.length; i++) {
     //get phone number and description from innerText
     const numAndDesc = telephoneNumbers[i].innerText;
-    console.log("numAndDec: " + numAndDesc);
-
-    // replace innerHTML with format
     telephoneNumbers[i].innerHTML = `<p> ${numAndDesc} </p> `;
   }
   //for agency pages
   const telly = document.querySelector(".field--type-telephone");
-  console.log("check for telly: " + telly.innerHTML);
   for (let i = 0; i < telly.children.length; i++) {
-    // replace innerHTML with format
-    console.log("telly: " + telly.children[i].innerText);
     const numAndDesc = telly.children[i].innerText;
     telly.children[i].innerHTML = `<p> ${numAndDesc} </p> `;
   }
