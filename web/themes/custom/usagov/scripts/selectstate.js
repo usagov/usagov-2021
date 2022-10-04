2a7042b22c80abdae056a7cb1fcea01918936044
Amy Farrell <amy.farrell@gsa.gov>
2022-10-03 19:11:02 -0700

    USAGOV-515-open-redirect-in-selectstate: Calculate the path for the submitted state, eliminating an open redirect.

--- web/themes/custom/usagov/scripts/selectstate.js
+++ web/themes/custom/usagov/scripts/selectstate.js
@@ -1,7 +1,10 @@
 function stateSelect() {
   let e = document.getElementById("state-info");
   let value = e.value;
+  let enEspanol = window.location.pathname.substr(0,4) == '/es/';
+  let statePath = enEspanol ? '/es/estados/' : '/states/';
   window.location.assign(
-    window.location.href + "/" + value.replace(/\s/g, "-").toLowerCase()
+    window.location.origin + statePath + value.replace(/\s/g, "-").toLowerCase()
   );
 }
+
