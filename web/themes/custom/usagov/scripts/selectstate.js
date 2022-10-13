3923f0e850dc0d83282364bf950d56e819525ccc
Amy Farrell <amy.farrell@gsa.gov>
2022-10-13 16:52:09 -0700

    USAGOV-515-open-redirect-in-selectstate: Use a less hacky method of getting the selected state.

--- web/themes/custom/usagov/scripts/selectstate.js
+++ web/themes/custom/usagov/scripts/selectstate.js
@@ -1,10 +1,9 @@
 function stateSelect() {
-  let e = document.getElementById("state-info");
-  let value = e.value;
-  let enEspanol = window.location.pathname.substr(0,4) == '/es/';
-  let statePath = enEspanol ? '/es/estados/' : '/states/';
-  window.location.assign(
-    window.location.origin + statePath + value.replace(/\s/g, "-").toLowerCase()
-  );
+    let stateForm = document.getElementById("stateForm");
+    let stateData = new FormData(stateForm);
+    let stateValue = stateData.get('state-info');
+    let enEspanol = window.location.pathname.substr(0,4) == '/es/';
+    let statePath = enEspanol ? '/es/estados/' : '/states/';
+    window.location.assign(window.location.origin + statePath + stateValue);
 }
 
