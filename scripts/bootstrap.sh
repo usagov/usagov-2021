ca8d303e0c533dc257c59577b00cfed6f0c59890
ednark <ednark@gmail.com>
2022-12-06 13:50:50 -0500

    USAGOV-388-run-tome-after-deploy: make sure ts file is readable

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -8,7 +8,9 @@ if [ -z "${VCAP_SERVICES:-}" ]; then
 fi
 
 if [ ! -f /container_start_timestamp ]; then
-    echo "$(date +'%s')" > /container_start_timestamp;
+  touch /container_start_timestamp
+  chmod a+r /container_start_timestamp
+  echo "$(date +'%s')" > /container_start_timestamp
 fi
 
 SECRETS=$(echo $VCAP_SERVICES | jq -r '.["user-provided"][] | select(.name == "secrets") | .credentials')
