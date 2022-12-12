0e24c502b5134f1d54032db6f0d252774d187580
Chris Wachtman <cwachtman@gmail.com>
2022-12-12 14:01:55 -0500

    USAGOV-608-mobile-nav-less-flashing: Apply to all links inside our mobile menu

--- web/themes/custom/usagov/scripts/modifyMobileNav.js
+++ web/themes/custom/usagov/scripts/modifyMobileNav.js
@@ -2,8 +2,8 @@
     // USWDS adds a handler (on the body element) that closes the menu when a nav link
     // is clicked. We don't want this when the user clicks to another page, so we
     // "stop propagation" on "regular" mobile nav links.
-    let nav_link_selector = '.usa-nav .navigation__item a';  
-    let current_page_selector = '.usa-nav .navigation__item.active>a';
+    let nav_link_selector = '.usagov-mobile-menu a';  
+    let current_page_selector = '.usagov-mobile-menu .navigation__item.active>a';
     document.querySelectorAll(nav_link_selector)
 	.forEach(function(mobileNavLink) {
 	    if (!mobileNavLink.matches(current_page_selector)) { 
