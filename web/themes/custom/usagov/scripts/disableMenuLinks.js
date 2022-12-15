39fc91b55fd97b6fe728ac698eee47ae0df97bb9
IsabelLaurenceau <isabel.laurenceau@gsa.gov>
2022-12-15 08:23:26 -0500

    USAGOV-611-menu-disable-links: replace only href

--- web/themes/custom/usagov/scripts/disableMenuLinks.js
+++ web/themes/custom/usagov/scripts/disableMenuLinks.js
@@ -1,13 +1,10 @@
 let path = window.location.pathname;
 
 let menu_paths = [
-  "/",
   "/about-the-us",
   "/money",
   "/laws-and-legal-issues",
   "/scams-and-fraud",
-  "/es/",
-  "/es",
   "/es/acerca-de-estados-unidos",
   "/es/dinero",
   "/es/leyes-y-asuntos-legales",
@@ -15,39 +12,26 @@ let menu_paths = [
 ];
 
 if (menu_paths.includes(path)) {
+  let listItem;
   if (path == "/about-the-us") {
-    let listItem = document.getElementById("usa-nav__about");
-    listItem.innerHTML =
-      "<a href='#skip-to-h1' class='usa-nav__link'><span>About the U.S. and its government</span></a>";
+    listItem = document.getElementById("usa-nav__about");
   } else if (path == "/money") {
-    let listItem = document.getElementById("usa-nav__money");
-    listItem.innerHTML =
-      "<a href='#skip-to-h1' class='usa-nav__link'><span>Money</span></a>";
+    listItem = document.getElementById("usa-nav__money");
   } else if (path == "/laws-and-legal-issues") {
-    let listItem = document.getElementById("usa-nav__law");
-    listItem.innerHTML =
-      "<a href='#skip-to-h1' class='usa-nav__link'><span>Laws and legal issues</span></a>";
+    listItem = document.getElementById("usa-nav__law");
   } else if (path == "/scams-and-fraud") {
-    let listItem = document.getElementById("usa-nav__scams");
-    listItem.innerHTML =
-      "<a href='#skip-to-h1' class='usa-nav__link'><span>Scams and fraud</span></a>";
+    listItem = document.getElementById("usa-nav__scams");
   } else if (path == "/es/acerca-de-estados-unidos") {
-    let listItem = document.getElementById("usa-nav__acerca");
-    listItem.innerHTML =
-      "<a href='#skip-to-h1' class='usa-nav__link'><span> Acerca de EE. UU. y directorios del Gobierno</span></a>";
+    listItem = document.getElementById("usa-nav__acerca");
   } else if (path == "/es/dinero") {
-    let listItem = document.getElementById("usa-nav__dinero");
-    listItem.innerHTML =
-      "<a href='#skip-to-h1' class='usa-nav__link'><span>Dinero</span></a>";
+    listItem = document.getElementById("usa-nav__dinero");
   } else if (path == "/es/leyes-y-asuntos-legales") {
-    let listItem = document.getElementById("usa-nav__leyes");
-    listItem.innerHTML =
-      "<a href='#skip-to-h1' class='usa-nav__link'><span>Leyes y asuntos legales</span></a>";
+    listItem = document.getElementById("usa-nav__leyes");
   } else if (path == "/es/estafas-y-fraudes") {
-    let listItem = document.getElementById("usa-nav__estafas");
-    listItem.innerHTML =
-      "<a href='#skip-to-h1' class='usa-nav__link'><span>Estafas y fraudes</span></a>";
+    listItem = document.getElementById("usa-nav__estafas");
   } else {
     //console.log(`Should never hit me`);
   }
+  let aElem = listItem.getElementsByTagName("a")[0];
+  aElem.setAttribute("href", "#skip-to-h1");
 }
