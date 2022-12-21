9d43fdceea358b261a8db9dbda6152e6144ffb1e
IsabelLaurenceau <isabel.laurenceau@gsa.gov>
2022-12-21 09:26:48 -0500

    USAGOV-624: one extra check

--- web/themes/custom/usagov/scripts/wizard.js
+++ web/themes/custom/usagov/scripts/wizard.js
@@ -6,23 +6,25 @@ if (submitButton != null) {
 
 function wizardError() {
   let choices = document.getElementsByName("options");
-  for (let choice = 0; choice < choices.length; choice++) {
-    let selected = choices[choice].checked;
-    if (selected == true) {
-      document.getElementById("msg").innerHTML = "";
-      document.getElementById("msg").removeAttribute("tabindex", "-1");
-      dataLayer.push({ event: "Wizard_Begin" });
-      return true;
-    } else if (
-      document.getElementsByTagName("html")[0].getAttribute("lang") == "en"
-    ) {
-      document.getElementById("msg").innerHTML =
-        "Error:Please choose one of the following options";
-      document.getElementById("msg").focus();
-    } else {
-      document.getElementById("msg").innerHTML =
-        "Error:Por favor elija una opción";
-      document.getElementById("msg").focus();
+  if (choices) {
+    for (let choice = 0; choice < choices.length; choice++) {
+      let selected = choices[choice].checked;
+      if (selected == true) {
+        document.getElementById("msg").innerHTML = "";
+        document.getElementById("msg").removeAttribute("tabindex", "-1");
+        dataLayer.push({ event: "Wizard_Begin" });
+        return true;
+      } else if (
+        document.getElementsByTagName("html")[0].getAttribute("lang") == "en"
+      ) {
+        document.getElementById("msg").innerHTML =
+          "Error:Please choose one of the following options";
+        document.getElementById("msg").focus();
+      } else {
+        document.getElementById("msg").innerHTML =
+          "Error:Por favor elija una opción";
+        document.getElementById("msg").focus();
+      }
     }
   }
   dataLayer.push({ event: "Wizard_Error", button: "Begin" });
