b988a3a9a19f12a6f8ceb6d8c522424a34170490
Amy Farrell <amy.farrell@gsa.gov>
2023-01-20 17:13:31 -0800

    USAGOV-673-block-domain: WAF: shell script syntax

--- .docker/src-waf/etc/nginx/dynamic/deny_domain_by_ip.sh
+++ .docker/src-waf/etc/nginx/dynamic/deny_domain_by_ip.sh
@@ -2,7 +2,7 @@
 # Get IP addresses for domains in domains-deny.list. 
 # If the results differ from deny-by-domain.conf, update that file.
 # If there were changes AND the flag --no_reload was not passed, reload nginx.
-# (--no_reload is only wanted during setup, before nginx has started.)
+# (--no-reload is only wanted during setup, before nginx has started.)
 
 BASEDIR=$(dirname $0)
 
@@ -13,16 +13,16 @@ do
   if [[ ! -z $ddns_record ]]; then
      resolved_ip=`getent ahosts $line | awk '{ print $1 ; exit }'`
      if [[ ! -z $resolved_ip ]]; then
-         echo "   deny $resolved_ip; # from $ddns_record" > ${BASEDIR}/deny-by-domain_new.conf
+         echo "   deny $resolved_ip; # from $ddns_record" >> ${BASEDIR}/deny-by-domain_new.conf
      fi
   fi
 done < ${BASEDIR}/domains-deny.list
 
 # Update deny-by-domain.conf only if there are changes. 
-CHANGES=`diff ${BASEDIR}/deny-by-domain.conf ${BASEDIR}/deny-by-domain_new.conf`
-if [[ ! -z $CHANGES ]]; then
+CHANGES=$(diff ${BASEDIR}/deny-by-domain.conf ${BASEDIR}/deny-by-domain_new.conf)
+if [[ ! -z "$CHANGES" ]]; then
     cat ${BASEDIR}/deny-by-domain_new.conf > ${BASEDIR}/deny-by-domain.conf
-    if [[ $1 != '--no_reload' ]]; then
+    if [ "$1" != "--no-reload" ]; then
 	/usr/sbin/nginx -s reload	
     fi
 fi
