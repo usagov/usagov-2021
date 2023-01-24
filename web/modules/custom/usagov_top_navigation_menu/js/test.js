dea4c49a66fa2e6878aef32145b7f142ef822a74
IsabelLaurenceau <isabel.laurenceau@gsa.gov>
2023-01-24 15:27:43 -0500

    USAGOV-686-Menu-Flexibility: checking path is in the menu

--- web/modules/custom/usagov_top_navigation_menu/js/test.js
+++ web/modules/custom/usagov_top_navigation_menu/js/test.js
@@ -4,9 +4,23 @@
    */
   Drupal.behaviors.mymoduleAccessData = {
     attach: function (context) {
-      var data = drupalSettings.mymoduleComputedData.title;
-      console.log("HELLO FROM JS");
-      console.log(data);
+      var top_nav = drupalSettings.top_nav_menu;
+      console.log(top_nav);
+      let dicts = {};
+      for (menu_item in top_nav) {
+        var menu_path = (Object.keys(top_nav[menu_item])[0]).toString();
+        var menu_id = Object.values(top_nav[menu_item])[0];
+        dicts[menu_path] = menu_id;
+      }
+
+      let current_path = window.location.pathname;
+      console.log(`HELLO FROM JS ${current_path}`);
+
+      if (current_path in dicts) {
+        console.log("YUP");
+      } else {
+        console.log("NOPE");
+      }
     },
   };
 })(jQuery, Drupal, drupalSettings);
