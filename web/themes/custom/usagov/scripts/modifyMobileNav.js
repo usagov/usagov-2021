9b00176fbd4046ba3c067d5c62385ab2197c41d8
Chris Wachtman <cwachtman@gmail.com>
2023-01-27 13:49:47 -0500

    USAGOV-382-Mobile-Navigation: Accessibility fixes for mobile menu

--- web/themes/custom/usagov/scripts/modifyMobileNav.js
+++ web/themes/custom/usagov/scripts/modifyMobileNav.js
@@ -1,7 +1,9 @@
+
 (function disableMobileNavToggle() {
     // USWDS adds a handler (on the body element) that closes the menu when a nav link
     // is clicked. We don't want this when the user clicks to another page, so we
     // "stop propagation" on "regular" mobile nav links.
+
     let nav_link_selector = '.usagov-mobile-menu a';  
     let current_page_selector = '.usagov-mobile-menu .navigation__item.active>a';
     document.querySelectorAll(nav_link_selector)
@@ -13,3 +15,23 @@
 	    }
 	});
 })();
+
+
+(function menuButtonModifications() {
+	// USWDS applies aria-hidden attribute to non-nav elements but misses the usa-navbar element.
+	// As a result, screen reader users can navigate out of the mobile menu while it is open.
+	// To prevent this, we are applying aria-hidden to the usa-navbar ourselves when the menu is opened.
+	// We also set the focus to the home link because that is the first focusable element in our mobile menu.
+
+	let menu_button = document.querySelector('.usa-menu-btn');
+	let navbar = document.querySelector('.usa-navbar');
+	let home_link = document.querySelector('#mobile-menu-home-link');
+	menu_button.addEventListener("click", function (e) {
+		navbar.setAttribute('aria-hidden', 'true');
+		navbar.setAttribute('data-nav-hidden', 'true');
+		setTimeout(() => {
+			home_link.focus();
+		}, 100);
+	});
+})();
+
