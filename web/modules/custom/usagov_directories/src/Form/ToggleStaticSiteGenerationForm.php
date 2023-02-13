6a850d75244908bb55e4ec0a906932c170696317
arpage <straypacket@gmail.com>
2023-02-13 10:40:51 -0500

    usagov-690_admin-ui: Minimize UI controls to a single button w/ explanatory text

--- web/modules/custom/usagov_directories/src/Form/ToggleStaticSiteGenerationForm.php
+++ web/modules/custom/usagov_directories/src/Form/ToggleStaticSiteGenerationForm.php
@@ -23,22 +23,31 @@ class ToggleStaticSiteGenerationForm extends FormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
- 
+
+    $toggle_state = \Drupal::state()->get(get_static_state_var()) ? 'Enable' : 'Disable';
+
+    $desc_text = $this->t(\Drupal::state()->get(get_static_state_var()) ?  
+      "Static Site Generation is currently DISABLED. Note: Disabling will not cancel a Tome run that is already in progress." :
+      "Static Site Generation is currently ENABLED."
+    );
+
     $form['description'] = [
       '#type' => 'processed_text',
-      '#text' => $this->t('Are you sure you want to toggle Tome site generation (this will not cancel a Tome run that is already in progress) ?'),
+      //'#text' => $this->t('Are you sure you want to toggle Tome site generation (this will not cancel a Tome run that is already in progress) ?'),
+      '#text' => $desc_text,
     ];
 
-    $toggle_state = \Drupal::state()->get(get_static_state_var()) ? TRUE : FALSE;
-    $form[get_static_state_button_name()] = [
+
+    /*$form[get_static_state_button_name()] = [
       '#type' => 'checkbox',
       '#title' => $this->t('Check this box to ENABLE Static Site Generation.  Uncheck to DISABLE.'),
       '#default_value' => $toggle_state,
-    ];
+    ];*/
+
     $form['actions']['#type'] = 'actions';
     $form['actions']['submit'] = [
       '#type' => 'submit',
-      '#value' => $this->t('Toggle Static Site Generation'),
+      '#value' => $this->t( '@able Static Site Generation',['@able'=>$toggle_state]),
       '#button_type' => 'primary',
     ];
     return $form;
@@ -47,23 +56,16 @@ class ToggleStaticSiteGenerationForm extends FormBase {
   /**
    * {@inheritdoc}
    */
-  /*public function validateForm(array &$form, FormStateInterface $form_state) {
-  }*/
-
-  /**
-   * {@inheritdoc}
-   */
   public function submitForm(array &$form, FormStateInterface $form_state) {
 
     $errors = FALSE;
-    $toggle_state = $form_state->getValue(get_static_state_button_name());
 
     try {
+      $toggle_state = \Drupal::state()->get(get_static_state_var()) ? FALSE : TRUE;
+      //$toggle_state = $form_state->getValue(get_static_state_button_name()) ? TRUE : FALSE;     
       if ( $toggle_state ) {
-        $this->messenger()->addStatus($this->t("Toggling Tome ON."));
         \Drupal::state()->set(get_static_state_var(),TRUE);
       } else {
-        $this->messenger()->addStatus($this->t("Toggling Tome OFF."));
         \Drupal::state()->delete(get_static_state_var());
       }
     }
@@ -76,8 +78,6 @@ class ToggleStaticSiteGenerationForm extends FormBase {
     if ($errors) {
       $this->messenger()->addError("Something went wrong. See the error log for details.");
     }
-
-    $this->messenger()->addStatus($this->t("Toggled Tome. I hope you're happy with yourself."));
   }
 
 }
