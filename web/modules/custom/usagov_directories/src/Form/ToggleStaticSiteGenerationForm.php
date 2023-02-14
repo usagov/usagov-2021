ca51606f96304b91d351f3eef217e9e4a6b875ca
arpage <straypacket@gmail.com>
2023-02-14 15:56:30 -0500

    usagov-690_admin-ui: USAGOV-690 - fix linting issues

--- web/modules/custom/usagov_directories/src/Form/ToggleStaticSiteGenerationForm.php
+++ web/modules/custom/usagov_directories/src/Form/ToggleStaticSiteGenerationForm.php
@@ -16,7 +16,7 @@ class ToggleStaticSiteGenerationForm extends FormBase {
    * {@inheritdoc}
    */
   public function getFormId() {
-    return get_static_state_form_id();
+    return usagov_directories_get_static_state_form_id();
   }
 
   /**
@@ -24,9 +24,9 @@ class ToggleStaticSiteGenerationForm extends FormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
 
-    $toggle_state = \Drupal::state()->get(get_static_state_var()) ? 'Enable' : 'Disable';
+    $toggle_state = \Drupal::state()->get(usagov_directories_get_static_state_var()) ? 'Enable' : 'Disable';
 
-    $desc_text = $this->t(\Drupal::state()->get(get_static_state_var()) ?  
+    $desc_text = $this->t(\Drupal::state()->get(usagov_directories_get_static_state_var()) ?  
       "Static Site Generation is currently DISABLED. Note: Disabling will not cancel a Tome run that is already in progress." :
       "Static Site Generation is currently ENABLED."
     );
@@ -37,8 +37,7 @@ class ToggleStaticSiteGenerationForm extends FormBase {
       '#text' => $desc_text,
     ];
 
-
-    /*$form[get_static_state_button_name()] = [
+    /*$form[usagov_directories_get_static_state_button_name()] = [
       '#type' => 'checkbox',
       '#title' => $this->t('Check this box to ENABLE Static Site Generation.  Uncheck to DISABLE.'),
       '#default_value' => $toggle_state,
@@ -61,12 +60,12 @@ class ToggleStaticSiteGenerationForm extends FormBase {
     $errors = FALSE;
 
     try {
-      $toggle_state = \Drupal::state()->get(get_static_state_var()) ? FALSE : TRUE;
-      //$toggle_state = $form_state->getValue(get_static_state_button_name()) ? TRUE : FALSE;     
+      $toggle_state = \Drupal::state()->get(usagov_directories_get_static_state_var()) ? FALSE : TRUE;
+      //$toggle_state = $form_state->getValue(usagov_directories_get_static_state_button_name()) ? TRUE : FALSE;     
       if ( $toggle_state ) {
-        \Drupal::state()->set(get_static_state_var(),TRUE);
+        \Drupal::state()->set(usagov_directories_get_static_state_var(),TRUE);
       } else {
-        \Drupal::state()->delete(get_static_state_var());
+        \Drupal::state()->delete(usagov_directories_get_static_state_var());
       }
     }
     catch (\Exception $e) {
