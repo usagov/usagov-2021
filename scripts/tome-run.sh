b05bc4be80e7b1ff0e5b8750b963796e90554dd1
Jacob Yeager <jacobaaronyeager@gmail.com>
2023-03-21 18:30:27 -0400

    USAGOV-56-integrate-analytics-app: removing errant comment

--- scripts/tome-run.sh
+++ scripts/tome-run.sh
@@ -75,7 +75,6 @@ if [ "$CONTENT_UPDATED" != "0" ] || [[ "$FORCE" =~ ^\-{0,2}f\(orce\)?$ ]] || [ "
   $SCRIPT_PATH/tome-static.sh $URI 2>&1 | tee -a $TOMELOG
   TOME_SUCCESS=$?
   if [ "$TOME_SUCCESS" == "0" ]; then
-    # Use a unique dir for each run - just in case more than one of this is running
     $SCRIPT_PATH/tome-sync.sh $TOMELOGFILE $YMDHMS $FORCE
   else
     echo "Tome static build failed with status $TOME_SUCCESS - not pushing to S3" | tee -a $TOMELOG
