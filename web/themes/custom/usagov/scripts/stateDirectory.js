c959e2c08089edc5179e9f8da972737720824138
developer43 <bryant.jones@gsa.gov>
2023-03-24 07:36:01 -0400

    usagov-28-50-state: usagov-28-50-state-code-update

--- web/themes/custom/usagov/scripts/stateDirectory.js
+++ web/themes/custom/usagov/scripts/stateDirectory.js
@@ -1,23 +1,23 @@
 jQuery(document).ready(function ($) {
-    $('#statelist').after('<div class="custom-select"><select id="stateselect" name="stateselect"></div>');
+    $('#statelist').after('<label class="visuallyhidden">Select your state:<select class="usa-select usa-sr-only usa-combo-box__select" name="state-info" id="stateselect" aria-hidden="true" tabindex="-1"></select></label>');
     $('#statelist li a').each(function(){
       $('#stateselect').append('<option value="'+$(this).attr('href')+'">'+$(this).text()+'</option>');
     });
     var b;
     if($('html').attr('lang')=="en"){
-      $('#statelist').after('<header><h2><label for="stateselect">Find your state or territory:</label></h2></header>');
-      b=$('<button id="statego">Go</button>');
+     // $('#test').after('<header><h2><label for="stateselect">Find your state or territory:</label></h2></header>');
+      b=$('<button id="statego" class="usa-button sd-go-btn" type="submit">Go</button>');
     }else{
-      $('#statelist').after('<header><h2><label for="stateselect">Encuentre su estado o territorio:</label></h2></header>');
-      b=$('<button id="statego">Ir</button>');
+     // $('#statelist').after('<header><h2><label for="stateselect">Encuentre su estado o territorio:</label></h2></header>');
+      b=$('<button id="statego" class="usa-button sd-go-btn" type="submit">Ir</button>');
     }
     $('#statelist').remove();
-  
+   
     var url=$('#stateselect').val();
     b.click(function(){
       window.location.href = url;
     });
-    $('#stateselect').parent().after(b);
+    $('#test').after(b);
     $('#stateselect').on('change', function(){
       url=$(this).val();
     });
