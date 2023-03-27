3d351f60c3f70bdcf64e695f74660fbe3ff1028c
developer43 <bryant.jones@gsa.gov>
2023-03-27 09:49:35 -0400

    usagov-28-50-state: usagov-28-50-state-code-update

--- web/themes/custom/usagov/scripts/stateDirectory.js
+++ web/themes/custom/usagov/scripts/stateDirectory.js
@@ -3,6 +3,8 @@ jQuery(document).ready(function ($) {
     $('#statelist li a').each(function(){
       $('#stateselect').append('<option value="'+$(this).attr('href')+'">'+$(this).text()+'</option>');
     });
+    $('#state-info').after('<span class="usa-combo-box__clear-input__wrapper" tabindex="-1"><button type="button" class="usa-combo-box__clear-input" aria-label="Clear the select contents">&nbsp;</button></span><span class="usa-combo-box__input-button-separator">&nbsp;</span><span class="usa-combo-box__toggle-list__wrapper" tabindex="-1"><button type="button" tabindex="0" class="usa-combo-box__toggle-list" aria-label="Toggle the dropdown list">&nbsp;</button></span><ul tabindex="-1" id="state-info--list" class="usa-combo-box__list" role="listbox" aria-labelledby="state-info-label" hidden=""></ul><div class="usa-combo-box__status usa-sr-only" role="status"></div><span id="state-info--assistiveHint" class="usa-sr-only">When autocomplete results are available use up and down arrows to review and enter to select.Touch device users, explore by touch or with swipe gestures.</span>');
+    
     var b;
     if($('html').attr('lang')=="en"){
      // $('#test').after('<header><h2><label for="stateselect">Find your state or territory:</label></h2></header>');
