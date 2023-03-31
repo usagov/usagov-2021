26b1c498cf49b928853be5886f2f1a8349cc92ad
developer43 <bryant.jones@gsa.gov>
2023-03-31 10:52:37 -0400

    usagov-28-50-state: usagov-28-50-state

--- web/themes/custom/usagov/scripts/stateDirectory.js
+++ web/themes/custom/usagov/scripts/stateDirectory.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function ($) {
-    $('#statelist').after('<label class="visuallyhidden">Select your state:<select class="usa-select usa-sr-only usa-combo-box__select" name="state-info" id="stateselect" aria-hidden="true" tabindex="-1"></select></label>');
+    $('#statelist').after('<label class="visuallyhidden">Select your state:<select class="usa-select usa-sr-only usa-combo-box__select" name="state-info" id="stateselect" aria-hidden="true" tabindex="-1" required></select></label>');
     $('#statelist li a').each(function(){
       $('#stateselect').append('<option value="'+$(this).attr('href')+'">'+$(this).text()+'</option>');
     });
@@ -15,6 +15,7 @@ jQuery(document).ready(function ($) {
     $('#statelist').remove();
 
     var url=$('#stateselect').val();
+    $('input[name="Alabama"]').val('Alabama');
     b.click(function(){
       window.location.href = url;
     });
@@ -28,6 +29,5 @@ jQuery(document).ready(function ($) {
         '50_state_name': statename
       });
     });
-    
   });
   
