d1e6e29d3dd4c53b2e55fc88f1e8b8756cbe4c3c
Amy Farrell <amy.farrell@gsa.gov>
2023-04-06 09:51:30 -0700

    USAGOV-872-no-es-redirect: make this branch deployable to dev

--- web/modules/custom/usagov_header_hooks/src/EventSubscriber/PagerPathSubscriber.php
+++ web/modules/custom/usagov_header_hooks/src/EventSubscriber/PagerPathSubscriber.php
@@ -6,13 +6,32 @@ use Drupal\tome_static\Event\ModifyHtmlEvent;
 use Drupal\tome_static\Event\TomeStaticEvents;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
+
 /**
  * Converts links to "/es" to "/es/".
  * Based on the tome_static PagerEventSubscriber.
  *
  * @internal
  */
+
 class PagerPathSubscriber implements EventSubscriberInterface {
+
+  /**
+   * Reacts to a modify destination event. This converts "/es/" to "/es", because
+   * Drupal will always redirect a path with a trailing / to its equivalent without,
+   * and we do not want to create a redirect at /es/index.html!
+   * This catches cases where text content includes links to "/es/".
+   * Content based on entity references will never have the trailing /, which is
+   * why we have to add it in an EventSubscriber.
+   *
+   * @param \Drupal\tome_static\Event\ModifyDestinationEvent $event
+   *   The event.
+   */
+  public function modifyDestination(ModifyDestinationEvent $event) {
+    $destination = $event->getDestination();
+    $destination = $this->modifyUrl($destination);
+    $event->setDestination($destination);
+  }
 
   /**
    * Reacts to a modify HTML event.
@@ -51,9 +70,29 @@ class PagerPathSubscriber implements EventSubscriberInterface {
   }
 
   /**
+   * Modifies a URL to replace "/es/" with "/es".
+   *
+   * @param string $url
+   *   A URL.
+   *
+   * @return string
+   *   The modified URL.
+   */
+  protected function modifyUrl($url) {
+    $url_parts = parse_url($url);
+    // TODO: Also check that this URL is on-site
+    if ($url_parts['path'] == '/es/') {
+      $url_parts['path'] = '/es';
+      $url = unparse_url($url_parts);
+    }
+    return $url;
+  }
+
+  /**
    * {@inheritdoc}
    */
   public static function getSubscribedEvents() {
+    $events[TomeStaticEvents::MODIFY_DESTINATION][] = ['modifyDestination'];
     $events[TomeStaticEvents::MODIFY_HTML][] = ['modifyHtml'];
     return $events;
   }
