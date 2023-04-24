d4f49317dd6d97d7ce1ce147881b474fea062286
IsabelLaurenceau <isabel.laurenceau@gsa.gov>
2023-04-24 14:10:20 -0400

    USAGOV-911-report-a-problem: Create reportAProblem.js

---
+++ web/themes/custom/usagov/scripts/reportAProblem.js
@@ -0,0 +1,85 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+  (function ($,jQuery) {
+    $(document).ready(function () {
+      function validateEmail(email) {
+        var re = /\S+@\S+\.\S+/; //This is a very simple regex for email
+        return re.test(email);
+      }
+      $(".validate").submit(function () {
+        var noerrors = true;
+        $(".required").each(function () {
+          var input = $(this).find("input,textarea");
+          if (
+            input.val() == "" ||
+            (input.attr("id") == "email" && !validateEmail(input.val()))
+          ) {
+            noerrors = false;
+            if (!$(this).find("p.err-label").length) {
+              var error = input.attr("data-error");
+              var errorId = "error_" + input.attr("id");
+              var label = $(this).find("label");
+              label.after(
+                '<p id="' +
+                  errorId +
+                  '" class="err-label" tabindex="0">' +
+                  error +
+                  "</p>"
+              );
+              input.attr("aria-labelledby", label.attr("id") + " " + errorId);
+            }
+          } else if ($(this).find("p.err-label").length) {
+            $(this).find("p.err-label").remove();
+            //input.removeAttribute('aria-labelledby');
+          }
+        });
+        if (!noerrors) {
+          var elem = document.querySelector(".err-label");
+          elem.focus();
+          var viewportOffset = elem.getBoundingClientRect();
+          var top = viewportOffset.top;
+          if (top < 108) {
+            window.scrollTo(0, window.pageYOffset - (108 - top));
+          }
+        }
+        return noerrors;
+      });
+    });
+  })(jQuery);
+<script src="https://www.google.com/recaptcha/api.js"></script>
+  function timestamp() {
+    var response = document.getElementById("g-recaptcha-response");
+    if (response == null || response.value.trim() == "") {
+      var elems = JSON.parse(
+        document.getElementsByName("captcha_settings")[0].value
+      );
+      elems["ts"] = JSON.stringify(new Date().getTime());
+      document.getElementsByName("captcha_settings")[0].value =
+        JSON.stringify(elems);
+    }
+  }
+  setInterval(timestamp, 500);
+  var submitPressed = function () {
+    if (grecaptcha.getResponse().length === 0) {
+      if ($(".err-label-captcha").length < 1) {
+        $(".g-recaptcha").before(
+          '<p class="err-label err-label-captcha" tabindex="0">Please fill out the reCaptcha</p>'
+        );
+      }
+      return false;
+    } else {
+      $(".err-label-captcha").remove();
+    }
+    return true;
+  };
+<script type="text/javascript">
+  jQuery(document).ready(function () {
+    $("#cntctbx").hide();
+  });
+</script>
+<script type="text/javascript">
+  jQuery(document).ready(function () {
+    $("#pagesurvey-hdr").hide();
+    $("#pagesurvey-trgt").hide();
+    $("#pagesurvey-ombnum").hide();
+  });
+</script>
