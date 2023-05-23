ada90918cad75b74b60d4cc8569471c88aa94ff2
IsabelLaurenceau <isabel.laurenceau@gsa.gov>
2023-05-23 07:39:03 -0400

    USAGOV-24-phone-numbers-: Update phoneForMobileinBody.js

--- web/themes/custom/usagov/scripts/phoneForMobileinBody.js
+++ web/themes/custom/usagov/scripts/phoneForMobileinBody.js
@@ -1,4 +1,4 @@
 var pattern = /[0-9]?[-. ]\(?([0-9]{3})\)?[-. ]([0-9]{3})[-. ]([0-9]{4})/g;
 if (window.innerWidth <= 480) {
-  document.body.innerHTML = document.body.innerHTML.replace(pattern, '<a href="tel: $&"> $& </a>');
+  document.body.innerHTML = document.body.innerHTML.replace(pattern, '<a href="tel: $&">$&</a>');
 }
