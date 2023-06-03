31359616495e30b408543663abaa9f548f807c77
arpage <straypacket@gmail.com>
2023-06-03 19:43:45 -0400

    USAGOV-1011-backup-pre-post-ambles: USAGOV-1011 - add 'try tome disable' script, tries every 15 seconds up to 25 minutes (can pass argument for max wait min)

--- scripts/try-tome-disable.sh
+++ scripts/try-tome-disable.sh
@@ -1,44 +1,64 @@
 #!/bin/sh
 
-#SCRIPT_NAME=tome-run.sh
-SCRIPT_NAME=ssh-agent
+source /etc/profile
+
+maxWaitMinutes=$1
+if [ x$maxWaitMinutes = x ]; then
+    maxWaitMinutes=25
+else
+    fail=0
+
+    case $maxWaitMinutes in *[!0123456789]*|0?*|"")
+        fail=1
+    esac
+
+    if [ $maxWaitMinutes -gt 30 ]; then
+        fail=1
+    fi
+
+    if [ $fail -ne 0 ]; then
+        echo
+        echo Usage $0 [maxWaitMinutes]
+        echo Where maxWaitMinutes must be an integer less than 30, or empty.
+        echo maxWaitMinuts defaults to 25
+        exit 1
+    fi
+fi
+
+SCRIPT_NAME=tome-run.sh
+#SCRIPT_NAME=just-sit-here.sh
 
 function isCmdRunning() {
     scriptName=$1
-    sleepInterval=$2
-    attemptCount=$3
-    if [ x$attemptCount != x ]; then
-        attemptCount="($attemptCount): "
-    fi
+
     PS_AUX=$(ps aux)
     ALREADY_RUNNING=$(echo "$PS_AUX" | grep $scriptName | wc -l)
     if [ "$ALREADY_RUNNING" -gt "0" ]; then
-        echo "$attemptCount$scriptName x $ALREADY_RUNNING.  Sleeping $sleepInterval"
-        sleep $sleepInterval
         return 0
     fi
     return 1
 }
 
-i=1
-until [ $i -gt 8 ]; do
-    case $i in
-    1) sleepTime=5 ;;
-    2) sleepTime=7 ;;
-    3) sleepTime=180 ;;
-    4) sleepTime=5 ;;
-    5) sleepTime=7 ;;
-    6) sleepTime=1200 ;;
-    7) sleepTime=5 ;;
-    8) sleepTime=7 ;;
-    esac
+startSeconds=$(date +'%s')  ### seconds since epoch
+diffSeconds=0
+diffMinutes=0
 
-    if ! isCmdRunning $SCRIPT_NAME $sleepTime $i; then
+sleepTime=15
+
+until [ $diffMinutes -gt $maxWaitMinutes ]; do
+    if ! isCmdRunning $SCRIPT_NAME; then
        echo "Running: /var/www/scripts/tome-disabled-toggle.sh 1"
-       /var/www/scripts/tome-disabled-toggle.sh 1
-       exit
+       #####/var/www/scripts/tome-disabled-toggle.sh 1
+       exit 0
+    else
+        echo "$SCRIPT_NAME is already running.  Sleeping $sleepTime (waited ${diffSeconds}s / ${diffMinutes}m of ${maxWaitMinutes}m)"
+        sleep $sleepTime
     fi
-    ((i++))
+    i=$((i+1))
+
+    currSeconds=$(date +'%s')
+    diffSeconds=$((currSeconds - startSeconds))
+    diffMinutes=$((diffSeconds / 60))
 done
 
 exit 1
