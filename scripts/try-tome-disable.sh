0d9214f6f4f8e1a1ef5cb3b6d9d1d324d2cd5e4e
arpage <straypacket@gmail.com>
2023-06-03 20:09:12 -0400

    USAGOV-1011-backup-pre-post-ambles: USAGOV-1011 - add dryrun and maintenance mode options

--- scripts/try-tome-disable.sh
+++ scripts/try-tome-disable.sh
@@ -2,11 +2,24 @@
 
 source /etc/profile
 
+# just testing?
+if [ x$1 == x"--dryrun" ]; then
+  export echo=echo
+  shift
+fi
+
 maxWaitMinutes=$1
+if [ x$maxWaitMinutes != x ]; then
+   shift
+fi
+
+setMaintMode=$1
+
+fail=0
+
 if [ x$maxWaitMinutes = x ]; then
     maxWaitMinutes=25
 else
-    fail=0
 
     case $maxWaitMinutes in *[!0123456789]*|0?*|"")
         fail=1
@@ -16,13 +29,24 @@ else
         fail=1
     fi
 
-    if [ $fail -ne 0 ]; then
-        echo
-        echo Usage $0 [maxWaitMinutes]
-        echo Where maxWaitMinutes must be an integer less than 30, or empty.
-        echo maxWaitMinutes defaults to 25
-        exit 1
+fi
+
+if [ x$setMaintMode = x ]; then
+   setMaintMode=0
+else
+   if [ $setMaintMode != "maintenance" ]; then
+      fail=1
     fi
+fi
+
+if [ $fail -ne 0 ]; then
+    echo
+    echo Usage $0 [maxWaitMinutes] [maintenance]
+    echo "Where maxWaitMinutes must be an integer less than 30, or empty (maxWaitMinutes defaults to 25)".
+    echo and
+    echo where maintenance is "maintenance" or empty.  If maintenance is set, the Drupal maintenance mode will be enabled.
+    echo
+    exit 1
 fi
 
 SCRIPT_NAME=tome-run.sh
@@ -48,7 +72,12 @@ sleepTime=15
 until [ $diffMinutes -gt $maxWaitMinutes ]; do
     if ! isCmdRunning $SCRIPT_NAME; then
        echo "Running: /var/www/scripts/tome-disabled-toggle.sh 1"
-       #####/var/www/scripts/tome-disabled-toggle.sh 1
+       $echo /var/www/scripts/tome-disabled-toggle.sh 1
+
+       if [ x$setMaintMode = x"maintenance" ]; then
+       echo "Running: /var/www/scripts/toggle-maintenance-mode.sh 1"
+       $echo /var/www/scripts/toggle-maintenance-mode.sh 1
+       fi
        exit 0
     else
         echo "$SCRIPT_NAME is already running.  Sleeping $sleepTime (waited ${diffSeconds}s / ${diffMinutes}m of ${maxWaitMinutes}m)"
