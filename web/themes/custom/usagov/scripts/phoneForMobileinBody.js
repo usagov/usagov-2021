e246f67b236691dac0aee48e372cb1630b71c686
IsabelLaurenceau <isabel.laurenceau@gsa.gov>
2023-07-26 10:00:41 -0400

    USAGOV-1070: linter

--- web/themes/custom/usagov/scripts/phoneForMobileinBody.js
+++ web/themes/custom/usagov/scripts/phoneForMobileinBody.js
@@ -3,7 +3,7 @@ var safariCheck = /<a href="tel:[0-9]?[-. ]\(?([0-9]{3})\)?[-. ]([0-9]{3})[-. ](
 
 if (window.innerWidth <= 480) {
   console.log(document.body.innerHTML.search(safariCheck));
-  if (document.body.innerHTML.search(safariCheck) == -1) {
+  if (document.body.innerHTML.search(safariCheck) === -1) {
     document.body.innerHTML = document.body.innerHTML.replace(pattern, '<a href="tel:$&">$&</a>');
   }
 }
