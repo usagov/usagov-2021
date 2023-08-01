ebe692699520e15efcd1b4380dfee4b7f6a00e64
Jacob Yeager <jacobaaronyeager@gmail.com>
2023-08-01 16:25:42 -0400

    USAGOV-1029-Manual-Updates: restructuring?????

--- .docker/src-reporter/analytics-reporter/src/google-analytics/client.js
+++ .docker/src-reporter/analytics-reporter/src/google-analytics/client.js
@@ -15,15 +15,11 @@ const fetchData = async (report) => {
 
 const _executeFetchDataRequest = async (query, { realtime }) => {
   return new Promise((resolve, reject) => {
-    _get(query, realtime)(query, (err, data) => {
-      if (err) {
-        reject(err)
-      } else {
-        resolve(data)
-      }
-    })
-  })
-}
+    _get(query, realtime) // Pass both query and realtime here
+      .then((data) => resolve(data))
+      .catch((err) => reject(err));
+  });
+};
 
 const _get = async (query, realtime) => {
   const analytics = google.analytics("v3")
