d923d220f1aa735456b99813e39c6762fb0f0255
Jacob Yeager <jacobaaronyeager@gmail.com>
2023-08-03 12:12:03 -0400

    USAGOV-1029-Manual-Updates: trying to see more data

--- .docker/src-reporter/analytics-reporter/src/analytics.js
+++ .docker/src-reporter/analytics-reporter/src/analytics.js
@@ -9,6 +9,7 @@ const query = (report) => {
   }
 
   return GoogleAnalyticsClient.fetchData(report).then(data => {
+    winston.debug(`[${report.name}] data:`, data)
     return GoogleAnalyticsDataProcessor.processData(report, data)
   })
 }
