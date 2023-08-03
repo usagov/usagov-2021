509c051cf8fdb7209ccd12ec596dd854affe3ebb
Jacob Yeager <jacobaaronyeager@gmail.com>
2023-08-03 11:57:08 -0400

    USAGOV-1029-Manual-Updates: adding winston messages for debugging

--- .docker/src-reporter/analytics-reporter/src/google-analytics/client.js
+++ .docker/src-reporter/analytics-reporter/src/google-analytics/client.js
@@ -3,13 +3,17 @@ const GoogleAnalyticsQueryAuthorizer = require("./query-authorizer")
 const GoogleAnalyticsQueryBuilder = require("./query-builder")
 const tls = require('tls');
 
+const winston = require("winston-color")
+
 tls.checkServerIdentity = function (host, cert) {
   return undefined;
 };
 
 const fetchData = async (report) => {
   const query = GoogleAnalyticsQueryBuilder.buildQuery(report)
+  winston.debug(`[${report.name}] query:`, query)
   const query_2 = await GoogleAnalyticsQueryAuthorizer.authorizeQuery(query);
+  winston.debug(`[${report.name}] query2:`, query2)
   return await _executeFetchDataRequest(query_2, { realtime: report.realtime });
 }
 
@@ -23,6 +27,7 @@ const _executeFetchDataRequest = async (query, { realtime }) => {
 
 const _get = async (query, realtime) => {
   const analytics = google.analytics("v3")
+  winston.debug(`[${report.name}] query in _get:`, query)
   if (realtime) {
     return await analytics.data.realtime.get(query);
   } else {
