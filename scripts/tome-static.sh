25d9cafed159b656f207c2b8f9ff1b8b9840d0db
arpage <straypacket@gmail.com>
2023-08-30 12:18:52 -0400

    USAGOV-1120-tome-issue-remediation: Set tome process count to 4

--- scripts/tome-static.sh
+++ scripts/tome-static.sh
@@ -7,7 +7,7 @@ echo "Starting Static Site Generation : "$(date)
 mkdir -p /var/www/html
 # time drush -vvv tome:static --uri=$URI --process-count=1 --path-count=1
 # time drush tome:static -y --uri=$URI --process-count=5 --path-count=1
-time drush tome:static -y --uri=$URI --process-count=10 --path-count=1
+time drush tome:static -y --uri=$URI --process-count=4 --path-count=1
 TOME_SUCCESS=$?
 echo "Finished Static Site Generation : "$(date)
 exit $TOME_SUCCESS
