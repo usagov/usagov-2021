71a6205bdbb5badf52437b0880bc5a86310a2084
Jacob Yeager <jacobaaronyeager@gmail.com>
2023-09-19 13:55:44 -0400

    USAGOV-1123-expand-orphaned-entities: Finishing touches. Setting page still shows machine names, but I can't figure it out

--- web/modules/custom/usa_orphaned_entities/src/Form/OrphanedEntitiesSettings.php
+++ web/modules/custom/usa_orphaned_entities/src/Form/OrphanedEntitiesSettings.php
@@ -6,7 +6,6 @@ use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 
 class OrphanedEntitiesSettings extends ConfigFormBase {
-
   /**
    * {@inheritdoc}
    */
@@ -18,21 +17,21 @@ class OrphanedEntitiesSettings extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    // Form constructor.
-    $form = parent::buildForm($form, $form_state);
-    // Default settings.
-    $config = $this->config('orphaned_entities.settings');
+    $entityTypeManager = \Drupal::service('entity_type.manager');
 
+    $form['#attached']['library'][] = 'usa_orphaned_entities/orphaned';
+    $form = parent::buildForm($form, $form_state);
+    $config = $this->config('orphaned_entities.settings');
     $form['entity_reference'] = [];
 
-    $entity_references = \Drupal::service('entity_type.manager')->getStorage('field_storage_config')->loadByProperties([
+    $entity_references = $entityTypeManager->getStorage('field_storage_config')->loadByProperties([
       'type' => 'entity_reference',
     ]);
 
     $form['markup'] = [
       '#type' => 'item',
-      '#title' => $this->t('Choose how orphaned nodes should be handled by each content type.'),
-      '#description' => $this->t('Select for each field, when a node is deleted, will the entity reference be cleared from any referencing nodes of that type and the associated node be saved or will the referencing nodes be deleted.'),
+      '#title' => $this->t('Choose how orphaned entities should be handled by each content type.'),
+      '#description' => $this->t('Select for each field, when an entity is deleted, will the entities that reference the deleted entity using that field have their reference field cleared and the entity saved, be deleted, or ignored. Selection refers to what happens to the entity <i>with that field</i>.'),
       '#attributes' => [
         'class' => [
           'markup',
@@ -53,21 +52,21 @@ class OrphanedEntitiesSettings extends ConfigFormBase {
 
     // Generate form.
     foreach ($reference_field_map as $bundle => $fields) {
-      //TODO: work out how to get bundle label from here.
+      //@todo work out how to get bundle label from here.
       $form[$bundle] = [
         '#type' => 'fieldset',
-        '#title' => $this->t($bundle),
+        '#title' => $bundle,
       ];
       foreach ($fields as $reference_field) {
         $form[$bundle][$bundle . '_' . $reference_field] = [
           '#type' => 'radios',
-          '#description' => $this->t($reference_field),
+          '#title' => $reference_field,
           '#options' => [
             'clear' => $this->t('Clear reference'),
             'delete' => $this->t('Delete node'),
             'skip' => $this->t('Do nothing'),
           ],
-          '#default_value' => 'clear',
+          '#default_value' =>  ($config->get('orphaned_entities.' . $bundle . '_' . $reference_field)) ?: 'skip',
         ];
       }
     }
@@ -78,7 +77,19 @@ class OrphanedEntitiesSettings extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
+    foreach ($form['entity_reference'] as $index => $entity_reference) {
+      $reference_field = $entity_reference->getName();
+      if (!str_contains($index, '#')) {
+        $bundles = $entity_reference->getBundles();
 
+        foreach ($bundles as $index => $bundle) {
+          if (empty($form_state->getValue($bundle . '_' . $reference_field)) ||
+            $form_state->getValue($bundle . '_' . $reference_field) == NULL) {
+            $form_state->setErrorByName($bundle . '_' . $reference_field, $this->t('Field option must be set.'));
+          }
+        }
+      }
+    }
   }
 
   /**
@@ -87,15 +98,18 @@ class OrphanedEntitiesSettings extends ConfigFormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $config = $this->config('orphaned_entities.settings');
     foreach ($form['entity_reference'] as $index => $entity_reference) {
+      $reference_field = $entity_reference->getName();
+
       if (!str_contains($index, '#')) {
         $bundles = $entity_reference->getBundles();
+
         foreach ($bundles as $index => $bundle) {
-          $reference_field = $entity_reference->getName();
-          $config->set('orphaned_entities.' . $bundle . '_' . $reference_field, $form_state->getValue($bundle . '_' . $reference_field));
+          $config->set(
+              'orphaned_entities.' . $bundle . '_' . $reference_field, $form_state->getValue($bundle . '_' . $reference_field));
         }
       }
     }
-    kint($config);
+
     $config->save();
     return parent::submitForm($form, $form_state);
   }
