530efca364b193adeac364225d205834f2d0d55f
IsabelLaurenceau <isabel.laurenceau@gsa.gov>
2023-11-01 10:12:14 -0400

    USAGOV-1119-Phone-Link-Footer: Only search the main element not the header or footer

--- web/themes/custom/usagov/scripts/phoneForMobile.js
+++ web/themes/custom/usagov/scripts/phoneForMobile.js
@@ -3,14 +3,6 @@ var safariCheck = /<a href="tel:[0-9]?[-. ]\(?([0-9]{3})\)?[-. ]([0-9]{3})[-. ](
 
 if (window.innerWidth <= 480) {
   if (document.body.innerHTML.search(safariCheck) === -1) {
-    document.body.innerHTML = document.body.innerHTML.replace(pattern, '<a href="tel:$&">$&</a>');
-
-    var footerPattern = /<ahref="\/phone">1-844-USAGOV1\(<\/a><ahref="tel:1-844-872-4681">1-844-872-4681<\/a>\)/g;
-    var footerPhoneWithSpaces = document.getElementsByClassName("usa-footer__contact-info")[0].innerHTML;
-    var footerPhoneNoSpaces = footerPhoneWithSpaces.replace(/\s/g, "");
-
-    if (footerPhoneNoSpaces.match(footerPattern)) {
-      document.getElementsByClassName("usa-footer__contact-info")[0].innerHTML = '<a href="/phone"> 1-844-USAGOV1 (1-844-872-4681) </a>';
-    }
+    document.body.getElementsByTagName("main")[0].innerHTML = document.body.getElementsByTagName("main")[0].innerHTML.replace(pattern, '<a href="tel:$&">$&</a>');
   }
 }
