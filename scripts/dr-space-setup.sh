a288e6375649c139cc75ebde554b392447507dfc
msvgsa <mark.vitek@gsa.gov>
2023-11-10 18:22:02 -0500

    USAGOV-1083-temp-domain-names: Adjustments for dr space name, dr database init.

--- scripts/dr-space-setup.sh
+++ scripts/dr-space-setup.sh
@@ -16,9 +16,9 @@ CTAG=cms-7149
 CDIGEST=@sha256:d5bb5c7cb4f28643e66dc62be24faa4915190780f52befed3d9cd41c8f3d415d
 WTAG=waf-7149
 WDIGEST=@sha256:6141cbd8010762842da90468b03780732843879230ee05ccfa0870c392e76afc
-
+ 
 echo  cf delete-space $APP_SPACE
-$echo cf delete-space $APP_SPACE
+while [ 1 = 1 ]; do clear; $echo cf delete-space $APP_SPACE; sleep 10; done
 exit
  
 echo  cf delete-space $EGRESS_SPACE
@@ -45,29 +45,27 @@ echo cf target -s $EGRESS_SPACE
 $echo cf target -s $EGRESS_SPACE
 echo cf create-service s3 basic-sandbox key-value  PIPE tee cskv.log
 $echo cf create-service s3 basic-sandbox key-value  | tee cskv.log
-exit
+#exit
  
 echo cf target -s $APP_SPACE
 $echo cf target -s $APP_SPACE
 echo  bin/cloudgov/create-service-account PIPE tee csa.log
 $echo bin/cloudgov/create-service-account | tee csa.log
-exit
+#exit
  
 echo  cf target -s $APP_SPACE
 $echo cf target -s $APP_SPACE
 echo  cf delete-service ${APP_SPACE}-usagov-domain
 $echo cf delete-service ${APP_SPACE}-usagov-domain
-exit
+#exit
  
 echo  cf target -s $APP_SPACE
 $echo cf target -s $APP_SPACE
 echo  cf create-service external-domain domain ${APP_SPACE}-usagov-domain -c '{"domains": "dev-dr.usa.gov,shared-egress-dr.usa.gov"}'
 $echo cf create-service external-domain domain ${APP_SPACE}-usagov-domain -c '{"domains": "dev-dr.usa.gov,shared-egress-dr.usa.gov"}'
-#echo  cf service ${APP_SPACE}-usagov-domain
-#$echo cf service ${APP_SPACE}-usagov-domain
 while [ 1 = 1 ]; do clear; cf service ${APP_SPACE}-usagov-domain; sleep 10; done
 exit
- 
+
 echo  cf target -s $APP_SPACE
 $echo cf target -s $APP_SPACE
 echo  bin/cloudgov/create-service-account PIPE tee csa2.log
@@ -96,3 +94,11 @@ DB_HOST=$(echo $VCAP_SERVICES | jq -r '.["aws-rds"][] | .credentials.host')
 DB_PORT=$(echo $VCAP_SERVICES | jq -r '.["aws-rds"][] | .credentials.port')
 mysql --protocol=TCP -h$DB_HOST -P$DB_PORT -u$DB_USER -p$DB_PW $DB_NAME
 ZZ
+
+#PREFIX=usagov-${DEPLOY_TAG}-${SPACE}
+#SQL_FILE=usagov.sql
+# 
+#echo "Attempting to deploy database backup $SQL_FILE to $APP_SPACE"
+#
+#$echo cat $SQL_FILE | cf ssh cms -c "cat - > /tmp/$SQL_FILE"
+#cf ssh $CMS_APP -c "if [ -f /tmp/$SQL_FILE ]; then . /etc/profile; drush sql-drop -y; cat /tmp/$SQL_FILE | drush sql-cli; drush cr; fi"
