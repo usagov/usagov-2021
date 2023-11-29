8983c1f97f809e052ad399b03ca03169dec87541
msvgsa <mark.vitek@gsa.gov>
2023-11-29 16:09:50 -0500

    USAGOV-1216-tome-indicator: First pass

--- scripts/tome-status-indicator-update.sh
+++ scripts/tome-status-indicator-update.sh
@@ -1,13 +1,40 @@
 #!/bin/sh
 
-IND_FILE=/var/www/web/static-site-status.txt
+#PREFIX=/var/www
+PREFIX=.
+IND_FILE=${PREFIX}/web/static-site-status.txt
 
-if [ $ARGV < 2 ]; then
-   exit 1
+#if [ ! -f "$IND_FILE" ]; then
+   echo "
+   <ul>
+   <li>Static Site Generator, status as of %timestamp%: %status%</li>
+   </ul>" > $IND_FILE
+#fi
+
+TR_START_TIME=$1
+if [ -n "$TR_START_TIME" ]; then
+    shift
 fi
 
-OP=$1
-shift
+STATUS=$1
+if [ -n "$STATUS" ]; then
+    shift
+fi
 
-TS=$1
-shift
+if [ -n "$TR_START_TIME" -a -n "$STATUS" ]; then
+
+   sed -i "s|%status%|$STATUS|" $IND_FILE
+   
+   OP_START_TIME=$1
+   if [ -n "$OP_START_TIME" ]; then
+       shift
+   fi
+
+   OP_FAIL_TIME=$1
+   if [ -n "$OP_FAIL_TIME" ]; then
+      shift
+   fi
+else
+   echo "To few args"
+   exit 1
+fi
