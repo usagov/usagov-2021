7057e720fd46925e974bc2ba2b7f4f3bf8571410
Amy Farrell <amy.farrell@gsa.gov>
2023-12-11 19:05:49 -0800

    USAGOV-1236-tome-check-for-wrong-mobile-nav: Check for Spanish nav on English home page in Tome output
    Cancels publish, removes English home page, and sets the flag to re-run Tome regardless of content changes

--- scripts/tome-sync.sh
+++ scripts/tome-sync.sh
@@ -225,6 +225,14 @@ if [ "$ES_HOME_CONTAINS_ENGLISH_MENU" != "0"  ]; then
   ES_HOME_HTML_BAD=1
 fi
 
+# Sometimes Tome generates an Spanish mobile menu on the English home page
+# "Navegaci&oacute;n" is the aria-label on the mobile nav (and the non-mobile nav, but we don't see this problem there)
+EN_HOME_CONTAINS_SPANISH_MENU=`grep -c 'Navegaci&oacute;n' $EN_HOME_HTML_FILE`
+if [ "$EN_HOME_CONTAINS_ENGLISH_MENU" != "0"  ]; then
+  echo "WARNING: *** EN index.html appears to contain Spanish nav ***" | tee -a $TOMELOG
+  EN_HOME_HTML_BAD=1
+fi
+
 # Check for the correct type of cards on the home page. (We can remove these checks if we don't
 # see this problem through, oh, 2023-07-30.)
 EN_HOME_CORRECT_CARDS=`grep -c 'homepage-card' $EN_HOME_HTML_FILE`
@@ -248,6 +256,13 @@ fi
 if [ "$ES_HOME_HTML_BAD" == "1" ]; then
   # Delete the known-bad file; it may be re-created correctly on the next run.
   rm $ES_HOME_HTML_FILE
+  touch $RETRY_SEMAPHORE_FILE
+  TOME_PUSH_NEW_CONTENT=0
+fi
+
+if [ "$EN_HOME_HTML_BAD" == "1" ]; then
+  # Delete the known-bad file; it may be re-created correctly on the next run.
+  rm $EN_HOME_HTML_FILE
   touch $RETRY_SEMAPHORE_FILE
   TOME_PUSH_NEW_CONTENT=0
 fi
