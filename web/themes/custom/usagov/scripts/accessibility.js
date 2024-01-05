c59557abeb4098ae76d68141a7e53e1fe005dac0
Yaritza M. Garcia Chaparro <yaritza.garcia@gsa.gov>
2024-01-05 10:13:21 -0400

    USAGOV-1299-USPS-credentials: Remove duplicated code

--- web/themes/custom/usagov/scripts/accessibility.js
+++ web/themes/custom/usagov/scripts/accessibility.js
@@ -100,10 +100,7 @@ async function addressUSPSValidation(streetAddress, city, state, zipCode) {
     }
 
     try {
-        const response = await fetch(`https://secure.shippingapis.com/ShippingAPI.dll?API=Verify \
-        &XML=<AddressValidateRequest USERID="${USERID}" PASSWORD="${PASSWORD}"><Address><Address1>\
-        </Address1><Address2>${streetAddress}</Address2><City>${city}</City><State>${state}\
-        </State><Zip5>${zipCode}</Zip5><Zip4></Zip4></Address></AddressValidateRequest>`);
+        const response = await fetch(url);
 
         if (!response.ok) {
             throw new Error('Network response was not ok');
