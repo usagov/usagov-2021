89d15a962a4471466e93c9552533fb857ec0b43c
Yaritza M. Garcia Chaparro <yaritza.garcia@gsa.gov>
2024-01-08 16:11:28 -0400

    USAGOV-1299-USPS-credentials: Credentials added during build time. Removed unnecessary code.

--- web/themes/custom/usagov/scripts/accessibility.js
+++ web/themes/custom/usagov/scripts/accessibility.js
@@ -88,7 +88,7 @@ const state_codes = {
 
 async function addressUSPSValidation(streetAddress, city, state, zipCode) {
     const url = `https://secure.shippingapis.com/ShippingAPI.dll?API=Verify \
-    &XML=<AddressValidateRequest USERID="${USERID}" PASSWORD="${PASSWORD}"><Address><Address1>\
+    &XML=<AddressValidateRequest USERID="${USPS_USERID}" PASSWORD="${USPS_USERID}"><Address><Address1>\
     </Address1><Address2>${streetAddress}</Address2><City>${city}</City><State>${state}\
     </State><Zip5>${zipCode}</Zip5><Zip4></Zip4></Address></AddressValidateRequest>`;
 
