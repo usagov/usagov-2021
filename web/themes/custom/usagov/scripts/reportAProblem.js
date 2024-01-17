c3f1c06f2e9601dd6a7351a0bd582cc553b0a67a
Yaritza M. Garcia Chaparro <yaritza.garcia@gsa.gov>
2024-01-17 16:19:39 -0400

    USAGOV-1296-Errors-Report-issue-page: Error Alert box, done for both, spanish and english

--- web/themes/custom/usagov/scripts/reportAProblem.js
+++ web/themes/custom/usagov/scripts/reportAProblem.js
@@ -1,14 +1,19 @@
 (function ($, jQuery) {
   "use strict";
+
   $(document).ready(function () {
     function validateEmail(email) {
       var re = /\S+@\S+\.\S+/; // This is a very simple regex for email
       return re.test(email);
     }
+
     $(".validate").submit(function () {
       var noerrors = true;
       $(".required").each(function () {
         var input = $(this).find("input,textarea");
+        var errorId = "error_" + input.attr("id");
+        var alertErrorId = "alert_" + errorId;
+
         if (
           input.val() === "" ||
           (input.attr("id") === "email" && !validateEmail(input.val()))
@@ -16,7 +21,10 @@
           noerrors = false;
           if (!$(this).find("span.err-label").length) {
             var error = input.attr("data-error");
-            var errorId = "error_" + input.attr("id");
+
+            // Makes the error text visible in the alert box.
+            document.getElementById(alertErrorId).classList.remove("usa-error--alert");
+
             var label = $(this).find("label");
             label.after(
               '<span id="' +
@@ -29,11 +37,13 @@
           }
 
           document.getElementById(input.attr("id")).classList.add("usa-user-error");
-
+          modifyAlertErrorBox();
         }
         else if ($(this).find("span.err-label").length) {
           $(this).find("span.err-label").remove();
           document.getElementById(input.attr("id")).classList.remove("usa-user-error");
+          // Makes the error text invisible in the alert box.
+          document.getElementById(alertErrorId).classList.add("usa-error--alert");
         }
       });
 
@@ -66,6 +76,35 @@ function timestamp() {
 
 setInterval(timestamp, 500);
 
+function modifyAlertErrorBox() {
+  // If there is an error, modify the alert box header text based on the number of fields with errors.
+  document.getElementById("error-box").classList.remove("usa-error--alert");
+  document.getElementById("error-box").focus();
+
+  var errors = document.querySelectorAll('[id*="alert_error_"]:not(.usa-error--alert)');
+  console.log(errors.length);
+  if (errors.length === 1) {
+      // English Header text when there is only one error
+      if (document.documentElement.lang === "en") {
+          document.getElementById("error-box").getElementsByTagName("h3")[0].innerHTML = "Your information contains an error";
+      }
+      // Spanish Header text when there is only one error
+      else {
+          document.getElementById("error-box").getElementsByTagName("h3")[0].innerHTML = "Su información contiene 1 error";
+      }
+  }
+  else {
+      // English Header text when there is more than one error
+      if (document.documentElement.lang === "en") {
+          document.getElementById("error-box").getElementsByTagName("h3")[0].innerHTML = "Your information contains " + errors.length + " errors";
+      }
+      // Spanish Header text when there is more than one error
+      else {
+          document.getElementById("error-box").getElementsByTagName("h3")[0].innerHTML = "Su información contiene " + errors.length + " errores";
+      }
+  }
+}
+
 // This function runs every time the "Submit" button is pressed on the "Report an issue" page.
 var submitPressed = function () {
   "use strict";
@@ -86,14 +125,18 @@ var submitPressed = function () {
       }
       // Adds the error outline to the reCaptcha box.
       document.getElementsByClassName("recaptcha-outline-padding")[0].classList.add("usa-user-error");
-
+      // Makes the reCaptcha error text visible in the alert box.
+      document.getElementById("alert_error_recaptcha").classList.remove("usa-error--alert");
+      modifyAlertErrorBox();
     }
-    return false;
+    return true;
   }
   else {
     // Removes the error style from the reCaptcha.
     document.getElementsByClassName("recaptcha-outline-padding")[0].classList.remove("usa-user-error");
     $(".err-label-captcha").remove();
+    // Makes the reCaptcha error text invisible in the alert box.
+    document.getElementById("alert_error_recaptcha").classList.add("usa-error--alert");
   }
   return true;
 };
