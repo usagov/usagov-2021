fc724de756e2a95072544bef3bdaed1215ae5e5b
IsabelLaurenceau <isabel.laurenceau@gsa.gov>
2024-02-09 12:00:26 -0500

    usagov-1313-SAYT-federal-directories: create agency search

--- web/themes/custom/usagov/components/searchbar/searchbar.js
+++ web/themes/custom/usagov/components/searchbar/searchbar.js
@@ -2,52 +2,57 @@ const search_input = document.getElementById('search-field-en-small');
 const dir_search_results = document.getElementById('fed-dir-search-results');
 
 let search_term = '';
-let agencies;
 
-const fetchAgencies = async () => {
-  console.log('test fetch');
-  agencies = [{
-      "agency_name": "AbilityOne Commission",
-      "agency_url": "http://localhost/agencies/u-s-abilityone-commission",
-    }, {
-      "agency_name": "Access Board",
-      "agency_url": "http://localhost/agencies/u-s-access-board",
-    }, {
-      "agency_name": "Administration for Children and Families",
-      "agency_url": "http://localhost/agencies/administration-for-children-and-families",
-    }, {
-      "agency_name": "Administration for Community Living",
-      "agency_url": "http://localhost/agencies/administration-for-community-living",
-    }];
+function fetchAgencies() {
+  return fetch('sites/default/files/directory_report_federal')
+    .then((response) => response.json());
 };
 
-const showAgencies = async () => {
-  await fetchAgencies();
+function searchAgencies(allAgencies) {
+  return allAgencies
+  .filter(agency =>
+      agency.agency_title.toLowerCase().includes(search_term.toLowerCase())
+  );
+}
+
+function showAgencies(filteredAgencies) {
+  console.log(filteredAgencies);
   dir_search_results.innerText = "";
   const usasearch_sayt = document.createElement('div');
   usasearch_sayt.classList.add('usasearch_sayt');
   const ul = document.createElement('ul');
 
-  agencies
+  filteredAgencies
     .forEach(agency => {
         const li = document.createElement('li');
-
         const anchor = document.createElement('a');
+
         anchor.href = agency.agency_url;
-        anchor.innerText = agency.agency_name;
+        anchor.innerText = agency.agency_title;
 
         li.appendChild(anchor);
-
         ul.appendChild(li);
     });
 
   usasearch_sayt.appendChild(ul);
   dir_search_results.appendChild(usasearch_sayt);
-  console.log(dir_search_results);
 };
 
-search_input.addEventListener('input', e => {
-  console.log('test event listener');
-  search_term = e.target.value;
-  showAgencies();
-});
+async function getAgencies() {
+  try {
+    let allAgencies = await fetchAgencies();
+    let filteredAgencies = await searchAgencies(allAgencies);
+    await showAgencies(filteredAgencies);
+  }
+  catch (status) {
+    // no suggestions found
+  }
+}
+
+if (search_input) {
+  search_input.addEventListener('input', e => {
+    search_term = e.target.value;
+    getAgencies();
+  });
+}
+
