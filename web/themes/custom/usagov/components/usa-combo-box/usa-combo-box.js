9fd676ac2211af11e166bc13ff1424019cd1ae15
IsabelLaurenceau <isabel.laurenceau@gsa.gov>
2024-02-26 15:41:12 -0500

    usagov-1387-state-abbreviations: Remove unused files

--- web/themes/custom/usagov/components/usa-combo-box/usa-combo-box.js
+++ web/themes/custom/usagov/components/usa-combo-box/usa-combo-box.js
@@ -6,23 +6,16 @@
   let stateSelect;
   let search_term = "";
 
-  // show the state list
-
   // create the state and territories array
-  async function getStatesAndTerritories() {
+  function getStatesAndTerritories() {
     stateSelect = document.getElementById("stateselect");
     statesAndTerritoriesOptions = stateSelect.getElementsByTagName("option");
-
+    statesAndTerritories = [];
     for (let i = 0; i < statesAndTerritoriesOptions.length; i++) {
       let key = statesAndTerritoriesOptions[i].attributes.key.value;
       let name = statesAndTerritoriesOptions[i].innerText;
       statesAndTerritories.push({"name": name, "key": key});
     }
-
-    let filteredStatesAndTerritories = await filterStatesAndTerritories(
-      statesAndTerritories
-    );
-    console.log(filteredStatesAndTerritories);
   }
 
   // filter the state and territories array
@@ -36,12 +29,20 @@
     );
   }
 
+  // show the state list
+  function displayFilteredStatesAndTerritories() {
+
+  }
+
   // get the input of the combo box
   if (stateInput) {
     stateInput.addEventListener("input", (e) => {
       search_term = e.target.value;
       if (search_term != "") {
         getStatesAndTerritories();
+        let filteredStatesAndTerritories = filterStatesAndTerritories(
+          statesAndTerritories
+        );
       }
     });
   }
