f320df3313f50421e504f6a415bfd8e8501db0be
IsabelLaurenceau <isabel.laurenceau@gsa.gov>
2024-03-04 15:32:14 -0500

    usagov-1387-less: Move combobox over to js

--- web/themes/custom/usagov/scripts/stateDirectory.js
+++ web/themes/custom/usagov/scripts/stateDirectory.js
@@ -1,8 +1,19 @@
 jQuery(document).ready(function ($) {
   "use strict";
-  $("#statelist").after(
-    '<label class="visuallyhidden">Select your state or territory:<select class="usa-select usa-sr-only usa-combo-box__select" name="state-info" id="stateselect" aria-hidden="true" tabindex="-1"></select></label>'
+  // {# For the data-filter =
+  //   "{{ '{{' }}query}}.*| -> suggest states that start with the query
+  //   .*\s{{ '{{' }}query}}.*| -> suggest states that start with the query
+  //   .*\({{ '{{' }}query}}.*" -> suggest states with an abbreviation that matches the query #}
+  var comboBoxDiv =`<div id="comboBoxDiv" class="usa-combo-box width-full mobile-lg:width-mobile" data-filter="{{ '{{' }}query}}.*|.*\\s{{ '{{' }}query}}.*|.*\\({{ '{{' }}query}}.*">`;
+
+  $("#comboBoxAfter").append(comboBoxDiv);
+
+  // hidden label for a11y
+  $("#comboBoxDiv").append(
+    '<label class="visuallyhidden">Select or type your state or territory:<select class="usa-select usa-sr-only usa-combo-box__select" name="state-info" id="stateselect" aria-hidden="true" tabindex="-1"></select></label>'
   );
+
+  // add the states to the options
   $("#statelist li a").each(function () {
     $("#stateselect").append(
       '<option key="' + $(this).attr("key") +'"' +
@@ -13,12 +24,26 @@ jQuery(document).ready(function ($) {
         "</option>"
     );
   });
-  $("#state-info").after(
-    '<span class="usa-combo-box__clear-input__wrapper" tabindex="-1"><button type="button" class="usa-combo-box__clear-input" aria-label="Clear the select contents">&nbsp;</button></span><span class="usa-combo-box__input-button-separator">&nbsp;</span><span class="usa-combo-box__toggle-list__wrapper" tabindex="-1"><button type="button" tabindex="0" class="usa-combo-box__toggle-list" aria-label="Toggle the dropdown list">&nbsp;</button></span><ul tabindex="-1" id="state-info--list" class="usa-combo-box__list" role="listbox" aria-labelledby="state-info-label" hidden=""></ul><div class="usa-combo-box__status usa-sr-only" role="status"></div><span id="state-info--assistiveHint" class="usa-sr-only">When autocomplete results are available use up and down arrows to review and enter to select.Touch device users, explore by touch or with swipe gestures.</span>'
+
+  // input for select options
+  $("#comboBoxDiv").append(
+    '<input id="state-info" aria-autocomplete="list" aria-expanded="false" autocapitalize="off" autocomplete="off" class="usa-combo-box__input" type="text" aria-labelledby="state-info-label" role="combobox" required>'
   );
 
+  // options for combobox
+  $("#state-info").after(
+    '<span class="usa-combo-box__input-button-separator">&nbsp;</span><span class="usa-combo-box__toggle-list__wrapper" tabindex="-1"><button type="button" tabindex="0" class="usa-combo-box__toggle-list" aria-label="Toggle the dropdown list">&nbsp;</button></span><ul tabindex="-1" id="state-info--list" class="usa-combo-box__list" role="listbox" aria-labelledby="state-info-label" hidden=""></ul><div class="usa-combo-box__status usa-sr-only" role="status"></div><span id="state-info--assistiveHint" class="usa-sr-only">When autocomplete results are available use up and down arrows to review and enter to select.Touch device users, explore by touch or with swipe gestures.</span>'
+  );
+
+  var docLang = [document.documentElement.lang];
+  const sumBtn = docLang == "es" ? "Ir" : "Go";
+  $("#submitAfter").append(
+    `<button class="usa-button sd-go-btn usa-button--secondary" type="submit">${sumBtn}</button>`
+  );
   var goButton = $(".sd-go-btn");
+
   $("#statelist").remove();
+
   var url=$('#stateselect').val();
   var statename;
 
@@ -40,4 +65,3 @@ jQuery(document).ready(function ($) {
     statename=$('#stateselect option:selected').text();
   });
 });
-
