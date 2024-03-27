857bf212ef7174f3ba0a6450f8a9523f636d92f5
developer43 <bryant.jones@gsa.gov>
2024-03-27 15:06:51 -0400

    USAGOV-1348-docker-config-for-cypress: update code to add text to wysiwyg

--- automated_tests/e2e-cypress/cypress/e2e/cms/life-event-es.cy.js
+++ automated_tests/e2e-cypress/cypress/e2e/cms/life-event-es.cy.js
@@ -4,9 +4,7 @@ describe('Life Event Es', () => {
     Cypress.on('uncaught:exception', () => false)
 
     cy.logIn()
-    cy.get('[data-drupal-selector="edit-name"]').type('')
-    cy.get('[data-drupal-selector="edit-pass"]').type('')
-    cy.get('[data-drupal-selector="edit-submit"]').click()
+
 
     //navigate menu to add content to a basic page
     //cy.get('div > a#toolbar-item-administration')
@@ -19,29 +17,19 @@ describe('Life Event Es', () => {
     cy.get("#edit-field-meta-description-0-value").type("Find government programs for food, health care, and other expenses to help during pregnancy and early childhood. See how to collect child support. test")
     cy.get("#edit-field-short-description-0-value").type("This is a test page description")
     cy.get('[data-drupal-selector="edit-field-language-toggle-0-target-id"]').type('Embarazo y primera infancia')
-    cy.get('#ui-id-4').click()
-    //Selects Language
-    //cy.get('#edit-langcode-0-value option:selected').select('Egnlish').should('have.value', 'English')
-    //cy.get("#edit-langcode-0-value").select("Español")
 
     //cy.get("edit-field-language-toggle-0-target-id").type()
 
-
     //cy.get('#edit-body-0-format--2').select("HTML").should('have.value', 'html')
 
-    //#cke_1_top, #cke_1_toolbox, #cke_42
-    cy.get('#cke_42').click()
+    //Selects Language
+    //cy.get('#edit-langcode-0-value option:selected').select('Egnlish').should('have.value', 'English')
+    cy.get("#edit-langcode-0-value").select("Español")
 
-    //Put content in the Body
-    cy.get("#cke_1_contents>textarea")
-        //.find('textarea')
-    //cy.get('[data-drupal-selector="edit-body-0-value"]')
-          //.its('0.contentDocument')
-          //.its('body')
-          //.find('p')
-          //.type('Learn how to get nutritious food for yourself and your family through SNAP (food stamps), D-SNAP, and WIC for women, infants, and children.')
-          //.type('<p>hello world</p>')
-    //cy.get('#edit-body-0-format--2').select("HTML").should('have.value', 'html')
+    //add content to the wysiwyg
+    cy.get('div.ck-editor__main .ck-blurred').eq(0).click()
+    cy.get('div.ck-editor__main .ck-focused').eq(0)
+    cy.get('.ck-content[contenteditable=true]').realType('Learn how to get nutritious food for yourself and your family through SNAP (food stamps), D-SNAP, and WIC for women, infants, and children.')
 
     //Select page type
     //cy.get("#edit-field-page-type").select("Standard Page")
@@ -64,13 +52,14 @@ describe('Life Event Es', () => {
     //cy.get("input").focus()
     //cy.get('#edit-upload--s6nLDVOayCI > div.form-managed-file__main > #edit-upload-upload--fIl5AIpXUcA').click()
 
-        //.selectFile('Banner_img_Birth_en.png')
-    cy.get('#edit-advanced')
-    cy.get('#edit-menu').click()
-    cy.get('[data-drupal-selector="edit-menu"]')
-    cy.get('#edit-menu-enabled').check()
-    cy.get('[data-drupal-selector="edit-menu-title"]').type('Having a child and early childhood')
-    cy.get('#edit-menu-node-menus-en-menu-parent').select('-- Life events')
+     //add link to menu and select taxonomy
+     cy.get('#edit-advanced')
+     cy.get('#edit-menu').click()
+     cy.get('[data-drupal-selector="edit-menu"]')
+     cy.get('#edit-menu-enabled').check()
+     cy.get('[data-drupal-selector="edit-menu-title"]').clear().type('Embarazo y primera infancia')
+     //cy.get('#edit-menu-node-menus-en-menu-parent').select('-- Life events')
+     cy.get('#edit-menu-node-menus-es-menu-parent').select('-- Etapas importantes de la vida')
 
     //fill out url alias
     cy.get ('[data-drupal-selector="edit-path-0-alias"]').type('/Having-child-early-childhood-test')
@@ -83,15 +72,16 @@ describe('Life Event Es', () => {
     //cy.get("#edit-moderation-state-0-state").select("Publish")
 
     //Save page
-    //cy.get('[ data-drupal-selector="edit-submit" ]').click()
+    cy.get('[ data-drupal-selector="edit-submit" ]').click()
 
     //delete test page
     /*
     cy.get('ul > li > a').contains('Content').focus().click()
-    cy.get('#edit-title').type('Having a child and early childhood test')
+    cy.get('#edit-combine').type('Having a child and early childhood test')
     cy.get('#edit-submit-content').click()
     cy.get('#edit-node-bulk-form-0').check()
-    cy.get('#edit-submit--2').click()
+    cy.get('#edit-action').select('Delete content')
+    cy.get('#edit-submit').click()
     cy.get('#edit-submit').click()
     */
   })
