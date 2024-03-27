e5255568e59b61b5554cc56f11626ad1ef12fd00
developer43 <bryant.jones@gsa.gov>
2024-03-27 11:33:04 -0400

    USAGOV-1348-docker-config-for-cypress: update code to add page to the left menu

--- automated_tests/e2e-cypress/cypress/e2e/cms/basic-page.cy.js
+++ automated_tests/e2e-cypress/cypress/e2e/cms/basic-page.cy.js
@@ -22,12 +22,6 @@ describe('Local cms login', () => {
 
     //cy.get("edit-field-language-toggle-0-target-id").type()
 
-    //Put content in the Body
-    cy.get("#edit-body-wrapper")
-          .find('div.ck-editor__editable')
-          .find('p')
-          .type('Learn how to get nutritious food for yourself and your family through SNAP (food stamps), D-SNAP, and WIC for women, infants, and children.')
-
     //Select page type
     cy.get("#edit-field-page-type").select("Standard Page")
     //cy.get("#edit-field-page-type").select("Life Events")
@@ -41,6 +35,17 @@ describe('Local cms login', () => {
     //cy.get('[data-drupal-selector="edit-field-navigation-banner-image-open-button"]')
     //cy.get('[data-drupal-selector="edit-upload-upload-yodljhjblcy"]').selectFile('Banner_img_Birth_en.png')
 
+    //add content to the wysiwyg
+    cy.get('div.ck-editor__main .ck-blurred').eq(0).click()
+    cy.get('div.ck-editor__main .ck-focused').eq(0)
+    cy.get('.ck-content[contenteditable=true]').realType('Learn how to get nutritious food for yourself and your family through SNAP (food stamps), D-SNAP, and WIC for women, infants, and children.')
+
+    //add link to left menu
+    cy.get('#edit-advanced')
+    cy.get('#edit-menu').click()
+    cy.get('[data-drupal-selector="edit-menu"]')
+    cy.get('#edit-menu-enabled').check()
+
     //fill out url alias
     cy.get ('[data-drupal-selector="edit-path-0-alias"]').type('/testing/test1')
 
@@ -48,20 +53,20 @@ describe('Local cms login', () => {
     //Right now I can't publish duo to the software not having rights to publish
     //Right now software cna only save as Draft or Ready for Review
     cy.get("#edit-moderation-state-0-state").select("Draft")
-    //cy.get("#edit-moderation-state-0-state").select("Publish")
+    //cy.get("#edit-moderation-state-0-state").select("Published")
 
     //Save page
-    //cy.get('[ data-drupal-selector="edit-submit" ]').click()
+    cy.get('[ data-drupal-selector="edit-submit" ]').click()
 
     //delete test page
     /*
     cy.get('ul > li > a').contains('Content').focus().click()
-    cy.get('#edit-title').type('This is a test title')
+    cy.get('#edit-combine').type('File Upload test')
     cy.get('#edit-submit-content').click()
     cy.get('#edit-node-bulk-form-0').check()
-    cy.get('#edit-submit--2').click()
+    cy.get('#edit-action').select('Delete content')
+    cy.get('#edit-submit').click()
     cy.get('#edit-submit').click()
     */
-
   })
 })
