fa9e4af3193205c41d146f08444566c615abc471
developer43 <bryant.jones@gsa.gov>
2024-03-28 10:13:53 -0400

    USAGOV-1348-docker-config-for-cypress: code clean-up

--- automated_tests/e2e-cypress/cypress/e2e/cms/life-event-es.cy.js
+++ automated_tests/e2e-cypress/cypress/e2e/cms/life-event-es.cy.js
@@ -5,7 +5,6 @@ describe('Life Event Es', () => {
 
     cy.logIn()
 
-
     //navigate menu to add content to a basic page
     //cy.get('div > a#toolbar-item-administration')
     //cy.get('ul.toolbar-menu:first > li.menu-item:nth-of-type(2) > a ~ ul.toolbar-menu:first > li.menu-item:first > a ~ ul.toolbar-menu:first > li.menu-item:first > a').focus().click()
@@ -16,14 +15,12 @@ describe('Life Event Es', () => {
     cy.get("#edit-field-page-intro-0-value").type("Find government programs to help during pregnancy and early childhood. test")
     cy.get("#edit-field-meta-description-0-value").type("Find government programs for food, health care, and other expenses to help during pregnancy and early childhood. See how to collect child support. test")
     cy.get("#edit-field-short-description-0-value").type("This is a test page description")
-    cy.get('[data-drupal-selector="edit-field-language-toggle-0-target-id"]').type('Embarazo y primera infancia')
-
-    //cy.get("edit-field-language-toggle-0-target-id").type()
-
-    //cy.get('#edit-body-0-format--2').select("HTML").should('have.value', 'html')
 
     //Select Spanish language
     cy.languageToggle()
+
+    //Input for the language toggle page
+    cy.get('[data-drupal-selector="edit-field-language-toggle-0-target-id"]').type('Having a child in early childhood')
 
     //add content to the wysiwyg
     cy.get('div.ck-editor__main .ck-blurred').eq(0).click()
