57d9af1fae92de2a955a4c06b1fb92d908566ddb
developer43 <bryant.jones@gsa.gov>
2024-03-28 09:16:28 -0400

    USAGOV-1348-docker-config-for-cypress: update code to simplify language toggle

--- automated_tests/e2e-cypress/cypress/e2e/cms/ps8/add-language-toggle-es-ps8.cy.js
+++ automated_tests/e2e-cypress/cypress/e2e/cms/ps8/add-language-toggle-es-ps8.cy.js
@@ -17,21 +17,19 @@ describe('Local cms login', () => {
     cy.get("#edit-field-short-description-0-value").type("This is a test page description")
     cy.get('[data-drupal-selector="edit-field-language-toggle-0-target-id"]').type('Embarazo y primera infancia')
 
-
-    //Selects Language
-    //cy.get('#edit-langcode-0-value option:selected').select('Egnlish').should('have.value', 'English')
-    //cy.get("#edit-langcode-0-value").select("Español")
-
     //Input page for the toggle language content
     //cy.get("edit-field-language-toggle-0-target-id").type()
 
     //Select html for wysywig to put html code
     //cy.get('#edit-body-0-format--2').select("HTML").should('have.value', 'html')
 
-     //add content to the wysiwyg
-     cy.get('div.ck-editor__main .ck-blurred').eq(0).click()
-     cy.get('div.ck-editor__main .ck-focused').eq(0)
-     cy.get('.ck-content[contenteditable=true]').realType('This is a test to upload a file image.')
+    //Select Spanish language
+    cy.languageToggle()
+
+    //add content to the wysiwyg
+    cy.get('div.ck-editor__main .ck-blurred').eq(0).click()
+    cy.get('div.ck-editor__main .ck-focused').eq(0)
+    cy.get('.ck-content[contenteditable=true]').realType('This is a test to upload a file image.')
 
 
     //Select page type
