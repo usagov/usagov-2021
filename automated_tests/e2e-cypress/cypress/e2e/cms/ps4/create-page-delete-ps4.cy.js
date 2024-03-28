800ce0ecf11ac353eac1774bea92cb3e4dc37128
developer43 <bryant.jones@gsa.gov>
2024-03-28 15:49:55 -0400

    USAGOV-1348-docker-config-for-cypress: code clean-up

--- automated_tests/e2e-cypress/cypress/e2e/cms/ps4/create-page-delete-ps4.cy.js
+++ automated_tests/e2e-cypress/cypress/e2e/cms/ps4/create-page-delete-ps4.cy.js
@@ -51,16 +51,6 @@ describe('Local cms login', () => {
     //fill out url alias
     cy.get ('[data-drupal-selector="edit-path-0-alias"]').type('/food-assistance-test')
 
-    //Select how to Saves Page
-    //Right now I can't publish due to the software not having rights to publish
-    //Right now software cna only save as Draft or Ready for Review
-    cy.get('.layout-region__content')
-    cy.get("#edit-moderation-state-0-state").select("Draft")
-    //cy.get("#edit-moderation-state-0-state").select("Publish")
-
-    //Save page
-    cy.get('[ data-drupal-selector="edit-submit" ]').click()
-
     //publish page
     cy.pagePublish()
 
