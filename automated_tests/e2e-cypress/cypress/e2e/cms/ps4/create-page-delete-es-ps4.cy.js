7ae749476402705fb9d0ddfab17c3ed8c6a33f76
developer43 <bryant.jones@gsa.gov>
2024-04-01 12:48:36 -0400

    USAGOV-1348-docker-config-for-cypress: update code to simplify adding a taxonomy link to a page

--- automated_tests/e2e-cypress/cypress/e2e/cms/ps4/create-page-delete-es-ps4.cy.js
+++ automated_tests/e2e-cypress/cypress/e2e/cms/ps4/create-page-delete-es-ps4.cy.js
@@ -32,14 +32,8 @@ describe('Local cms login', () => {
     //Select navigation page image
     cy.imageSelect()
 
-    //add link to menu and select taxonomy
-    cy.get('#edit-advanced')
-    cy.get('#edit-menu').click()
-    cy.get('[data-drupal-selector="edit-menu"]')
-    cy.get('#edit-menu-enabled').check()
-    cy.get('[data-drupal-selector="edit-menu-title"]').clear().type('Embarazo y primera infancia')
-    //cy.get('#edit-menu-node-menus-en-menu-parent').select('-- Life events')
-    cy.get('#edit-menu-node-menus-es-menu-parent').select('-- Etapas importantes de la vida')
+    //add taxonomy link to Spanish page
+    cy.taxonomyLinkSpanish()
 
     //fill out url alias
     cy.get ('[data-drupal-selector="edit-path-0-alias"]').type('/es/embarazo-primera-infancia-test')
