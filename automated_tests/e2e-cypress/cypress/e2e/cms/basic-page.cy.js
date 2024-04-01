a99ab2de805c9e77e4ca0e043eb0083b0ad9a947
developer43 <bryant.jones@gsa.gov>
2024-04-01 14:40:02 -0400

    USAGOV-1348-docker-config-for-cypress: update description of test

--- automated_tests/e2e-cypress/cypress/e2e/cms/basic-page.cy.js
+++ automated_tests/e2e-cypress/cypress/e2e/cms/basic-page.cy.js
@@ -1,4 +1,4 @@
-describe('Local cms login', () => {
+describe('Create a basic page', () => {
   it('Gets, types and clicks to create a basic page', () => {
     //log into local cms
     Cypress.on('uncaught:exception', () => false)
