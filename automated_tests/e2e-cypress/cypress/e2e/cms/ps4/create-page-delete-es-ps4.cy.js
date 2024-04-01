4294b4fae14002626f766e919ceaf70562454810
developer43 <bryant.jones@gsa.gov>
2024-04-01 09:53:07 -0400

    USAGOV-1348-docker-config-for-cypress: update code to simplify adding text to wysiwyg

--- automated_tests/e2e-cypress/cypress/e2e/cms/ps4/create-page-delete-es-ps4.cy.js
+++ automated_tests/e2e-cypress/cypress/e2e/cms/ps4/create-page-delete-es-ps4.cy.js
@@ -26,10 +26,8 @@ describe('Local cms login', () => {
     //select html option for wysiwyg
     //cy.get('#edit-body-0-format--2').click().select("HTML")
 
-    //add content to the wysiwyg
-    cy.get('div.ck-editor__main .ck-blurred').eq(0).click()
-    cy.get('div.ck-editor__main .ck-focused').eq(0)
-    cy.get('.ck-content[contenteditable=true]').realType('Encuentre programas del Gobierno que ofrecen ayuda durante el embarazo y la primera infancia.')
+    //add Spanish text to wysiwyg
+    cy.textSpanish()
 
     //Select image
     cy.get('[data-drupal-selector="edit-field-navigation-banner-image-open-button"]').click()
