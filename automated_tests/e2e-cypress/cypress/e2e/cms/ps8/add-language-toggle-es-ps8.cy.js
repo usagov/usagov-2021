c3a5f98eb5950ac575f283f9a2056b75847955de
developer43 <bryant.jones@gsa.gov>
2024-04-01 14:32:38 -0400

    USAGOV-1348-docker-config-for-cypress: update description of test

--- automated_tests/e2e-cypress/cypress/e2e/cms/ps8/add-language-toggle-es-ps8.cy.js
+++ automated_tests/e2e-cypress/cypress/e2e/cms/ps8/add-language-toggle-es-ps8.cy.js
@@ -1,4 +1,4 @@
-describe('Local cms login', () => {
+describe('Add a language toggle to a Spanish page', () => {
   it('Gets, types and clicks', () => {
     //log into local cms
     Cypress.on('uncaught:exception', () => false)
