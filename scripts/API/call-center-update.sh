67948786941cd1afc817bb4c7608507bf62a56a0
Jacob Yeager <jacobaaronyeager@gmail.com>
2024-04-26 16:13:48 -0400

    USAGOV-1346-call-center-wait-times: Working call center api call dumping to s3

--- scripts/API/call-center-update.sh
+++ scripts/API/call-center-update.sh
@@ -1,28 +1,74 @@
 #!/bin/bash
 
-# Make a GET request to the Genesys API
-queues=$(curl -X GET https://api.mypurecloud.com/api/v2/routing/queues)
+environment="use2.us-gov-pure.cloud"
+clientId="ad16bccf-2afc-43f3-b6bb-37633c026c51"
+clientSecret="5zLMAwQHDHH36xq8cBR54NY4ezS2iTmT11aooK3HI0g"
+enQueueId="99b60af0-1047-4968-824e-7197fe582cea"
+spQueueId="e7eef7e6-77f9-4b8a-bb8b-fb324f5d96be"
+tokenFile="token.json"
+waitTimeFile="waittime.json"
 
-#TODO: determine what queue id belongs to usa.gov, assuming we need to find out this way.
-queueid=1
+# yes I know there is a ton of repeated code here, I couldn't get functions to work properly
+if [ -e $tokenFile ]; then
+  echo "Token file exists."
+  tokenJSON=$(< $tokenFile)
 
-waittime=$(curl -X GET /api/v2/routing/queues/$queueId/estimatedwaittime)
+  if [ -z "$tokenJSON" ]; then
+    echo "Token file is empty!"
+    tokenResponse=$(curl -X POST https://login.$environment/oauth/token -H "Content-Type: application/x-www-form-urlencoded" -d "grant_type=client_credentials&client_id=$clientId&client_secret=$clientSecret")
 
-# get json status attribute
-status=$(echo $waittime | jq -r '.status')
+    currentTime=$(date +%s)
+    expiresIn=$(echo "$tokenResponse" | jq -r '.expires_in')
+    expireTime=$((currentTime + expiresIn))
+    tokenResponse=$(jq -r --arg expireTime "$expireTime" '.expiresAt=$expireTime' <<< "$tokenResponse")
 
-if [[ $status == 200 ]]; then
-  echo "$waittime" > waittime.json
+    echo "$tokenResponse" > $tokenFile
+    token=$(echo "$tokenJSON" | jq -r '.access_token')
+  else
+    currentTime=$(date +%s)
+    expireTime=$(jq -r '.expiresAt' <<< "$tokenJSON")
+
+    if [ "$currentTime" -gt "$expireTime" ]; then
+      echo "Token expired!"
+      tokenResponse=$(curl -X POST https://login.$environment/oauth/token -H "Content-Type: application/x-www-form-urlencoded" -d "grant_type=client_credentials&client_id=$clientId&client_secret=$clientSecret")
+
+      currentTime=$(date +%s)
+      expiresIn=$(echo "$tokenResponse" | jq -r '.expires_in')
+      expireTime=$((currentTime + expiresIn))
+      tokenResponse=$(jq -r --arg expireTime "$expireTime" '.expiresAt=$expireTime' <<< "$tokenResponse")
+
+      echo "$tokenResponse" > $tokenFile
+      token=$(echo "$tokenJSON" | jq -r '.access_token')
+    else
+      echo "Token not expired."
+      token=$(echo "$tokenJSON" | jq -r '.access_token')
+    fi
+  fi
 else
-  test_data='
-  { "results": [
-    { "intent": "CALL",
-      "formula": "BEST",
-      "estimatedWaitTimeSeconds": 123 }
-  ] }'
+  echo "Token file does not exist!"
+  tokenResponse=$(curl -X POST https://login.$environment/oauth/token -H "Content-Type: application/x-www-form-urlencoded" -d "grant_type=client_credentials&client_id=$clientId&client_secret=$clientSecret")
 
-  echo "$test_data" > waittime.json
+  currentTime=$(date +%s)
+  expiresIn=$(echo "$tokenResponse" | jq -r '.expires_in')
+  expireTime=$((currentTime + expiresIn))
+  tokenResponse=$(jq -r --arg expireTime "$expireTime" '.expiresAt=$expireTime' <<< "$tokenResponse")
+
+  echo "$tokenResponse" > $tokenFile
+  token=$(echo "$tokenJSON" | jq -r '.access_token')
 fi
 
-# Upload file to S3 bucket
+echo "Getting en wait time."
+enWaitTime=$(curl -X GET https://api.$environment/api/v2/routing/queues/$enQueueId/estimatedwaittime -H "Authorization:Bearer $token" | jq -r '.results[].estimatedWaitTimeSeconds');
+
+echo "Getting sp wait time."
+spWaitTime=$(curl -X GET https://api.$environment/api/v2/routing/queues/$spQueueId/estimatedwaittime -H "Authorization:Bearer $token" | jq -r '.results[].estimatedWaitTimeSeconds');
+
+combinedWaitTimes='{ "enEstimatedWaitTimeSeconds": '$enWaitTime',
+  "spEstimatedWaitTimeSeconds": '$spWaitTime'}'
+
+echo "Setting wait time file."
+echo "$combinedWaitTimes" > $waitTimeFile
+
+# # Upload file to S3 bucket
+echo "uploading waittime.json to S3."
 aws s3 cp waittime.json s3://cg-33ba2c88-f377-4249-8b26-0a9d24caf3f5/cms/public/waittime.json
