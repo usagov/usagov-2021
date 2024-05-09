acd5becb6c1a195bad1ad931187b099144f48382
OscarMerida <oscar.merida@gsa.gov>
2024-05-09 12:29:51 -0400

    USAGOV-1514-benefits-search: Encapsulates search functionally in a separate object

--- web/themes/custom/usagov/scripts/benefitSearch.js
+++ web/themes/custom/usagov/scripts/benefitSearch.js
@@ -1,41 +1,69 @@
-const benefitSearch = {
+/**
+ *
+ * @param {string} src
+ * @param {Element} form
+ * @param {Element} resultsContainer
+ * @constructor
+ */
+function BenefitSearch(src, form, resultsContainer) {
+  "use strict";
+
+  this.src = src;
+  this.resultsContainer = resultsContainer;
+  this.form = form;
+  this.boxes = this.form.querySelectorAll('#benefitSearch input[type="checkbox"]');
+
+  let myself = this;
+
   /**
-   * @param {string} url
    * @returns {Promise<any>}
    */
-  "fetch": async function(url) {
-    "use strict";
-
-    const response = await fetch(url);
+  this.fetch = async function() {
+    const response = await fetch(myself.src);
     if (!response.ok) {
       throw new Error('Error fetching benefits ' + response.status);
     }
     return await response.json();
-  },
+  };
 
-  /**
-   *
-   * @param {Event} ev
-   * @param {NodeListOf<Element>} boxes
-   */
-  "toggleCheckboxes": function(ev, boxes) {
-    "use strict";
+  this.handleClear = function() {
+    myself.resultsContainer.innerHTML = '';
+  };
 
-    let newState = ev.target.checked;
-    for (const box of boxes) {
-      box.checked = newState;
+  this.handleSubmit = function() {
+    //  grab term ids from checked filters
+    let checked = myself.form.querySelectorAll('#benefitSearch input[type="checkbox"]:checked');
+
+    if (checked.length === 0) {
+      myself.showError();
+      return;
     }
-  },
+    // prepare to show results
+    for (const err of myself.form.querySelectorAll('.error')) {
+      err.remove();
+    }
+
+    myself.resultsContainer.innerHTML = '';
+    let terms = Array.from(checked).map((elt) => {
+      return elt.value;
+    });
+
+    // keep the benefits that match
+    let matches = myself.benefits.filter((item) => {
+      let numMatches = item.field_category.filter((value) => terms.includes(value));
+      return numMatches.length > 0;
+    });
+
+    //  c) display matching pages (later - paginate results)
+    myself.showResults(matches);
+  };
 
   /**
-   *
-   * @param {Element} container
    * @param benefits
    */
-  "showResults": function(container, benefits) {
-    "use strict";
+  this.showResults = function(benefits) {
 
-    container.scrollIntoView({"behavior": 'smooth'});
+    myself.resultsContainer.scrollIntoView({"behavior": 'smooth'});
 
     for (const b of benefits) {
       let elt = document.createElement('template');
@@ -50,55 +78,51 @@ const benefitSearch = {
 
       elt.innerHTML += `<div><h3>${b.title}</h3><p>${descr}</p></div>`;
 
-      container.innerHTML = container.innerHTML + elt.innerHTML;
+      myself.resultsContainer.innerHTML += elt.innerHTML;
     }
-  }
-};
+  };
+
+  this.showError = function() {
+    let elt = document.createElement('template');
+    elt.innerHTML = '<div class="error">Select one or more categories</div>';
+
+    myself.form.prepend(elt.content);
+  };
+
+  /**
+   * @param {Event} ev
+   */
+  this.toggleCheckboxes = function(ev) {
+    let newState = ev.target.checked;
+    for (const box of myself.boxes) {
+      box.checked = newState;
+    }
+  };
+
+  // init stuff
+  this.init = async function() {
+    this.benefits = await myself.fetch();
+
+    // select/hide all
+    const toggleAll = myself.form.querySelector('input[type="checkbox"][value="all"]');
+    toggleAll.addEventListener('click', myself.toggleCheckboxes);
+
+    // form events
+    myself.form.addEventListener('submit', myself.handleSubmit);
+    myself.form.addEventListener('reset', myself.handleClear);
+  };
+}
 
 jQuery(document).ready(async function () {
   "use strict";
 
-  let toggleAll = document.querySelector('#benefitSearch input[type="checkbox"][value="all"]');
-  let boxes = document.querySelectorAll('#benefitSearch input[type="checkbox"]');
-  let container = document.querySelector('#matchingBenefits');
-
-  toggleAll.addEventListener('click', (ev) => {
-    benefitSearch.toggleCheckboxes(ev, boxes);
-  });
-
   // 1) load search json (todo: toggle languages)
   const src = "/benefits-search.json";
-  const benefits = await benefitSearch.fetch(src);
 
-  let form = document.querySelector('#benefitSearch');
-  // 2) on click of submit, find potential matches for selected categories
-  form.addEventListener('submit', (ev) => {
-    //  a) grab term ids from checked filters
-    let checked = form.querySelectorAll('#benefitSearch input[type="checkbox"]:checked');
-
-    if (checked.length === 0) {
-      alert('Select at least one');
-      return;
-    }
-
-    container.innerHTML = '';
-    let terms = Array.from(checked).map((elt) => {
-      return elt.value;
-    });
-
-    // keep the benefits that match
-    let matches = benefits.filter((item) => {
-      let numMatches = item.field_category.filter((value) => terms.includes(value));
-      return numMatches.length > 0;
-    });
-
-    console.log(terms);
-
-    //  c) display matching pages (later - paginate results)
-    benefitSearch.showResults(container, matches);
-  });
-
-  form.addEventListener('reset', () => {
-    container.innerHTML = '';
-  });
+  const ben = new BenefitSearch(
+    src,
+    document.querySelector('#benefitSearch'),
+    document.querySelector('#matchingBenefits')
+  );
+  await ben.init();
 });
