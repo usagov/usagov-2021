8c980544a8687ceace58815e656992282d46cc22
Jacob Yeager <jacobaaronyeager@gmail.com>
2024-05-10 14:00:32 -0400

    USAGOV-1346-call-center-wait-times: Converting to minutes/seconds, finalizing conditions

--- web/themes/custom/usagov/scripts/callCenterWaitTimeLookup.js
+++ web/themes/custom/usagov/scripts/callCenterWaitTimeLookup.js
@@ -2,33 +2,68 @@ jQuery(document).ready(async function () {
   "use strict";
 
   var waittime = (function() {
-    jQuery.ajax({
-      "url": "/wait-time",
-      "type": "GET",
-      "success": function (response) {
-        var json = jQuery.parseJSON(response);
-        var seconds = false;
-        var content = "";
+    if (jQuery("html[lang|='en']").length ||
+        jQuery("html[lang|='es']").length) {
+      jQuery.ajax({
+        "url": "/wait-time",
+        "type": "GET",
+        "success": function (response) {
+          var json = jQuery.parseJSON(response);
+          var seconds = -1;
+          if (jQuery("html[lang|='en']").length) {
+            seconds = json.enEstimatedWaitTimeSeconds;
+          }
+          if (jQuery("html[lang|='es']").length) {
+            seconds = json.spEstimatedWaitTimeSeconds;
+          }
 
-        if (jQuery("html[lang|='en']").length) {
-          seconds = json.enEstimatedWaitTimeSeconds;
-          content = "Estimated wait time: " + seconds + " seconds";
-        }
-        if (jQuery("html[lang|='es']").length) {
-          seconds = json.spEstimatedWaitTimeSeconds;
-          content = "Tiempo de espera estimado: " + seconds + " segundos";
-        }
+          console.log(seconds);
+          if (seconds >= 0) {
+            var minutes = Math.floor(seconds / 60);
+            var remainingMinutes = minutes % 60;
+            seconds = seconds - (minutes * 60);
+            var content = "Estimated wait time: ";
+            var secondsText = " second";
+            var minuteText = " minute";
+            var noneText = "None";
 
-        if (seconds >= 0) {
-          jQuery('#callCenterTime').html(content);
+            if (jQuery("html[lang|='es']").length) {
+              content = "Tiempo de espera estimado: ";
+              secondsText = " segundo";
+              minuteText = " minuto";
+              noneText = "Ninguno";
+            }
+
+            if (remainingMinutes > 0) {
+              content += remainingMinutes + minuteText;
+              if (remainingMinutes > 1) {
+                content += "s";
+              }
+            }
+
+            if (seconds > 0) {
+              if (remainingMinutes > 0) {
+                content += ", ";
+              }
+              content += seconds + secondsText;
+              if (seconds > 1) {
+                content += "s";
+              }
+            }
+
+            if (minutes === 0 && seconds === 0) {
+              content = content + noneText;
+            }
+            jQuery('#callCenterTime').html(content);
+          }
+        },
+        "error": function (xhr, status, error) {
+          console.log('fail');
+          console.log(error);
+          jQuery('#callCenterTime').html(error);
         }
-      },
-      "error": function (xhr, status, error) {
-        console.log('fail');
-        console.log(error);
-        jQuery('#callCenterTime').html(error);
-      }
-    });
+      });
+    }
   });
 
   if (jQuery('#callCenterTime').length > 0) {
