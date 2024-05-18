76fee5940690008ace864749732e4e3abb448dd8
Amy Farrell <amy.farrell@gsa.gov>
2024-05-17 18:38:30 -0700

    USAGOV-1407-with-nginx: Correct the PROXYROUTE substitution in nginx config

--- .docker/src-reporter/init_config.sh
+++ .docker/src-reporter/init_config.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "Inserting environment variables into nginx config templates ... "
+echo "Inserting PROXYROUTE value into nginx config templates ... "
 for FILE in /etc/nginx/*/*.conf.tmpl /etc/nginx/*.conf.tmpl; do
     if [ -f "$FILE" ]; then
         OUTFILE=${FILE%.tmpl}
 	echo " generating $OUTFILE"
-        envsubst "$ENV_VARIABLES" < "$FILE" > "$OUTFILE"
+        envsubst "\$PROXYROUTE" < "$FILE" > "$OUTFILE"
     fi
 done
