a07991148944631597d50dc984745073f65e0b7b
OscarMerida <oscar.merida@gsa.gov>
2024-06-03 11:02:36 -0400

    USAGOV-1514-benefits-search: Makes previous & next links functional. Simplifies order of pagination updates so they are done in consistently.

--- web/themes/custom/usagov/scripts/pagination.js
+++ web/themes/custom/usagov/scripts/pagination.js
@@ -11,22 +11,24 @@
  * @property {string} navAria -- aria label for nav container
  * @property {string} lastPageAria -- aria role for nav container
  */
- /**
+
+/**
  * @param {int} total
  * @param {int} current
+ * @param {string} assetBase
  * @param {PaginationOptions} labels
  * @param {onClickCallback} onClick -- runs when link is clicked
  * @property {PaginationOptions} labels
  * @property {onClickCallback} onClick
  * @constructor
  */
-function Pagination(total, current, labels, onClick) {
+function Pagination(total, current, assetBase, labels, onClick) {
   "use strict";
 
   this.total = total;
   this.current = current;
+  this.assetBase = assetBase;
   this.labels = labels;
-  this.assetBase = '/themes/custom/usagov';
   this.onClick = onClick;
   this.nav = null;
 
@@ -64,27 +66,15 @@ function Pagination(total, current, labels, onClick) {
    /**
     * Handle when a pager link is clicked to internally update the displayed pager.
     * Then invoke the callback configured with the requested page number
-    * @param ev
+    * @param {int} num
+    * @param {Element} link
     */
-  this.handlePageClick = function(ev) {
-    /**
-     * @var {Element} link
-     */
-    let link = ev.target;
-    // get other links in the list
-    const current = link.parentElement
-      .parentElement
-      .querySelector('li > a[class*="usa-current"]')
-    ;
-    // remove current marker from siblings
-    current.classList.remove('usa-current');
-    current.removeAttribute('aria-current');
-    // mark our link as the current one
-    link.classList.add('usa-current');
-    link.setAttribute('aria-current', myself.labels.page);
-    // trigger configured click handler
-    const num = parseInt(link.innerHTML);
+  this.handlePageClick = function(num, link) {
+    // set current page and then re-draw the pager elements
+    // set current page and then re-draw the pager elements
     myself.setCurrentPage(num);
+    myself.updatePageLinks(link);
+    // trigger configured click handler
     myself.onClick(num);
   };
   /**
@@ -115,8 +105,8 @@ function Pagination(total, current, labels, onClick) {
     );
 
     link.addEventListener('click', function() {
-      myself.current += 1;
-      myself.onClick(myself.current);
+      let num = myself.current + 1;
+      myself.handlePageClick(num, link)
     });
 
     if (myself.current === myself.total) {
@@ -140,9 +130,13 @@ function Pagination(total, current, labels, onClick) {
         aria-label="${this.labels.page} ${num}"
         >${num}</a>`
     );
-
+    // handle clicks
     link.querySelector('a')
-      .addEventListener('click', myself.handlePageClick);
+      .addEventListener('click', function(ev) {
+        const link = ev.target;
+        const num = parseInt(link.innerText);
+        myself.handlePageClick(num, link);
+      });
 
     let atag = link.querySelector('a');
     if (num === this.current) {
@@ -176,9 +170,9 @@ function Pagination(total, current, labels, onClick) {
         <span class="usa-pagination__link-text">${this.labels.previous}</span></a>`
     );
 
-    link.addEventListener('click', function() {
-      myself.current -= 1;
-      myself.onClick(myself.current);
+    link.addEventListener('click', function(ev) {
+      let num = myself.current - 1;
+      myself.handlePageClick(num, link)
     });
 
     if (this.current === 1) {
@@ -245,40 +239,6 @@ function Pagination(total, current, labels, onClick) {
         myself.hideElement(myself.lastSpacer);
       }
     }
-    // figure out which pages we need to show
-    const pages = this.getNumericRange();
-    // hide the ones outside this range
-    for (let i in myself.pageLinks) {
-      if (i < pages.startAt || i > pages.stopAt) {
-        this.hideElement(myself.pageLinks[i]);
-      }
-    }
-    // now show the correct page links
-    for (let i = pages.startAt; i <= pages.stopAt; i++) {
-      if (myself.pageLinks[i]) {
-        this.showElement(myself.pageLinks[i]);
-      }
-      else {
-        myself.pageLinks[i] = this.makePageLink(i);
-        if (i <= myself.current) {
-          // Since we're counting up to insert nodes, we need a little
-          // help here to find what numeric pager element in front of
-          // which we need to insert the new one.
-          const nextSibling = this.getNextSiblingKey(i);
-          if (nextSibling) {
-            myself.pageLinks[nextSibling]
-              .insertAdjacentElement('beforebegin', myself.pageLinks[i]);
-          }
-        }
-        else {
-          // Unlike above, we can always insert this new element
-          // before the last spacer because we are counting up.
-          // It will always be the rightmost one
-          myself.lastSpacer
-            .insertAdjacentElement('beforebegin', myself.pageLinks[i]);
-        }
-      }
-    }
   };
    /**
     * Returns the key of the node to the right of num in existing pager links
@@ -326,7 +286,7 @@ function Pagination(total, current, labels, onClick) {
     // previous link
     list.append(this.makePreviousLink());
     // always show first page
-    list.append(this.makePageLink(1));
+    list.append(this.makePageLink(1, false));
     // first spacer
     if (myself.total > 6) {
       myself.firstSpacer = this.makeSpacer(pages.startAt > 2);
@@ -334,7 +294,7 @@ function Pagination(total, current, labels, onClick) {
     }
     // numeric pages
     for (let i = pages.startAt; i <= pages.stopAt; i++) {
-      myself.pageLinks[i] = this.makePageLink(i);
+      myself.pageLinks[i] = this.makePageLink(i, false);
       list.append(myself.pageLinks[i]);
     }
     if (myself.total > 6) {
@@ -354,5 +314,63 @@ function Pagination(total, current, labels, onClick) {
 
     this.nav = nav;
     return nav;
+  };
+
+  this.updatePageLinks = function(link) {
+    // figure out which pages we need to show
+    const pages = this.getNumericRange();
+    // hide the ones outside this range
+    for (let i in myself.pageLinks) {
+      if (i < pages.startAt || i > pages.stopAt) {
+        this.hideElement(myself.pageLinks[i]);
+      }
+    }
+    // now show the correct page links
+    for (let i = pages.startAt; i <= pages.stopAt; i++) {
+      if (myself.pageLinks[i]) {
+        this.showElement(myself.pageLinks[i]);
+      }
+      else {
+        myself.pageLinks[i] = this.makePageLink(i, false);
+        if (i <= myself.current) {
+          // Since we're counting up to insert nodes, we need a little
+          // help here to find what numeric pager element in front of
+          // which we need to insert the new one.
+          const nextSibling = this.getNextSiblingKey(i);
+          if (nextSibling) {
+            myself.pageLinks[nextSibling]
+              .insertAdjacentElement('beforebegin', myself.pageLinks[i]);
+          }
+        }
+        else {
+          // Unlike above, we can always insert this new element
+          // before the last spacer because we are counting up.
+          // It will always be the rightmost one
+          myself.lastSpacer
+            .insertAdjacentElement('beforebegin', myself.pageLinks[i]);
+        }
+      }
+    }
+
+    // get other links in the list
+    const current = link.parentElement
+      .parentElement
+      .querySelector('li > a[class*="usa-current"]')
+    ;
+    // remove current marker from siblings
+    current.classList.remove('usa-current');
+    current.removeAttribute('aria-current');
+    // mark the current page
+    const pageLinks = link.parentElement
+      .parentElement
+      .querySelectorAll('li.usa-pagination__page-no > a')
+    ;
+
+    pageLinks.forEach(function(link) {
+      if (link.innerText == myself.current) {
+        link.classList.add('usa-current');
+        link.setAttribute('aria-current', myself.page);
+      }
+    });
   };
 }
