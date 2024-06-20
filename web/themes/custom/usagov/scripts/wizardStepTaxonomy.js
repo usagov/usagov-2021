770044d191ca5265459781721a149233d1731814
mike dranove <96207895+soapy341@users.noreply.github.com>
2024-06-20 11:11:18 -0400

    USAGOV-1645-wizard-error-message: USAGOV-1645: Configure no option selected error message behavior for wizard steps. Copy existing error message js and rework it to allow current js to remain in place for the time being. Also hide the options list in the wizard form display.

---
+++ web/themes/custom/usagov/scripts/wizardStepTaxonomy.js
@@ -0,0 +1,53 @@
+let priorButton = document.getElementById("prior");
+
+if (priorButton != null) {
+  priorButton.addEventListener("click", priorStepFunction);
+}
+
+function priorStepFunction() {
+  "use strict";
+  dataLayer.push({"event": "Wizard_Prior"});
+}
+
+let nextButton = document.getElementById("next");
+
+if (nextButton != null) {
+  nextButton.addEventListener("click", wizardStepError);
+}
+
+function wizardStepError() {
+  "use strict";
+  let choices = document.getElementsByName("options");
+
+  const htmlLangAttr = document.documentElement.lang;
+
+
+  if (choices) {
+    for (let choice = 0; choice < choices.length; choice++) {
+      let selected = choices[choice].checked;
+      if (selected === true) {
+        document.getElementById("msg").innerHTML = "";
+        document.getElementById("msg").removeAttribute("tabindex", "-1");
+        document.getElementById("wizard-border").classList.remove("wizard_error");
+        dataLayer.push({"event": "Wizard_Next"});
+        return true;
+      }
+ else if (
+        htmlLangAttr === "en"
+      ) {
+        document.getElementById("msg").innerHTML =
+          "Error: Please choose one of the following options";
+        document.getElementById("msg").focus();
+        document.getElementById("wizard-border").classList.add("wizard_error");
+      }
+ else {
+        document.getElementById("msg").innerHTML =
+          "Error: Por favor elija una opción";
+        document.getElementById("msg").focus();
+        document.getElementById("wizard-border").classList.add("wizard_error");
+      }
+    }
+  }
+  dataLayer.push({"event": "Wizard_Error", "button": "Next"});
+  return false;
+}
