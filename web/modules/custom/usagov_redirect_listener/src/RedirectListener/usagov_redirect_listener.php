cd3ef5166c1da486013c328c8dfee8cf86740e88
Yaritza M. Garcia Chaparro <yaritza.garcia@gsa.gov>
2024-07-02 09:29:02 -0400

    USAGOV-818-Add-robots-noindex-to-metarefresh-page: Update script

---
+++ web/modules/custom/usagov_redirect_listener/src/RedirectListener/usagov_redirect_listener.php
@@ -0,0 +1,18 @@
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+
+class RedirectController extends Controller
+{
+    public function redirectAction()
+    {
+        $uri = $this->generateUrl($_redirectTo);
+
+        $response = new RedirectResponse($uri, 301);
+        $response->setContent($this->render(
+            'usagov_redirect_listener:templates:301.html.twig',
+            array( 'uri' => $uri )
+        ));
+
+        return $response;
+    }
+}
