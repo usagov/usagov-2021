6ba021363f2d5fe519f70f4752a625f0cbb9079d
arpage <straypacket@gmail.com>
2024-07-03 19:11:33 -0400

    USAGOV-1659-update-dr-space-setup-script-unofficial-merges: Latest

--- scripts/dr-space-setup.sh
+++ scripts/dr-space-setup.sh
@@ -49,87 +49,87 @@ echo "EGRESS_SPACE: $EGRESS_SPACE"
 
 source bin/deploy/get-latest-prod-containers
 
-echo bin/cloudgov/s3-clear-bucket --proceed-with-bucket-content-deletion $APP_SPACE
-$echo bin/cloudgov/s3-clear-bucket --proceed-with-bucket-content-deletion $APP_SPACE
-exit
+#echo bin/cloudgov/s3-clear-bucket --proceed-with-bucket-content-deletion $APP_SPACE
+#$echo bin/cloudgov/s3-clear-bucket --proceed-with-bucket-content-deletion $APP_SPACE
+#exit
 
 #echo  cf delete-space $APP_SPACE
 #while [ 1 = 1 ]; do clear; $echo cf delete-space $APP_SPACE; sleep 10; done
 #exit
 
+### README: !!! Only if re-creating egress space!!!
 #echo  cf delete-space $EGRESS_SPACE
 #$echo cf delete-space $EGRESS_SPACE
 #exit
 
+### README: !!! Only if re-creating egress space!!!
 #echo bin/cloudgov/create-egress-space $EGRESS_SPACE $ORG  PIPE tee ce.org
 #$echo bin/cloudgov/create-egress-space $EGRESS_SPACE $ORG | tee ce.log
 #exit
 
-# echo bin/cloudgov/create-app-space $APP_SPACE $ORG PIPE tee ca.log
-# $echo bin/cloudgov/create-app-space $APP_SPACE $ORG | tee ca.log
-# echo assertSpaceExists $APP_SPACE
-# $echo assertSpaceExists $APP_SPACE
-# echo cf target -s $APP_SPACE
-# $echo cf target -s $APP_SPACE
-# echo assertCurSpace $APP_SPACE
-# $echo assertCurSpace $APP_SPACE
-# exit
+#echo bin/cloudgov/create-app-space $APP_SPACE $ORG PIPE tee ca.log
+#$echo bin/cloudgov/create-app-space $APP_SPACE $ORG | tee ca.log
+#echo assertSpaceExists $APP_SPACE
+#$echo assertSpaceExists $APP_SPACE
+#echo cf target -s $APP_SPACE
+#$echo cf target -s $APP_SPACE
+#echo assertCurSpace $APP_SPACE
+#$echo assertCurSpace $APP_SPACE
+#exit
 
-echo cf target -s $APP_SPACE
-$echo cf target -s $APP_SPACE
-echo assertCurSpace $APP_SPACE
-$echo assertCurSpace $APP_SPACE
-echo bin/cloudgov/deploy-services PIPE tee ds.log
-$echo bin/cloudgov/deploy-services | tee ds.log
-exit
+#echo cf target -s $APP_SPACE
+#$echo cf target -s $APP_SPACE
+#echo assertCurSpace $APP_SPACE
+#$echo assertCurSpace $APP_SPACE
+#echo cf create-service s3 basic-sandbox key-value  PIPE tee cskv.log
+#$echo cf create-service s3 basic-sandbox key-value  | tee cskv.log
+#exit
 
+#echo bin/cloudgov/deploy-services PIPE tee ds.log
+#$echo bin/cloudgov/deploy-services | tee ds.log
+#exit
 
+### README: !!! Only if re-creating egress space!!!
 #echo cf target -s $EGRESS_SPACE
 #$echo cf target -s $EGRESS_SPACE
 #echo cf create-service s3 basic-sandbox key-value  PIPE tee cskv.log
 #$echo cf create-service s3 basic-sandbox key-value  | tee cskv.log
 #exit
 
+#echo cf target -s $APP_SPACE
+#$echo cf target -s $APP_SPACE
+#echo  bin/cloudgov/create-service-account $APP_SPACE cci PIPE tee csa.log
+#$echo bin/cloudgov/create-service-account $APP_SPACE cci | tee csa.log
+#echo  bin/cloudgov/create-service-account $APP_SPACE cfevents PIPE tee csa.log
+#$echo bin/cloudgov/create-service-account $APP_SPACE cfevents | tee csa.log
+#exit
 
-# echo cf target -s $APP_SPACE
-# $echo cf target -s $APP_SPACE
-# echo  bin/cloudgov/create-service-account cci PIPE tee csa.log
-# $echo bin/cloudgov/create-service-account cci | tee csa.log
-# echo  bin/cloudgov/create-service-account cfevents PIPE tee csa.log
-# $echo bin/cloudgov/create-service-account cfevents | tee csa.log
-# exit
-
-#
 #echo  cf target -s $APP_SPACE
 #$echo cf target -s $APP_SPACE
 #echo  cf delete-service ${APP_SPACE}-usagov-domain
 #$echo cf delete-service ${APP_SPACE}-usagov-domain
 #exit
+
 #
-
+# The creation of the external domains takes a bit of time, so we will loop over
+# a status command, and wait until we see success (or failure) and Ctl-C out of the loop
+#
 #echo  cf target -s $APP_SPACE
 #$echo cf target -s $APP_SPACE
-#echo  cf create-service external-domain domain ${APP_SPACE}-usagov-domain -c '{"domains": "dev-dr.usa.gov,shared-egress-dr.usa.gov"}'
-#$echo cf create-service external-domain domain ${APP_SPACE}-usagov-domain -c '{"domains": "dev-dr.usa.gov,shared-egress-dr.usa.gov"}'
-#while [ 1 = 1 ]; do clear; cf service ${APP_SPACE}-usagov-domain; sleep 10; done
+#DOMAIN_STRING="{\"domains\": \"beta-${APP_SPACE}.usa.gov, cms-${APP_SPACE}.usa.gov\"}"
+#echo  cf create-service external-domain domain ${APP_SPACE}-usagov-domain -c "$DOMAIN_STRING"
+#$echo cf create-service external-domain domain ${APP_SPACE}-usagov-domain -c "$DOMAIN_STRING"
+#while [ 1 = 1 ]; do cf service ${APP_SPACE}-usagov-domain; sleep 10; done
 #exit
 
-##
-## why is this being run again??
-##
-#echo  cf target -s $APP_SPACE
-#$echo cf target -s $APP_SPACE
-#echo  bin/cloudgov/create-service-account PIPE tee csa2.log
-#$echo bin/cloudgov/create-service-account | tee csa2.log
-#exit
-
+#
+# README:  This sequence needed to be run twice, to successfully deploy the cms app for the first time.
 #
 #echo  cf target -s $APP_SPACE
 #$echo cf target -s $APP_SPACE
 #bin/cloudgov/deploy-cms $CCI_BUILD_ID $CMS_DIGEST
 #exit
 
-#
 #echo  cf target -s $APP_SPACE
 #$echo cf target -s $APP_SPACE
 #bin/cloudgov/deploy-www $CCI_BUILD_ID $WWW_DIGEST
@@ -143,25 +143,30 @@ exit
 
 #cf set-env $WAF_APP IP_ALLOW_ALL_CMS 1
 #cf set-env $WAF_APP IP_ALLOW_ALL_WWW 1
-#cf reCCI_BUILD_IDe $WAF_APP
+#cf restage $WAF_APP
+#exit
 
 ###
-cat <<'ZZ'
-### Run this on the CMS app, to test the connection to RDS
-#!/bin/sh
-DB_NAME=$(echo $VCAP_SERVICES | jq -r '.["aws-rds"][] | .credentials.db_name')
-DB_NAME=$(echo $VCAP_SERVICES | jq -r '.["aws-rds"][] | .credentials.db_name')
-DB_USER=$(echo $VCAP_SERVICES | jq -r '.["aws-rds"][] | .credentials.username')
-DB_PW=$(echo $VCAP_SERVICES | jq -r '.["aws-rds"][] | .credentials.password')
-DB_HOST=$(echo $VCAP_SERVICES | jq -r '.["aws-rds"][] | .credentials.host')
-DB_PORT=$(echo $VCAP_SERVICES | jq -r '.["aws-rds"][] | .credentials.port')
-mysql --protocol=TCP -h$DB_HOST -P$DB_PORT -u$DB_USER -p$DB_PW $DB_NAME
-ZZ
-exit
+#cat <<'ZZ'
+####
+#### Run this on the CMS app, to test the connection to RDS
+####
+#### If the command runs successfully on the cms, then we have a working connection to the database!
+####
+##!/bin/sh
+#DB_NAME=$(echo $VCAP_SERVICES | jq -r '.["aws-rds"][] | .credentials.db_name')
+#DB_NAME=$(echo $VCAP_SERVICES | jq -r '.["aws-rds"][] | .credentials.db_name')
+#DB_USER=$(echo $VCAP_SERVICES | jq -r '.["aws-rds"][] | .credentials.username')
+#DB_PW=$(echo $VCAP_SERVICES | jq -r '.["aws-rds"][] | .credentials.password')
+#DB_HOST=$(echo $VCAP_SERVICES | jq -r '.["aws-rds"][] | .credentials.host')
+#DB_PORT=$(echo $VCAP_SERVICES | jq -r '.["aws-rds"][] | .credentials.port')
+#mysql --protocol=TCP -h$DB_HOST -P$DB_PORT -u$DB_USER -p$DB_PW $DB_NAME
+#ZZ
+#exit
 
 #PREFIX=usagov-${DEPLOY_TAG}-${SPACE}
 SQL_FILE=usagov.sql
 
-#echo "Attempting to deploy database backup $SQL_FILE to $APP_SPACE"
-#$echo cat $SQL_FILE | cf ssh cms -c "cat - > /tmp/$SQL_FILE"
-#cf ssh $CMS_APP -c "if [ -f /tmp/$SQL_FILE ]; then . /etc/profile; drush sql-drop -y; cat /tmp/$SQL_FILE | drush sql-cli; drush cr; fi"
+echo "Attempting to deploy database backup $SQL_FILE to $APP_SPACE"
+$echo cat $SQL_FILE | cf ssh cms -c "cat - > /tmp/$SQL_FILE"
+cf ssh $CMS_APP -c "if [ -f /tmp/$SQL_FILE ]; then . /etc/profile; drush sql-drop -y; cat /tmp/$SQL_FILE | drush sql-cli; drush cr; fi"
