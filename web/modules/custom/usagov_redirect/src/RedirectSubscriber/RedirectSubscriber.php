ece062856f6f9577b8ad9240d23d291fa6bd4398
Yaritza M. Garcia Chaparro <yaritza.garcia@gsa.gov>
2024-07-23 16:53:59 -0400

    USAGOV-818-Add-robots-noindex-to-metarefresh-page: Fix lint errors

--- web/modules/custom/usagov_redirect/src/RedirectSubscriber/RedirectSubscriber.php
+++ web/modules/custom/usagov_redirect/src/RedirectSubscriber/RedirectSubscriber.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Example event subscriber.
  */
@@ -10,22 +11,23 @@ namespace Drupal\usagov_redirect\RedirectSubscriber;
 
 // This is the interface we are going to implement.
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-// This class contains the event we want to subscribe to.
-use Symfony\Component\HttpKernel\KernelEvents;
-// Our event listener method will receive one of these.
-use Symfony\Component\HttpKernel\Event\RequestEvent;
 // We'll use this to perform a redirect if necessary.
 use Symfony\Component\HttpFoundation\RedirectResponse;
+// Our event listener method will receive one of these.
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+// This class contains the event we want to subscribe to.
+use Symfony\Component\HttpKernel\KernelEvents;
 
 /**
  * Subscribe to KernelEvents::REQUEST events and redirect if site is currently
  * in maintenance mode.
  */
 class RedirectSubscriber implements EventSubscriberInterface {
+
   /**
    * {@inheritdoc}
    */
-  static function getSubscribedEvents() {
+  public static function getSubscribedEvents() {
     $events[KernelEvents::REQUEST][] = array('checkForRedirection');
     return $events;
   }
@@ -45,28 +47,9 @@ class RedirectSubscriber implements EventSubscriberInterface {
 
       $event->setResponse(new RedirectResponse('http://localhost/'));
       return;
+
     }
 
-  //   $uri  = $response->getTargetUrl();
-
-  //   $raw_html = `<!DOCTYPE html>
-  //   <html>
-  //       <head>
-  //           <meta charset="UTF-8" />
-  //           <meta name="robots" content="noindex" />
-  //           <meta http-equiv="refresh" content="0;url=\'$uri\'" />
-
-  //           <title>Redirecting to $uri</title>
-  //       </head>
-  //       <body>
-  //           Redirecting to <a href="$uri">$uri</a>.
-  //       </body>
-  //   </html>`;
-
-  //   $renderer = \Drupal::service('renderer');
-  //   $HTML_conversion = $renderer->render($raw_html);
-
-  //   $response->setContent($HTML_conversion);
-
   }
+
 }
