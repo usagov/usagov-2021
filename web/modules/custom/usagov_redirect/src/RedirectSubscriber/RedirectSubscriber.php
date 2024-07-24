93af55b783127aa8622f964ef9ef4cf6525ec0b8
Yaritza M. Garcia Chaparro <yaritza.garcia@gsa.gov>
2024-07-24 12:37:36 -0400

    USAGOV-818-Add-robots-noindex-to-metarefresh-page: Set the content for the redirect response

--- web/modules/custom/usagov_redirect/src/RedirectSubscriber/RedirectSubscriber.php
+++ web/modules/custom/usagov_redirect/src/RedirectSubscriber/RedirectSubscriber.php
@@ -42,17 +42,71 @@ class RedirectSubscriber implements EventSubscriberInterface {
 
     $response = $event->getResponse();
 
-    if (($response instanceof RedirectResponse)) {
-
-      $event->setResponse(new RedirectResponse('http://localhost/'));
-
-      // $url = Url::fromRoute('entity.node.canonical', ['node' => 1]);
-      // $response = new RedirectResponse($url->toString());
-      // $event->setResponse($response);
-
+    if (!($response instanceof RedirectResponse)) {
       return;
-
     }
+
+    // $event->setResponse(new RedirectResponse('http://localhost/'));
+
+    // $url = Url::fromRoute('entity.node.canonical', ['node' => 1]);
+    // $response = new RedirectResponse($url->toString());
+    // $event->setResponse($response);
+
+    $uri  = $response->getTargetUrl();
+
+    // dump($response);
+
+    $response->setContent(
+      sprintf('<!DOCTYPE html>
+      <html>
+          <head>
+              <meta charset="UTF-8" />
+              <meta name="robots" content="noindex" />
+              <meta http-equiv="refresh" content="0;url=\'%1$s\'" />
+
+              <title>Redirecting to %1$s</title>
+          </head>
+          <body>
+              Redirecting to <a href="%1$s">%1$s</a>.
+          </body>
+      </html>', htmlspecialchars($uri, ENT_QUOTES, 'UTF-8')));
+
+    // dump($response);
+
+    // $raw_html = `<!DOCTYPE html>
+    // <html>
+    //     <head>
+    //         <meta charset="UTF-8" />
+    //         <meta name="robots" content="noindex" />
+    //         <meta http-equiv="refresh" content="0;url=\'$uri\'" />
+
+    //         <title>Redirecting to $uri</title>
+    //     </head>
+    //     <body>
+    //         Redirecting to <a href="$uri">$uri</a>.
+    //     </body>
+    // </html>`;
+
+    // $renderer = \Drupal::service('renderer');
+    // $HTML_conversion = $renderer->render($raw_html);
+
+    // $response->setContent(
+    //   \sprintf('<!DOCTYPE html>
+    //   <html>
+    //   <head>
+    //       <meta charset="UTF-8" />
+    //       <meta name="robots" content="noindex" />
+    //       <meta http-equiv="refresh" content="0;url=\'%1$s\'" />
+
+    //       <title>Redirecting to %1$s</title>
+    //   </head>
+    //   <body>
+    //       Redirecting to <a href="%1$s">%1$s</a>.
+    //   </body>
+    //   </html>', htmlspecialchars($uri, \ENT_QUOTES, 'UTF-8')));
+
+    // $response->setContent($raw_html);
+    // $response->setContent('Hello World');
 
   }
 
