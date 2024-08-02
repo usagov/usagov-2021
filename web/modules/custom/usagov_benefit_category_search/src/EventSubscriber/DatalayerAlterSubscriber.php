10ca00dfa6976ee4bccc8f441b097c0f7f7c4f3a
OscarMerida <oscar.merida@gsa.gov>
2024-08-02 16:05:11 -0400

    USAGOV-1752-refactor-datalayer: Adds benefit category values to datalayer

--- web/modules/custom/usagov_benefit_category_search/src/EventSubscriber/DatalayerAlterSubscriber.php
+++ web/modules/custom/usagov_benefit_category_search/src/EventSubscriber/DatalayerAlterSubscriber.php
@@ -2,9 +2,14 @@
 
 namespace Drupal\usagov_benefit_category_search\EventSubscriber;
 
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Drupal\node\Entity\Node;
+use Drupal\taxonomy\Entity\Term;
 use Drupal\usa_twig_vars\Event\DatalayerAlterEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
+/**
+ * Add benefit category info to datalayer.
+ */
 class DatalayerAlterSubscriber implements EventSubscriberInterface {
 
   /**
@@ -19,8 +24,21 @@ class DatalayerAlterSubscriber implements EventSubscriberInterface {
   /**
    * Adds category information to the datalayer.
    */
-  public function onDatalayerAlter(DatalayerAlterEvent $event):void {
+  public function onDatalayerAlter(DatalayerAlterEvent $event): void {
+    $node = \Drupal::routeMatch()->getParameter('node');
+    $event->datalayer['hasBenefitCategory'] = FALSE;
 
+    if ($node instanceof Node && $node->getType() === 'basic_page') {
+      $pageType = $node->field_page_type->referencedEntities()[0]->getName();
+      if ($pageType === 'Standard Page') {
+        // Provide list of categories for this page to GTM via taxonomy-data.
+        if ($terms = $node->field_benefits_category->referencedEntities()) {
+          $event->datalayer['hasBenefitCategory'] = TRUE;
+          $benefitCategories = array_map(fn(Term $term) => $term->getName(), $terms);
+          $event->datalayer['benefitCategories'] = implode('; ', $benefitCategories);
+        }
+      }
+    }
   }
 
 }
