67a7fe2f907a86685f1f2ee1547a192d2a3bcfe5
arpage <straypacket@gmail.com>
2024-08-05 12:12:59 -0400

    USAGOV-1753-generic-cron-app-alpine: Source profile in cfevents cron script

--- .docker/src-cron/etc/periodic/1min/cfevents.sh
+++ .docker/src-cron/etc/periodic/1min/cfevents.sh
@@ -6,4 +6,6 @@ if [ x$SPACE = x ]; then
    SPACE=$(echo $VCAP_APPLICATION | jq -r '.space_name')
 fi
 
+. ~/.profile $SPACE &> /dev/null
+
 cf run-task cfevents --name cfevents-instance --command "/opt/cfevents/capture-latest-events $SPACE"
