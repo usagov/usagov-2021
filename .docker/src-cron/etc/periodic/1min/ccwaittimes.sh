647d3643108a89d0e4e903347155241efb8909b3
arpage <straypacket@gmail.com>
2024-08-13 13:10:38 -0400

    USAGOV-1831-only-allow-sequential-task-runs: The functions, but probably does not behave as desired in all error cases.

--- .docker/src-cron/etc/periodic/1min/ccwaittimes.sh
+++ .docker/src-cron/etc/periodic/1min/ccwaittimes.sh
@@ -1,3 +1,5 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+. ~/.profile &> /dev/null
 
 cf run-task callcenter --name callcenter-wt-update --command "/opt/callcenter/call-center-update"
--- .docker/src-cron/etc/periodic/1min/cfevents.sh
+++ .docker/src-cron/etc/periodic/1min/cfevents.sh
@@ -1,11 +1,41 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 SPACE=$1
-
 if [ x$SPACE = x ]; then
-   SPACE=$(echo $VCAP_APPLICATION | jq -r '.space_name')
+  SPACE=$(echo $VCAP_APPLICATION | jq -r '.space_name')
 fi
 
-. ~/.profile $SPACE &> /dev/null
+source /opt/cron/cf-task-check
 
-cf run-task cfevents --name cfevents-instance --command "/opt/cfevents/capture-latest-events $SPACE"
+/opt/cron/task-check 1 cfevents
+RETVAL=$?
+
+case $RETVAL in
+3)
+  echo Incorrect usage error for task-check
+  exit 1
+  ;;
+2)
+ exit 0 ### skipping run - removed stale lock
+  ;;
+1)
+  exit 0 ### skipping run - previous run still in progress
+  ;;
+0)
+
+  . ~/.profile $SPACE &> /dev/null
+  TASKRUNNING=$(runtask cfevents "/opt/cfevents/capture-latest-events $SPACE")
+
+  while [ x"$TASKRUNNING" != x ]; do
+    echo checking cfevents status
+    TASKRUNNING=$(cktasklocks cfevents)
+    echo "TASKRUNNING: $TASKRUNNING"
+    sleep 10
+  done
+  /opt/cron/clean-task cfevents
+  ;;
+*)
+  echo "Unknown error: $RETVAL"
+  exit $RETVAL
+  ;;
+esac
