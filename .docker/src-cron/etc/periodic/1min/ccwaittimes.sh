745492afe5f33fdd4a296f2e1b896ba6a290e186
arpage <straypacket@gmail.com>
2024-08-20 19:25:27 -0400

    USAGOV-1753-generic-cron-app-alpine: Fix path to lock pre/post scripts

--- .docker/src-cron/etc/periodic/1min/ccwaittimes.sh
+++ .docker/src-cron/etc/periodic/1min/ccwaittimes.sh
@@ -2,4 +2,11 @@
 
 . ~/.profile &> /dev/null
 
+TASKNAME=$(basename $0)
+TASKPID=$$
+
+source $TASKLOCK_SCRIPT_ROOT/lock-singleton-task $TASKNAME $TASKPID
+
 /opt/callcenter/call-center-update
+
+source $TASKLOCK_SCRIPT_ROOT/unlock-singleton-task $TASKNAME $TASKPID
--- .docker/src-cron/etc/periodic/1min/cfevents.sh
+++ .docker/src-cron/etc/periodic/1min/cfevents.sh
@@ -2,4 +2,13 @@
 
 SPACE=$1
 
+source ~/.profile $SPACE &> /dev/null
+
+TASKNAME=$(basename $0)
+TASKPID=$$
+
+source $TASKLOCK_SCRIPT_ROOT/lock-singleton-task $TASKNAME $TASKPID
+
 /opt/cfevents/capture-latest-events $SPACE
+
+source $TASKLOCK_SCRIPT_ROOT/unlock-singleton-task $TASKNAME $TASKPID
