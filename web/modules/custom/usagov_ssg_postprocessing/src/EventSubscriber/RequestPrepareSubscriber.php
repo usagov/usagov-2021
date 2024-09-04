fa48c2385c6f5de931de3e4cd1f6ebc18170a1a7
OscarMerida <oscar.merida@gsa.gov>
2024-09-04 11:16:28 -0400

    USAGOV-1937-clear-menu-link: Adds an event subscriber to clear menu link cache.
     Needed when running a Tome export to build page navigation correctly when more than one path is exported by a process.

---
+++ web/modules/custom/usagov_ssg_postprocessing/src/EventSubscriber/RequestPrepareSubscriber.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\usagov_ssg_postprocessing\EventSubscriber;
+
+use Drupal\Core\Menu\MenuActiveTrailInterface;
+use Drupal\tome_static\Event\TomeStaticEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Clears the menu active trail cache between Tome requests.
+ *
+ * @internal
+ */
+class RequestPrepareSubscriber implements EventSubscriberInterface {
+
+  public function __construct(
+    private MenuActiveTrailInterface $menu_active_trail,
+  ) {
+
+  }
+
+  public function requestPrepare() {
+    $this->menu_active_trail->clear();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[TomeStaticEvents::REQUEST_PREPARE][] = ['requestPrepare'];
+    return $events;
+  }
+
+}
