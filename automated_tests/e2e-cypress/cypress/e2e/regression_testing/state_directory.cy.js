29dc3d0e42f30e9092613839f582368c4ffa8a67
IsabelLaurenceau <isabel.laurenceau@gsa.gov>
2024-09-05 16:22:55 -0400

    USAGOV-1846-State-Directory-Cypress-Tests: Update state_directory.cy.js

--- automated_tests/e2e-cypress/cypress/e2e/regression_testing/state_directory.cy.js
+++ automated_tests/e2e-cypress/cypress/e2e/regression_testing/state_directory.cy.js
@@ -1,64 +1,98 @@
-describe("State Directory [ENG]", () => {
-  beforeEach(() => {
-    cy.visit("/state-governments");
+describe("State Directories", () => {
+  context("State Directory", () => {
+    beforeEach(() => {
+      cy.visit("/state-governments");
+    });
+    it("BTE 50A: Landing page: state drop-down", () => {
+      // testing dropdown menu on click
+      cy.get("[data-test='stateInput']")
+        .click()
+        .then(($p) => {
+          cy.get("[data-test='stateDropDown']").should("be.visible");
+        });
+    });
+
+    it("BTE 50B: Landing page: state drop-down", () => {
+      //testing typing in dropdown menu
+      cy.get("[data-test='stateInput']")
+        .type("Ne")
+        .then(($p) => {
+          cy.get("[data-test='stateDropDown']").should("be.visible");
+        });
+
+      cy.get("[data-test='stateInput']")
+        .type("bras")
+        .then(($p) => {
+          cy.get("[data-test='stateDropDown']").find("li").contains("Nebraska");
+        });
+    });
+
+    it("BTE 50C: Landing page: state drop-down", () => {
+      cy.get("[data-test='stateInput']")
+        .type("1")
+        .then(($p) => {
+          cy.get("[data-test='stateDropDown']")
+            .find("li")
+            .contains("No results found");
+        });
+    });
   });
 
-  // it('BTE 50: Landing page: state drop-down', () => {
-  //     // testing dropdown menu
-  //     cy.get('#block-usagov-content')
-  //         .find('[id=stateForm]')
-  //         .find('ul')
-  //         .should('[role=listbox]')
-  //         .each((el) => {
-  //             cy.wrap(el)
-  //                 .invoke('data-value')
-  //                 .then(href => {
-  //                     cy.request(href)
-  //                         .its('status')
-  //                         .should('eq', 200)
-  //                 })
-  //         })
-  // })
+  context("State Page", () => {
+    it("BTE 51: Test Alaska Page", () => {
+      cy.visit("/states/alaska");
 
-  // it('BTE 51: Test Alaska Page', () => {
-  //     cy.visit('/states/alaska')
-
-  //     // Test links on page.
-  //     cy.get('#State-Directory-Table').find('a').then(regLink => {
-  //         cy.get(regLink[0]).should('have.attr', 'href').and('include', 'https://alaska.gov/')
-  //         cy.get(regLink[1]).should('have.attr', 'href').and('include', 'https://gov.alaska.gov/')
-  //     })
-  // })
-
-  it("BTE 52A: Landing page: state drop-down", () => {
-    // testing dropdown menu on click
-    cy.get("[data-test='stateInput']")
-      .click()
-      .then(($p) => {
-        cy.get("[data-test='stateDropDown']").should("be.visible");
-      });
+      // Test links on page.
+      cy.get("#State-Directory-Table")
+        .find("a")
+        .then((regLink) => {
+          cy.get(regLink[0])
+            .should("have.attr", "href")
+            .and("include", "https://alaska.gov/");
+          cy.get(regLink[1])
+            .should("have.attr", "href")
+            .and("include", "https://gov.alaska.gov/");
+        });
+    });
   });
-  it("BTE 52B: Landing page: state drop-down", () => {
-    //testing typing in dropdown menu
-    cy.get("[data-test='stateInput']")
-      .type("Ne")
-      .then(($p) => {
-        cy.get("[data-test='stateDropDown']").should("be.visible");
+
+  context("50 State Pages", () => {
+    beforeEach(() => {
+        cy.visit("/state-motor-vehicle-services");
       });
 
-    cy.get("[data-test='stateInput']")
-      .type("bras")
-      .then(($p) => {
-        cy.get("[data-test='stateDropDown']").find("li").contains("Nebraska");
-      });
-  });
-  it("BTE 52C: Landing page: state drop-down", () => {
-    cy.get("[data-test='stateInput']")
-      .type("1")
-      .then(($p) => {
-        cy.get("[data-test='stateDropDown']")
-          .find("li")
-          .contains("No results found");
-      });
+    it("BTE 52A: 50-state pages: state drop-down", () => {
+      // testing dropdown menu on click
+      cy.get("[data-test='stateInput']")
+        .click()
+        .then(($p) => {
+          cy.get("[data-test='stateDropDown']").should("be.visible");
+        });
+    });
+
+    it("BTE 52B: 50-state pages: state drop-down", () => {
+      //testing typing in dropdown menu
+      cy.get("[data-test='stateInput']")
+        .type("Ne")
+        .then(($p) => {
+          cy.get("[data-test='stateDropDown']").should("be.visible");
+        });
+
+      cy.get("[data-test='stateInput']")
+        .type("bras")
+        .then(($p) => {
+          cy.get("[data-test='stateDropDown']").find("li").contains("Nebraska");
+        });
+    });
+
+    it("BTE 52C: 50-state pages: state drop-down", () => {
+      cy.get("[data-test='stateInput']")
+        .type("1")
+        .then(($p) => {
+          cy.get("[data-test='stateDropDown']")
+            .find("li")
+            .contains("No results found");
+        });
+    });
   });
 });
