fc5a255b47ddb963008c67ed51fb71305c7fa470
IsabelLaurenceau <isabel.laurenceau@gsa.gov>
2024-09-11 14:35:39 -0400

    USAGOV-1846-State-Directory-Cypress-Tests: Update visual_testing.cy.js

--- automated_tests/e2e-cypress/cypress/e2e/regression_testing/visual_testing.cy.js
+++ automated_tests/e2e-cypress/cypress/e2e/regression_testing/visual_testing.cy.js
@@ -1,14 +1,15 @@
 const paths = {};
-paths["home"]="/";
-paths["spanish_home"]="/es";
-paths["state_governments"]="/state-governments"
-
+paths["home"]="https://www.usa.gov/";
+paths["home_es"]="https://www.usa.gov/es";
+paths["state_governments"]="https://www.usa.gov/state-governments";
+paths["state_governments_es"]="https://www.usa.gov/es/gobiernos-estatales";
 
 context("Visual Regression Tests", () => {
   for (let name in paths){
     it("Create snapshots for visual tests", () => {
       cy.visit(paths[name]);
-      cy.compareSnapshot(name)
+      cy.scrollTo('bottom', { duration: 1500 });
+      cy.compareSnapshot(name);
     });
   }
 });
