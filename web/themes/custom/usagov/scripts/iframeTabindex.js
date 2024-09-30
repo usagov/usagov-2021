b70c9e981a2674866ff72c4c38ae9285f56da61b
developer43 <bryant.jones@gsa.gov>
2024-09-30 10:22:53 -0400

    usagov-1949-report-issue-incorrect-tab-order: script to add iframe attribute

---
+++ web/themes/custom/usagov/scripts/iframeTabindex.js
@@ -0,0 +1,23 @@
+/**
+ * Improve the accessibility of pages that use recaptcha.
+ * Adding a tabindex attribute to an iframe in the recaptcha for the correct tabbing order
+ * Used on pages: report a website issue in English and Spanish
+ */
+
+
+// checks to see if the page has loaded
+function winLoad(callback) {
+    "use strict";
+  if (document.readyState === 'complete') {
+    callback();
+  } else {
+    window.addEventListener("load", callback);
+  }
+}
+
+// adds the attrubute to the iframe once the page is loaded
+winLoad(function() {
+    "use strict";
+    const iframe = document.querySelector("iframe");
+    iframe.setAttribute("tabindex", "1");
+});
