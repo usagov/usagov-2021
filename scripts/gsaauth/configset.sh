296e813ef97d50eaf4516532ceb7da79a21a2d5f
arpage <straypacket@gmail.com>
2024-10-07 12:30:35 -0400

    USAGOV-1990-gsaauth: Prevent splitting of env vars

--- scripts/gsaauth/configset.sh
+++ scripts/gsaauth/configset.sh
@@ -10,8 +10,8 @@ SPACE=$(echo "$SPACE" | tr '[:upper:]' '[:lower:]')
 
 if [ -f $SCRIPT_DIR/gsaauth.$SPACE.conf ]; then
   while read -r f v; do
-    echo  drush cset -y --input-format=yaml samlauth.authentication ${f} ${v}
-    $echo drush cset -y --input-format=yaml samlauth.authentication ${f} ${v}
+    echo  drush cset -y --input-format=yaml samlauth.authentication "${f}" "${v}"
+    $echo drush cset -y --input-format=yaml samlauth.authentication "${f}" "${v}"
   done < $SCRIPT_DIR/gsaauth.$SPACE.conf 
 else
   echo Cannot find GSA Auth config file for $SPACE: $SCRIPT_DIR/gsaauth.$SPACE.conf 
