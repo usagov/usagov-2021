7312e49ca195ed0dfba77a3609bcfc9819a96da4
developer43 <bryant.jones@gsa.gov>
2024-11-05 15:45:08 -0500

    usagov-2053-ceo-error-inline-message: added "Error" to inline error form text

--- web/themes/custom/usagov/scripts/ceoSubmission.js
+++ web/themes/custom/usagov/scripts/ceoSubmission.js
@@ -241,6 +241,7 @@ async function handleFormSubmission() {
             document.getElementById(errorID).classList.remove("usa-error--alert");
 
             var message;
+
             if (!field.value) {
                 // Changing to use the error method specified in the CMS if available
                 var cmsError = document.getElementById(errorID);
@@ -259,7 +260,7 @@ async function handleFormSubmission() {
                 document.getElementById(errorID).getElementsByTagName("span")[0].innerHTML =  sanitizeResponse;
             }
 
-            field.previousElementSibling.innerHTML = message;
+            field.previousElementSibling.innerHTML = "Error: " + message;
 
             // Check if the street address, zip code or city field is empty and if it is, add the vertical line on the left side.
             if (field.previousElementSibling.id === "street" ||
