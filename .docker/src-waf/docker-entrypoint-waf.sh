1f0c6ac21c37bef480fc7979eb6e86565c7ba69a
Jacob Yeager <jacobaaronyeager@gmail.com>
2024-12-30 16:31:02 -0500

    USAGOV-2079-waf-nginx-update: fixed the path for activate_rules.sh

--- .docker/src-waf/docker-entrypoint-waf.sh
+++ .docker/src-waf/docker-entrypoint-waf.sh
@@ -93,7 +93,7 @@ done
 # Update the list of IPs blocked via domain lookup:
 /etc/nginx/dynamic/deny_domain_by_ip.sh --no-reload
 
-# . /opt/modsecurity/activate-rules.sh
+. /opt/modsecurity/activate-rules.sh
 
 # Run crond
 exec /usr/sbin/crond -c /etc/crontabs &
