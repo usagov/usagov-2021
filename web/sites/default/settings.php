ce53465acbc9f0be253df4129e45de4a99e1ec7a
OscarMerida <oscar.merida@gsa.gov>
2025-01-03 11:27:56 -0500

    USAGOV-2110-memcache: Configure drupal to use memcache for caching.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -939,11 +939,76 @@ if (!empty($cf_application_data['space_name']) &&
   }
 }
 
+if (extension_loaded('memcached')) {
+  $settings['memcache']['servers'] = ['cache:11211' => 'default'];
+  $settings['memcache']['bins'] = ['default' => 'default'];
+  $settings['memcache']['key_prefix'] = '';
+  $settings['memcache']['options'] = [
+    Memcached::OPT_COMPRESSION => TRUE,
+  ];
+
+  $settings['container_yamls'][] = $app_root . '/modules/contrib/memcache/example.services.yml';
+
+  $memcache_exists = class_exists('Memcache', FALSE);
+  $memcached_exists = class_exists('Memcached', FALSE);
+  if ($memcache_exists || $memcached_exists) {
+    $class_loader->addPsr4('Drupal\\memcache\\', 'modules/contrib/memcache/src');
+
+    // Define custom bootstrap container definition to use Memcache for cache.container.
+    $settings['bootstrap_container_definition'] = [
+      'parameters' => [],
+      'services' => [
+        # Dependencies.
+        'settings' => [
+          'class' => 'Drupal\Core\Site\Settings',
+          'factory' => 'Drupal\Core\Site\Settings::getInstance',
+        ],
+        'memcache.settings' => [
+          'class' => 'Drupal\memcache\MemcacheSettings',
+          'arguments' => ['@settings'],
+        ],
+        'memcache.factory' => [
+          'class' => 'Drupal\memcache\Driver\MemcacheDriverFactory',
+          'arguments' => ['@memcache.settings'],
+        ],
+        'memcache.timestamp.invalidator.bin' => [
+          'class' => 'Drupal\memcache\Invalidator\MemcacheTimestampInvalidator',
+          # Adjust tolerance factor as appropriate when not running memcache on localhost.
+          'arguments' => ['@memcache.factory', 'memcache_bin_timestamps', 0.001],
+        ],
+        'memcache.timestamp.invalidator.tag' => [
+          'class' => 'Drupal\memcache\Invalidator\MemcacheTimestampInvalidator',
+          # Remember to update your main service definition in sync with this!
+          # Adjust tolerance factor as appropriate when not running memcache on localhost.
+          'arguments' => ['@memcache.factory', 'memcache_tag_timestamps', 0.001],
+        ],
+        'memcache.backend.cache.container' => [
+          'class' => 'Drupal\memcache\DrupalMemcacheInterface',
+          'factory' => ['@memcache.factory', 'get'],
+          # Actual cache bin to use for the container cache.
+          'arguments' => ['container'],
+        ],
+        # Define a custom cache tags invalidator for the bootstrap container.
+        'cache_tags_provider.container' => [
+          'class' => 'Drupal\memcache\Cache\TimestampCacheTagsChecksum',
+          'arguments' => ['@memcache.timestamp.invalidator.tag'],
+        ],
+        'cache.container' => [
+          'class' => 'Drupal\memcache\MemcacheBackend',
+          'arguments' => ['container', '@memcache.backend.cache.container', '@cache_tags_provider.container', '@memcache.timestamp.invalidator.bin'],
+        ],
+      ],
+    ];
+  }
+
+  $settings['cache']['default'] = 'cache.backend.memcache';
+
+}
 // Add cache.backend.null:
 $settings['container_yamls'][] = DRUPAL_ROOT . '/sites/default/nonlocal.services.yml';
 
 if (PHP_SAPI === 'cli' && str_starts_with($_SERVER["argv"][1], 'tome:static')) {
-  // Disable the page and menu cache on tome runs
+  // Disable the page and menu cache on tome runs.
   $settings['cache']['bins']['page'] = 'cache.backend.null';
   $settings['cache']['bins']['menu'] = 'cache.backend.null';
   $settings['cache']['bins']['data'] = 'cache.backend.null';
