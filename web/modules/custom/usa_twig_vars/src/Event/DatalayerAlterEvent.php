8a09dd7862642ac75a31ecc1b13be198a410d776
OscarMerida <oscar.merida@gsa.gov>
2025-01-15 13:17:36 -0500

    USAGOV-2186-phpstan-twig-vars: Adding return types, argument types, docblocks for usa_twig_vars.module code based on PHPStan level 6 errors.

--- web/modules/custom/usa_twig_vars/src/Event/DatalayerAlterEvent.php
+++ web/modules/custom/usa_twig_vars/src/Event/DatalayerAlterEvent.php
@@ -3,10 +3,17 @@
 namespace Drupal\usa_twig_vars\Event;
 
 use Drupal\Component\EventDispatcher\Event;
+use Drupal\usa_twig_vars\TaxonomyDatalayerBuilder;
 
+/**
+ * @phpstan-import-type TaxonomyBreadcrumb from TaxonomyDatalayerBuilder
+ */
 class DatalayerAlterEvent extends Event {
   const EVENT_NAME = 'usa_twig_vars.datalayer_alter';
 
+  /**
+   * @param TaxonomyBreadcrumb $datalayer
+   */
   public function __construct(
     public array $datalayer,
   ) {}
