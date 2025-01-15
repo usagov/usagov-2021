2a715d8f76bb0dc3803c4e5fd0ddcdebcf8b1c8c
YaritzaGarcia <yaritza.garcia@gsa.gov>
2025-01-15 07:50:44 -0400

    USAGOV-2164-analytics-CMS-space: Update Controller code for testing

--- web/modules/custom/usagov_analytics/src/Controller/UsaGovAnalyticsController.php
+++ web/modules/custom/usagov_analytics/src/Controller/UsaGovAnalyticsController.php
@@ -14,19 +14,19 @@ class UsaGovAnalyticsController extends ControllerBase {
    * Controller routines for USA Contact Center API routes.
    */
   public function runScript() {
+    
     // Check if the user is authenticated.
     $current_user = \Drupal::currentUser();
 
     if ($current_user->isAuthenticated()) {
 
-      $command = escapeshellcmd('cd modules ls');
-      $output = shell_exec($command);
-      echo $output;
+      // Run the python script.
+      $output = shell_exec('
+        cd modules/custom/usagov_analytics/src/Scripts
+        chmod +x setup.sh
+        sh ./setup.sh
+      ');
 
-      // // Run the python script.
-      // $output = shell_exec('python3 /modules/custom/usagov_analytics/Scripts/gsc_api_v3.py');
-
-      // echo $output;
       // Create a file response.
       $response = new Response($output);
       $response->headers->set('Content-Type', 'text/plain');
