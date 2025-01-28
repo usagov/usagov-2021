0d1af983ee73ed3c42ddaf7b19796ea786f0af65
Todd Blumenthal <todd.blumenthal@gsa.gov>
2025-01-28 11:15:52 -0500

    USAGOV-2191-phpstan-usagov-login: phpstan corrections

--- web/modules/custom/usagov_login/src/UserRouteAccess.php
+++ web/modules/custom/usagov_login/src/UserRouteAccess.php
@@ -3,6 +3,8 @@
 namespace Drupal\usagov_login;
 
 use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Access\AccessResultForbidden;
+use Drupal\Core\Access\AccessResultAllowed;
 use Drupal\Core\Config\ImmutableConfig;
 use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
 use Drupal\Core\Session\AccountInterface;
@@ -16,6 +18,10 @@ class UserRouteAccess implements ContainerInjectionInterface {
     private StateInterface $state,
   ) {}
 
+  /**
+   * @param ContainerInterface $container
+   * @return self
+   */
   public static function create(ContainerInterface $container): self {
     return new self(
       config: $container->get('config.factory')->get('usagov_login.settings'),
@@ -23,7 +29,11 @@ class UserRouteAccess implements ContainerInjectionInterface {
     );
   }
 
-  public function checkAccess(AccountInterface $account) {
+  /**
+   * @param AccountInterface $account
+   * @return AccessResultForbidden|AccessResultAllowed
+   */
+  public function checkAccess(AccountInterface $account): AccessResultForbidden|AccessResultAllowed {
     $loginPath = $this->config->get('sso_login_path');
     $forceLocalForm = $this->state->get('usagov_login_local_form', 0);
 
