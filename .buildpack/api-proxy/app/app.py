6d1fabe5c317a90bd1f727d73ae15264aed7e3b9
Jacob Yeager <jacobaaronyeager@gmail.com>
2025-02-21 16:05:49 -0500

    USAGOV-2220-api-proxy-python: added flask_limiter for rate limiting

--- .buildpack/api-proxy/app/app.py
+++ .buildpack/api-proxy/app/app.py
@@ -14,7 +14,16 @@ import requests_cache
 from urllib.parse import unquote
 from flask import Flask, Response, jsonify, request
 
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+
 app = Flask(__name__)
+limiter = Limiter(
+    get_remote_address,
+    app=app,
+    default_limits=["200 per day", "50 per hour"],
+    storage_uri="memory://",
+)
 
 # Configure logging
 logging.basicConfig(
