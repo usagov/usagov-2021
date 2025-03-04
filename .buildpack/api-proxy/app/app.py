59fca892af7af04a92e86a482cbdf86b5de0cda9
Jacob Yeager <jacobaaronyeager@gmail.com>
2025-03-04 11:51:40 -0500

    USAGOV-2220-api-proxy-python: trying changing the port to 61443

--- .buildpack/api-proxy/app/app.py
+++ .buildpack/api-proxy/app/app.py
@@ -116,6 +116,7 @@ def handle_connect():
     )
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8080))
+    # port = int(os.getenv("PORT", 8080))
+    port = int(os.getenv("PORT", 61443))
     logger.info("Starting Flask API Proxy on port %s", port)
     app.run(host="0.0.0.0", port=port)
