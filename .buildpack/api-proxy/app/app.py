795591bb7222c30520aaca6108949ff04c13651e
Jacob Yeager <jacobaaronyeager@gmail.com>
2025-03-21 14:33:02 -0400

    USAGOV-2220-api-proxy-python: Attempt 2 ad having snyk ignore it

--- .buildpack/api-proxy/app/app.py
+++ .buildpack/api-proxy/app/app.py
@@ -158,5 +158,3 @@ if __name__ == "__main__":
     port = int(os.getenv("PORT", 8080))
     logger.info("Starting Flask API Proxy on port %s", port)
     app.run(host="0.0.0.0", port=port)
-
-# snyk: ignore:SSRF
