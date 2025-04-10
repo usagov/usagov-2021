aa77a9bc98b0ca5f1fff8bf06f45809191cfe402
Jacob Yeager <jacobaaronyeager@gmail.com>
2025-04-10 14:36:22 -0400

    USAGOV-2314-cron-for-scheduled-publish: simplifying run-lightweight-cron.sh uri selection and removing logging

--- .docker/src-cron/etc/periodic/5min/run-lightweight-cron.sh
+++ .docker/src-cron/etc/periodic/5min/run-lightweight-cron.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-echo "Attempting lightweight cron"
 # grab the cloudgov space we are hosted in
 APP_SPACE=$(echo "$VCAP_APPLICATION" | jq -r '.space_name')
 SECRETS=$(echo $VCAP_SERVICES | jq -r '.["user-provided"][] | select(.name == "cron-secrets") | .credentials')
@@ -10,19 +9,13 @@ CRON_KEY=$(echo $SECRETS | jq -r '.CRON_KEY')
 if [ "${CF_INSTANCE_INDEX:-''}" == "0" ]; then
 
   # Use unique uri per environment - default to prod
-  if [ "${APP_SPACE}" = "dev" ]; then
-    URI="http://cms-dev-usagov.apps.internal"
-  elif [ "${APP_SPACE}" = "dr" ]; then
-    URI="http://cms-dr-usagov.apps.internal"
-  elif [ "${APP_SPACE}" = "stage" ]; then
-    URI="http://cms-stage-usagov.apps.internal"
-  elif [ "${APP_SPACE}" = "local" ]; then
-    URI="http://localhost"
+  if [ "${APP_SPACE}" = "local" ]; then
+    URI="http://cms-usagov.docker.local"
   else
-    URI="http://cms-prod-usagov.apps.internal"
+    URI="http://cms-${APP_SPACE}-usagov.apps.internal"
   fi
 
-  echo "Running lightweight cron on $URI/scheduler/cron/$CRON_KEY"
+  echo "Running lightweight cron"
   curl "$URI/scheduler/cron/$CRON_KEY"
 
 fi
