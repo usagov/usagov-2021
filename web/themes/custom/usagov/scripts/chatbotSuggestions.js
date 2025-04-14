0bc72771d37e9c51e098bc39420f5e293e4da735
YaritzaGarcia <yaritza.garcia@gsa.gov>
2025-04-14 12:41:29 -0400

    USAGOV-Chatbot-API: Improved the toggle implementation and added a twig template for the chatbot

--- web/themes/custom/usagov/scripts/chatbotSuggestions.js
+++ web/themes/custom/usagov/scripts/chatbotSuggestions.js
@@ -27,14 +27,17 @@
                     const currentClass = mutation.target.className;
                     
                     const isOpen = currentClass.includes("is-closed");
-
-                    svgContainer = document.querySelector('#block-usagov-aideepchatchatbot > div > div.ai-deepchat--header > .toggle-icon');
+                    
+                    const openChatbotIcon = document.getElementsByClassName("open-chatbot-icon")[0];
+                    const closeChatbotIcon = document.getElementsByClassName("close-chatbot-icon")[0];
 
                     if (isOpen) {
-                        svgContainer.innerHTML = '<svg class="toogle-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="#1b1b1b" d="M432 256c0 17.7-14.3 32-32 32L48 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z"/></svg>';
+                        closeChatbotIcon.style.display = 'inline';
+                        openChatbotIcon.style.display = 'none';
                     }   
                     else {
-                        svgContainer.innerHTML = '<svg class="toogle-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>'
+                        openChatbotIcon.style.display = 'inline';
+                        closeChatbotIcon.style.display = 'none';
                     }
 
                 }
