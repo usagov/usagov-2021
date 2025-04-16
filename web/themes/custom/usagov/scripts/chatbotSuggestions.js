c2a0678e8dc7f2a8cc0e3973cfc2958eb6358b44
YaritzaGarcia <yaritza.garcia@gsa.gov>
2025-04-16 17:27:54 -0400

    USAGOV-Chatbot-API: Moved all the styling that was in the YML to the twig template. Fixed some styling stuff and spaces.
    The YML was only available in my local, it wasn't working correctly but this is a solution for it.

--- web/themes/custom/usagov/scripts/chatbotSuggestions.js
+++ web/themes/custom/usagov/scripts/chatbotSuggestions.js
@@ -1,11 +1,6 @@
 (function () {
 
-    // const suggestionBox = document.createElement("div");
-    // suggestionBox.classList.add("deep-chat-temporary-message");
-    // suggestionBox.innerHTML = '<button class="deep-chat-button deep-chat-suggestion-button" style="margin-top: 5px">Suggestion1</button>';
-
     document.querySelector("#chevron > path").setAttribute("fill", "");
-
     
     const targetSelector = '#block-usagov-aideepchatchatbot > div > div.ai-deepchat--header > .toggle-icon';
 
@@ -49,6 +44,8 @@
 })();
 
 document.addEventListener('DOMContentLoaded', function() {
+
+    // Adding the tag "Experimental".
     const experimentalTag = document.createElement("button");
     experimentalTag.classList.add("chatbot-tag");
     experimentalTag.innerHTML = 'Experimental';
