b08077e2795e50c547c50b647ee5a8e2b300b3b7
YaritzaGarcia <yaritza.garcia@gsa.gov>
2025-05-13 09:57:32 -0400

    USAGOV-chatbot-custom-UI: Adding new library for Ollama and updating the code to use both libraries.

--- web/modules/custom/usagov_chatbot/src/Controller/GenerateRAGController.php
+++ web/modules/custom/usagov_chatbot/src/Controller/GenerateRAGController.php
@@ -5,6 +5,7 @@ namespace Drupal\usagov_chatbot\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Codewithkyrian\ChromaDB\ChromaDB;
 use Codewithkyrian\ChromaDB\ChromaDB\Embeddings\OllamaEmbeddingFunction;
+use ArdaGnsrn\Ollama\Ollama;
 
 /**
  * Class GenerateRAGController.
@@ -22,11 +23,46 @@ class GenerateRAGController {
         ->withPort(80)
         ->connect();
 
-      $version = $chromaDB->version();
+      $collection = $chromaDB->getCollection('usagovsite');
 
-      $output = "<h2>ChromaDB Version: $version</h2>";
+      // return new Response($collection->count());
 
-      return new Response($output);
+      // $embeddingFunction = new OllamaEmbeddingFunction();
+
+      // Connect to Ollama server.
+      $ollamaClient = Ollama::client('http://ob.straypacket.com');
+
+      // Need to change this with the user message.
+      $query = "What is usa.gov?";
+
+      // Find a way to list the models available and return it.
+
+      $queryembed = $ollamaClient->embed()->create([
+          'model' => 'nomic-embed-text',
+          'input' => [
+            $query,
+          ]
+      ]);
+
+      // $relateddocs = ;
+
+      $prompt =
+        "{$query} - Answer that question using ONLY the resources provided. " .
+        "Please avoid saying things similar to 'not enough data' and 'there is no further information'" .
+        "Do not admit ignorance of other data, even if there is more data available, " .
+        "outside of the resources provided. " .
+
+        "Please keep the answer factual, and avoid superlatives or unnecessary adjectives." .
+
+        "Do not provide any data, or make any suggestions unless it comes from the " .
+        "following resources: {$relateddocs}.";
+
+      $completions = $ollamaClient->completions()->create([
+        'model' => 'llama3.2',
+        'prompt' => $prompt,
+      ]);
+
+      return new Response(dump($completions->response));
     }
     catch (\Exception $e) {
       return new Response('An error occured: ' . $e->getMessage(), 500);
