8a4fbf2b0d5d263dc72758daf5f91fe88dc86aa2
Jacob Yeager <jacobaaronyeager@gmail.com>
2025-05-23 14:16:04 -0400

    USAGOV-1685-Fix-Site-banner: minor change to bump in cci

--- scripts/tome-run.sh
+++ scripts/tome-run.sh
@@ -89,7 +89,6 @@ if [ -f $RETRY_SEMAPHORE_FILE ]; then
   rm $RETRY_SEMAPHORE_FILE
 fi
 
-
 # check nodes and blocks for any content changes in the last 30 minutes
 export CONTENT_UPDATED=$(drush sql:query "SELECT SUM(c) FROM ( (SELECT count(*) as c from node_field_data where changed > (UNIX_TIMESTAMP(now())-(1800)))
  UNION ( SELECT count(*) as c from block_content_field_data where changed > (UNIX_TIMESTAMP(now())-(1800)))
