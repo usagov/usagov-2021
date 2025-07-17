68e4ab41af419a6ce6f2a1054cd3a9b3ea2d9156
Dale Frey <dale.frey@mobomo.com>
2025-07-17 13:58:49 -0400

    USAGOV-2436: forgot to put sleep commands in

--- scripts/tome-running-check.sh
+++ scripts/tome-running-check.sh
@@ -4,11 +4,13 @@ SCRIPT_PATH=$(dirname "$0")
 SCRIPT_NAME=tome-run.sh
 #SCRIPT_PID=$$
 
+sleep 2
 PS_AUX=$(ps aux)
 ALREADY_RUNNING=$(echo "$PS_AUX" | grep $SCRIPT_NAME | wc -l)
 if [ "$ALREADY_RUNNING" -gt "0" ]; then
 
     # Due to potential race-conditions brought up in USAGOV-2436, we will wait, and check again just to confirm
+    sleep 3
     PS_AUX2=$(ps aux)
     ALREADY_RUNNING2=$(echo "$PS_AUX2" | grep $SCRIPT_NAME | wc -l)
     if [ "$ALREADY_RUNNING2" -gt "0" ]; then
