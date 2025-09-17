257ed8cd00c6004c62025bb6b99c60ed47fa2c2f
Jacob Yeager <jacobaaronyeager@gmail.com>
2025-09-17 13:17:36 -0400

    USAGOV-2515-fix-blog-static-images: trying to do this in tome itself instead -- replacing spaces with pluses

--- web/modules/custom/usagov_ssg_postprocessing/src/EventSubscriber/TomeEventSubscriber.php
+++ web/modules/custom/usagov_ssg_postprocessing/src/EventSubscriber/TomeEventSubscriber.php
@@ -153,6 +153,35 @@ class TomeEventSubscriber implements EventSubscriberInterface {
 
     $xpath = new \DOMXPath($document);
     $changes = FALSE;
+
+    // --- USAGOV-2515: Replace spaces with '+' in image/media src and srcset attributes ---
+    // img[src], source[src], and any srcset attributes
+    $img_nodes = $xpath->query('//*[@src]');
+    foreach ($img_nodes as $node) {
+      $src = $node->getAttribute('src');
+      if (strpos($src, ' ') !== FALSE) {
+        $new_src = str_replace(' ', '+', $src);
+        $node->setAttribute('src', $new_src);
+        $changes = TRUE;
+      }
+    }
+    // srcset attributes (img, source, etc)
+    $srcset_nodes = $xpath->query('//*[@srcset]');
+    foreach ($srcset_nodes as $node) {
+      $srcset = $node->getAttribute('srcset');
+      if (strpos($srcset, ' ') !== FALSE) {
+        // srcset can be multiple URLs, e.g. "img1.png 1x, img2.png 2x"
+        $new_srcset = preg_replace_callback('/([^,]+)( [0-9]+[wx])?/', function($matches) {
+          $url = $matches[1];
+          $rest = isset($matches[2]) ? $matches[2] : '';
+          return str_replace(' ', '+', $url) . $rest;
+        }, $srcset);
+        $node->setAttribute('srcset', $new_srcset);
+        $changes = TRUE;
+      }
+    }
+
+    // Existing logic: fix /es links
     $nodes = $xpath->query('//a[starts-with(@href,"/es")]');
 
     /** @var \DOMElement $node */
